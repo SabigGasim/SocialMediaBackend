016883fc821f57c3c72ad62669177e7054c7a95a
unknown <sabgasim@gmail.com>
2025-04-01 10:44:16 +0300

    feat(domain): Implement comment creation and like/unlike functionality

--- Domain/Entities/Comment.cs
+++ Domain/Entities/Comment.cs
@@ -2,21 +2,67 @@
 
 namespace SocialMediaBackend.Domain.Entities;
 
-public class Comment : Entity<Guid>
+public class Comment : AuditableEntity<Guid>
 {
     private readonly List<CommentLike> _likes = new();
     private readonly List<Comment> _replies = new();
-    public string? Text { get; private set; }
+
+    private Comment(Guid postId, Guid userId,
+        string text, Guid? parentCommentId = null)
+    {
+        PostId = postId;
+        UserId = userId;
+        Text = text;
+        ParentCommentId = parentCommentId;
+
+        Id = Guid.NewGuid();
+        Created = DateTimeOffset.UtcNow;
+        CreatedBy = userId.ToString();
+        LastModified = DateTimeOffset.UtcNow;
+        LastModifiedBy = userId.ToString();
+    }
+
+    private Comment() {}
+
     public Guid PostId { get; private set; }
     public Post Post { get; private set; } = default!;
     public Guid UserId { get; private set; }
     public User User { get; private set; } = default!;
     public Guid? ParentCommentId { get; private set; }
     public Comment? ParentComment { get; private set; }
+    public string? Text { get; private set; }
     public int LikesCount { get; private set; }
-    public int RepilesCount { get; private set; }
+    public int RepliesCount { get; private set; }
 
     public IReadOnlyCollection<CommentLike> Likes => _likes.AsReadOnly();
     public IReadOnlyCollection<Comment> Replies => _replies.AsReadOnly();
 
+    internal static Comment? Create(Guid postId, Guid userId, string text, Guid? parentCommentId)
+    {
+        if(string.IsNullOrEmpty(text)) 
+            return null;
+
+        return new Comment(postId, userId, text, parentCommentId);
+    }
+
+    internal bool AddLike(Guid userId)
+    {
+        var like = CommentLike.Create(userId, this.Id);
+        _likes.Add(like);
+        LikesCount++;
+
+        return true;
+    }
+
+    internal bool RemoveLike(Guid userId)
+    {
+        var like = _likes.Find(x => x.UserId == userId);
+        if (like is null)
+            return false;
+
+        _likes.Remove(like);
+        LikesCount--;
+
+        return true;
+    }
 }
