4df27cfc01ef9e53564c4506cd4c2451575a8fb4
unknown <sabgasim@gmail.com>
2025-04-07 16:06:27 +0300

    feat(sdk): add sdk for create user endpoint

---
+++ Sdk/ApiEndpoints.cs
@@ -0,0 +1,13 @@
+﻿namespace SocialMediaBackend.Sdk;
+
+internal static class ApiEndpoints
+{
+    private const string ApiBase = "/api";
+
+    internal static class Users
+    {
+        private const string Base = $"{ApiBase}/users";
+
+        public const string Create = Base;
+    }
+}
---
+++ Sdk/ISocialMediaApi.cs
@@ -0,0 +1,11 @@
+﻿using Refit;
+using SocialMediaBackend.Application.Users.CreateUser;
+
+namespace SocialMediaBackend.Sdk;
+
+
+public interface ISocialMediaApi
+{
+    [Post(ApiEndpoints.Users.Create)]
+    public Task<CreateUserResponse> CreateUserAsync(CreateUserRequest request);
+}
