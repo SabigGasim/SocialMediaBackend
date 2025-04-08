d413e2c4db825cf9df04889bcce1078decc47fc6
unknown <sabgasim@gmail.com>
2025-04-08 20:14:57 +0300

    feat(api): add DeleteUserEndpoint

---
+++ src/Presentation/Api/Endpoints/Users/DeleteUserEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Application.Users.DeleteUser;
+
+namespace SocialMediaBackend.Api.Endpoints.Users;
+
+[HttpDelete(ApiEndpoints.Users.Delete), AllowAnonymous]
+public class DeleteUserEndpoint : RequestEndpoint<DeleteUserRequest>
+{
+    public override async Task HandleAsync(DeleteUserRequest req, CancellationToken ct)
+    {
+        await HandleCommandAsync(new DeleteUserCommand(req.UserId), ct);
+    }
+}
