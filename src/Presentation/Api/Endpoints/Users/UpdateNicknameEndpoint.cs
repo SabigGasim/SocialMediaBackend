6ce42540595273a08abc5d1059550654efdcd56c
unknown <sabgasim@gmail.com>
2025-04-08 20:16:07 +0300

    feat(api): add UpdateNicknameEndpoint

---
+++ src/Presentation/Api/Endpoints/Users/UpdateNicknameEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Application.Users.UpdateNickname;
+
+namespace SocialMediaBackend.Api.Endpoints.Users;
+
+[HttpPatch(ApiEndpoints.Users.PatchNickname), AllowAnonymous]
+internal class UpdateNicknameEndpoint : RequestEndpoint<UpdateNicknameRequest>
+{
+    public override async Task HandleAsync(UpdateNicknameRequest req, CancellationToken ct)
+    {
+        await HandleCommandAsync(new UpdateNicknameCommand(req.UserId, req.Nickname), ct);
+    }
+}
