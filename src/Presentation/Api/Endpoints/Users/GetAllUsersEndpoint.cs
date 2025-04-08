85ae7cb2b29be7d450d45eeb0e006b0e8b5324e4
unknown <sabgasim@gmail.com>
2025-04-08 20:08:29 +0300

    feat(api): add GetAllUsersEndpoint

---
+++ src/Presentation/Api/Endpoints/Users/GetAllUsersEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Application.Users.GetAllUsers;
+
+namespace SocialMediaBackend.Api.Endpoints.Users;
+
+[HttpGet(ApiEndpoints.Users.GetAll), AllowAnonymous]
+public class GetAllUsersEndpoint : RequestEndpoint<GetAllUsersRequest, GetAllUsersResponse>
+{
+    public override async Task HandleAsync(GetAllUsersRequest req, CancellationToken ct)
+    {
+        await HandleRequestAsync(new GetAllUsersQuery(req.Slug, req.PageNumber, req.PageSize), ct);
+    }
+}
