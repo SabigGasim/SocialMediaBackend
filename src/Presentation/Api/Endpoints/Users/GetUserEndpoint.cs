b3614ea1625d1b1eb3be18108f6420bdc4de1f93
unknown <sabgasim@gmail.com>
2025-04-08 20:15:43 +0300

    feat(api): add GetUserEndpoint

---
+++ src/Presentation/Api/Endpoints/Users/GetUserEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Application.Users.GetUser;
+
+namespace SocialMediaBackend.Api.Endpoints.Users;
+
+[HttpGet(ApiEndpoints.Users.Get), AllowAnonymous]
+internal class GetUserEndpoint : RequestEndpoint<GetUserRequest, GetUserResponse>
+{
+    public override async Task HandleAsync(GetUserRequest req, CancellationToken ct)
+    {
+        await HandleRequestAsync(new GetUserQuery(req.IdOrUsername), ct);
+    }
+}
