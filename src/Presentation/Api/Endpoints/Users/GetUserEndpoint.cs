4c8496c45f3914d5ef10b54ff075d4a69bb86039
unknown <sabgasim@gmail.com>
2025-04-09 18:17:53 +0300

    feat(api): add summary for GetUserEndpoint

--- src/Presentation/Api/Endpoints/Users/GetUserEndpoint.cs
+++ src/Presentation/Api/Endpoints/Users/GetUserEndpoint.cs
@@ -1,6 +1,7 @@
 ﻿using FastEndpoints;
 using Microsoft.AspNetCore.Authorization;
 using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Api.Contracts.Responses;
 using SocialMediaBackend.Application.Users.GetUser;
 
 namespace SocialMediaBackend.Api.Endpoints.Users;
@@ -11,5 +12,17 @@ internal class GetUserEndpoint : RequestEndpoint<GetUserRequest, GetUserResponse
     public override async Task HandleAsync(GetUserRequest req, CancellationToken ct)
     {
         await HandleRequestAsync(new GetUserQuery(req.IdOrUsername), ct);
+    }
+}
+
+internal class GetUserEndpointSummary : Summary<GetUserEndpoint>
+{
+    public GetUserEndpointSummary()
+    {
+        Summary = "Gets a user with the specified id or username from the system";
+        Description = "Gets a user with the specified id or username from the system";
+        Response<GetUserResponse>(200, "A user with the given id or username was returned");
+        Response<ValidationFailureResponse>(400, "The given request parameter isn't valid for a username or an id");
+        Response(404, "No user with the given id or username was found");
     }
 }
