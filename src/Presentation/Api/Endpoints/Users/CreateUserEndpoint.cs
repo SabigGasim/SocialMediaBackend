93eb3b113f1e6f0e3901ed967e1a221b8c08e523
unknown <sabgasim@gmail.com>
2025-04-09 18:19:00 +0300

    feat(api): add summary for CreatUserEndpoint

--- src/Presentation/Api/Endpoints/Users/CreateUserEndpoint.cs
+++ src/Presentation/Api/Endpoints/Users/CreateUserEndpoint.cs
@@ -1,6 +1,7 @@
 ﻿using FastEndpoints;
 using Microsoft.AspNetCore.Authorization;
 using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Api.Contracts.Responses;
 using SocialMediaBackend.Application.Users.CreateUser;
 
 namespace SocialMediaBackend.Api.Endpoints.Users;
@@ -17,5 +18,19 @@ public class CreateUserEndpoint : RequestEndpoint<CreateUserRequest, CreateUserR
             req.ProfilePicture);
 
         await HandleRequestAsync(command, ct);
+    }
+}
+
+
+
+public class CreateUserEndpointSummary : Summary<CreateUserEndpoint>
+{
+    public CreateUserEndpointSummary()
+    {
+        Summary = "Creates a new user in the system";
+        Description = "Creates a new user in the system";
+        Response<CreateUserResponse>(201, "User was successfully created");
+        Response<ValidationFailureResponse>(400, "The request didn't pass validation checks");
+        Response<ValidationFailureResponse>(400, "A user with this username already exists");
     }
 }
