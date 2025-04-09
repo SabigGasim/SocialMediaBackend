bbb7f2838dab61ee54481939db38344d44520f27
unknown <sabgasim@gmail.com>
2025-04-09 22:15:13 +0300

    fix(app): attach PagedRequestValidator.cs

---
+++ src/Application/Common/PagedRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using FastEndpoints;
+using FluentValidation;
+using SocialMediaBackend.Application.Abstractions;
+
+namespace SocialMediaBackend.Application.Common;
+
+public abstract class PagedRequestValidator<TPagedRequest> : Validator<TPagedRequest>
+    where TPagedRequest : PagedRequest
+{
+    public PagedRequestValidator()
+    {
+        RuleFor(x => x.Page)
+            .GreaterThan(0);
+
+        RuleFor(x => x.PageSize)
+            .InclusiveBetween(1, 10);
+    }
+}
