cb2706de56f2db79cb75aa62501599aa8bb66af5
unknown <sabgasim@gmail.com>
2025-04-09 18:17:18 +0300

    feat(api): add summary for UpdateNicknameEndpoint

--- src/Presentation/Api/Endpoints/Users/UpdateNicknameEndpoint.cs
+++ src/Presentation/Api/Endpoints/Users/UpdateNicknameEndpoint.cs
@@ -1,6 +1,7 @@
 ﻿using FastEndpoints;
 using Microsoft.AspNetCore.Authorization;
 using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Api.Contracts.Responses;
 using SocialMediaBackend.Application.Users.UpdateNickname;
 
 namespace SocialMediaBackend.Api.Endpoints.Users;
@@ -11,5 +12,18 @@ internal class UpdateNicknameEndpoint : RequestEndpoint<UpdateNicknameRequest>
     public override async Task HandleAsync(UpdateNicknameRequest req, CancellationToken ct)
     {
         await HandleCommandAsync(new UpdateNicknameCommand(req.UserId, req.Nickname), ct);
+    }
+}
+
+
+internal class UpdateNicknameEndpointSummary : Summary<UpdateNicknameEndpoint>
+{
+    public UpdateNicknameEndpointSummary()
+    {
+        Summary = "Updates the nickname of a given user";
+        Description = "Updates the nickname of a given user";
+        Response<ValidationFailureResponse>(400, "The given nickname format isn't valid");
+        Response(404, "No user with the given id was found");
+        Response(204, "Nickname updated successfully");
     }
 }
