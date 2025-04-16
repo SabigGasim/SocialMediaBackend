cc5cf9b098234f7a465a9b439cf2d03dcc2f7441
unknown <sabgasim@gmail.com>
2025-04-16 23:09:40 +0300

    feat(api): add GetPostEndpoint

---
+++ src/Presentation/Api/Endpoints/Posts/GetPostEndpoints.cs
@@ -0,0 +1,15 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Application.Posts.GetPost;
+
+namespace SocialMediaBackend.Api.Endpoints.Posts;
+
+[HttpGet(ApiEndpoints.Posts.Get), AllowAnonymous]
+public class GetPostEndpoints : RequestEndpoint<GetPostRequest, GetPostResponse>
+{
+    public override Task HandleAsync(GetPostRequest req, CancellationToken ct)
+    {
+        return HandleRequestAsync(new GetPostQuery(req.PostId), ct);
+    }
+}
