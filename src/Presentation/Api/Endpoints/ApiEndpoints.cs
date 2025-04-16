387a2883cf1ebcf6c3ac58d49e4127152124d3e8
unknown <sabgasim@gmail.com>
2025-04-16 22:57:13 +0300

    feat(api): add CreatePostEndpoint

--- src/Presentation/Api/Endpoints/ApiEndpoints.cs
+++ src/Presentation/Api/Endpoints/ApiEndpoints.cs
@@ -17,4 +17,14 @@ public static class ApiEndpoints
         public const string PatchUsername = $"{PatchBase}/username";
         public const string PatchNickname = $"{PatchBase}/nickname";
     }
+
+    public static class Posts
+    {
+        private const string Base = $"{ApiBase}/posts";
+
+        public const string Create = Base;
+        public const string Get = $"{Base}/{{PostId}}";
+        public const string GetAll = Base;
+        public const string Delete = $"{Base}/{{PostId}}";
+    }
 }
---
+++ src/Presentation/Api/Endpoints/Posts/CreatePostEndpoint.cs
@@ -0,0 +1,18 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Application.Posts.CreatePost;
+
+namespace SocialMediaBackend.Api.Endpoints.Posts;
+
+[HttpPost(ApiEndpoints.Posts.Create), AllowAnonymous]
+public class CreatePostEndpoint : RequestEndpoint<CreatePostRequest, CreatePostResponse>
+{
+    public override Task HandleAsync(CreatePostRequest req, CancellationToken ct)
+    {
+        var userId = Guid.Parse("d6a319b0-5c31-4929-84a0-3b918efc318e");
+        var command = new CreatePostCommand(userId, req.Text, req.MediaItems);
+        
+        return HandleRequestAsync(command, ct);
+    }
+}
