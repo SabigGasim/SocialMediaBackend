42ee2572182f34bb2b52ddfb4a5c1890a8ea539b
unknown <sabgasim@gmail.com>
2025-04-17 03:23:45 +0300

    feat(api): add UpdatePostEndpoint

---
+++ src/Presentation/Api/Endpoints/Posts/UpdatePostEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Application.Posts.UpdatePost;
+
+namespace SocialMediaBackend.Api.Endpoints.Posts;
+
+[HttpPatch(ApiEndpoints.Posts.Patch), AllowAnonymous]
+public class UpdatePostEndpoint : RequestEndpoint<UpdatePostRequest>
+{
+    public override Task HandleAsync(UpdatePostRequest req, CancellationToken ct)
+    {
+        return HandleCommandAsync(new UpdatePostCommand(req.PostId, req.Text), ct);
+    }
+}
