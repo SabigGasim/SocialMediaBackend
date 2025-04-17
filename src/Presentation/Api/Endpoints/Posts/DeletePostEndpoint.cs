2cf827ef824cc1eb5f3b8364346eeb8c05651b16
unknown <sabgasim@gmail.com>
2025-04-17 03:21:31 +0300

    feat(api): add DeletePostEndpoint

---
+++ src/Presentation/Api/Endpoints/Posts/DeletePostEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Application.Posts.DeletePost;
+
+namespace SocialMediaBackend.Api.Endpoints.Posts;
+
+[HttpDelete(ApiEndpoints.Posts.Delete), AllowAnonymous]
+public class DeletePostEndpoint : RequestEndpoint<DeletePostRequest>
+{
+    public override Task HandleAsync(DeletePostRequest req, CancellationToken ct)
+    {
+        return HandleCommandAsync(new DeletePostCommand(req.PostId), ct);
+    }
+}
