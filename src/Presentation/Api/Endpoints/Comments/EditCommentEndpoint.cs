7b7556a041f65de62a4dcf9d2b2a5e4f094981bf
unknown <sabgasim@gmail.com>
2025-04-18 04:44:48 +0300

    feat(api): add EditCommentEndpoint

---
+++ src/Presentation/Api/Endpoints/Comments/EditCommentEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Application.Comments.EditComment;
+
+namespace SocialMediaBackend.Api.Endpoints.Comments;
+
+[HttpPatch(ApiEndpoints.Comments.Patch), AllowAnonymous]
+public class EditCommentEndpoint : RequestEndpoint<EditCommentRequest>
+{
+    public override Task HandleAsync(EditCommentRequest req, CancellationToken ct)
+    {
+        return HandleCommandAsync(new EditCommentCommand(req.CommentId, req.Text), ct);
+    }
+}
