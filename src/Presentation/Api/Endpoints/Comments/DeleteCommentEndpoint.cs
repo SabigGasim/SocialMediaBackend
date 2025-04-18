22911c3e2809e447bd1b3d3ca2aaf6df33c8cf70
unknown <sabgasim@gmail.com>
2025-04-18 04:46:13 +0300

    feat(api): add DeleteCommentEndpoint

---
+++ src/Presentation/Api/Endpoints/Comments/DeleteCommentEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Application.Comments.DeleteComment;
+
+namespace SocialMediaBackend.Api.Endpoints.Comments;
+
+[HttpDelete(ApiEndpoints.Posts.DeleteComment), AllowAnonymous]
+public class DeleteCommentEndpoint : RequestEndpoint<DeleteCommentRequest>
+{
+    public override Task HandleAsync(DeleteCommentRequest req, CancellationToken ct)
+    {
+        return HandleCommandAsync(new DeleteCommentCommand(req.CommentId, req.PostId), ct);
+    }
+}
