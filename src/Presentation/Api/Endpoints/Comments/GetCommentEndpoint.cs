e6a5694927fb888ecc98a856673c6d476734307f
unknown <sabgasim@gmail.com>
2025-04-18 04:42:03 +0300

    feat(api): add GetCommentEndpoint

---
+++ src/Presentation/Api/Endpoints/Comments/GetCommentEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Application.Comments.GetComment;
+
+namespace SocialMediaBackend.Api.Endpoints.Comments;
+
+[HttpGet(ApiEndpoints.Comments.Get), AllowAnonymous]
+public class GetCommentEndpoint : RequestEndpoint<GetCommentRequest, GetCommentResponse>
+{
+    public override Task HandleAsync(GetCommentRequest req, CancellationToken ct)
+    {
+        return HandleRequestAsync(new GetCommentQuery(req.CommentId), ct);
+    }
+}
