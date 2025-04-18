64fc0066e34cc0aea8500c4b86e93e93c49d74d7
unknown <sabgasim@gmail.com>
2025-04-18 04:43:35 +0300

    feat(api): add GetAllPostCommentsEndpoint

---
+++ src/Presentation/Api/Endpoints/Comments/GetAllPostCommentsEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Application.Comments.GetAllPostComments;
+
+namespace SocialMediaBackend.Api.Endpoints.Comments;
+
+[HttpGet(ApiEndpoints.Posts.GetAllPostComments), AllowAnonymous]
+public class GetAllPostCommentsEndpoint : RequestEndpoint<GetAllPostCommentsRequest, GetAllPostCommentsResponse>
+{
+    public override Task HandleAsync(GetAllPostCommentsRequest req, CancellationToken ct)
+    {
+        return HandleRequestAsync(new GetAllPostCommentsQuery(req.PostId, req.Page, req.PageSize), ct);
+    }
+}
