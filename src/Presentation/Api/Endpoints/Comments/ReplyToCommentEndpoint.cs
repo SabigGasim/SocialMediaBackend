115288bdd1ab592bd6c46ac64bf13b540a2c4f7d
unknown <sabgasim@gmail.com>
2025-04-19 23:22:56 +0300

    feat(api): add ReplyToCommentEndpoint

---
+++ src/Presentation/Api/Endpoints/Comments/ReplyToCommentEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Application.Comments.ReplyToComment;
+
+namespace SocialMediaBackend.Api.Endpoints.Comments;
+
+[HttpPost(ApiEndpoints.Comments.Reply), AllowAnonymous]
+public class ReplyToCommentEndpoint : RequestEndpoint<ReplyToCommentRequest, ReplyToCommentResponse>
+{
+    public override Task HandleAsync(ReplyToCommentRequest req, CancellationToken ct)
+    {
+        return HandleRequestAsync(new ReplyToCommentCommand(req.ParentId, req.UserId, req.Text), ct);
+    }
+}
