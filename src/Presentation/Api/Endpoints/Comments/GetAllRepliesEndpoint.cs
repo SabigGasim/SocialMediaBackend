e903209c626c2ceb53e47392f61583d0dcf66acc
unknown <sabgasim@gmail.com>
2025-04-19 23:24:16 +0300

    feat(api): add GetAllRepliesEndpoint

---
+++ src/Presentation/Api/Endpoints/Comments/GetAllRepliesEndpoint.cs
@@ -0,0 +1,15 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Application.Comments.GetAllReplies;
+
+namespace SocialMediaBackend.Api.Endpoints.Comments;
+
+[HttpGet(ApiEndpoints.Comments.GetReplies), AllowAnonymous]
+public class GetAllRepliesEndpoint : RequestEndpoint<GetAllRepliesRequest, GetAllRepliesResponse>
+{
+    public override Task HandleAsync(GetAllRepliesRequest req, CancellationToken ct)
+    {
+        return HandleRequestAsync(new GetAllRepliesQuery(req.ParentId, req.Page, req.PageSize), ct);
+    }
+}
