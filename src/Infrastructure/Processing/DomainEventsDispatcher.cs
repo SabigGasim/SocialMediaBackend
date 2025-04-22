065794f8aead9068a70674f74b5d7eb970389aa1
unknown <sabgasim@gmail.com>
2025-04-22 20:09:15 +0300

    feat(infra): implement DomainEventsDispatcher

---
+++ src/Infrastructure/Processing/DomainEventsDispatcher.cs
@@ -0,0 +1,23 @@
+﻿using Mediator;
+using SocialMediaBackend.Domain.Common;
+using SocialMediaBackend.Infrastructure.Helpers;
+
+namespace SocialMediaBackend.Infrastructure.Processing;
+
+public class DomainEventsDispatcher(IMediator mediator) : IDomainEventsDispatcher
+{
+    private readonly IMediator _mediator = mediator;
+
+    public ValueTask DispatchAsync<TDomainEvent>(TDomainEvent domainEvent, CancellationToken ct = default)
+        where TDomainEvent : IDomainEvent
+    {
+        return _mediator.Publish(domainEvent, ct);
+    }
+
+    public ValueTask DispatchAsync(IEnumerable<IDomainEvent> domainEvents, CancellationToken ct = default)
+    {
+        var tasks = domainEvents.Select(x => _mediator.Publish(x, ct));
+
+        return ValueTaskHelper.WhenAll(tasks);
+    }
+}
---
+++ src/Infrastructure/Processing/IDomainEventsDispatcher.cs
@@ -0,0 +1,10 @@
+﻿using SocialMediaBackend.Domain.Common;
+
+namespace SocialMediaBackend.Infrastructure.Processing;
+public interface IDomainEventsDispatcher
+{
+    ValueTask DispatchAsync<TDomainEvent>(TDomainEvent domainEvent, CancellationToken ct = default)
+        where TDomainEvent : IDomainEvent;
+
+    ValueTask DispatchAsync(IEnumerable<IDomainEvent> domainEvents, CancellationToken ct = default);
+}
