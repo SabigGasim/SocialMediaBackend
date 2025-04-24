10e3d08ebacbe102c8b6607ab64ce926184d8a7f
unknown <sabgasim@gmail.com>
2025-04-24 07:56:37 +0300

    refactor(app): add IOptionalUserRequest & IUserRequestBase

--- src/Application/Abstractions/Requests/IUserRequest.cs
+++ src/Application/Abstractions/Requests/IUserRequest.cs
@@ -1,9 +1,19 @@
 ﻿namespace SocialMediaBackend.Application.Abstractions.Requests;
+public interface IUserRequestBase<TRequest> : IRequest
+{
+    TRequest WithUserId(Guid userId);
+    TRequest AndAdminRole(bool isAdmin);
+}
 
-public interface IUserRequest<TRequest> : IRequest
+public interface IUserRequest<TRequest> : IUserRequestBase<TRequest>
 {
     Guid UserId { get; }
     bool IsAdmin { get; }
-    TRequest WithUserId(Guid userId);
-    TRequest AndAdminRole(bool isAdmin);
+}
+
+
+public interface IOptionalUserRequest<TRequest> : IUserRequestBase<TRequest>
+{
+    Guid? UserId { get; }
+    bool IsAdmin { get; }
 }
