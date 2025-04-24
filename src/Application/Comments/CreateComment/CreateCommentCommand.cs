3ca9c33093dc8a37c026f9d9d0d66ec9e57d4408
unknown <sabgasim@gmail.com>
2025-04-25 02:45:36 +0300

    feat(app, api): add auth support for CreateCommentEndpiont

--- src/Application/Comments/CreateComment/CreateCommentCommand.cs
+++ src/Application/Comments/CreateComment/CreateCommentCommand.cs
@@ -1,12 +1,28 @@
-﻿using SocialMediaBackend.Application.Abstractions.Requests.Commands;
+﻿using SocialMediaBackend.Application.Abstractions.Requests;
+using SocialMediaBackend.Application.Abstractions.Requests.Commands;
 using SocialMediaBackend.Domain.Posts;
 using SocialMediaBackend.Domain.Users;
 
 namespace SocialMediaBackend.Application.Comments.CreateComment;
 
-public class CreateCommentCommand(Guid userId, Guid postId, string text) : CommandBase<CreateCommentResponse>
+public class CreateCommentCommand(Guid postId, string text) 
+    : CommandBase<CreateCommentResponse>, IUserRequest<CreateCommentCommand>
 {
-    public UserId UserId { get; } = new(userId);
+    public UserId UserId { get; private set; } = default!;
     public PostId PostId { get; } = new(postId);
     public string Text { get; } = text;
+
+    public bool IsAdmin { get; private set; }
+
+    public CreateCommentCommand AndAdminRole(bool isAdmin)
+    {
+        IsAdmin = isAdmin;
+        return this;
+    }
+
+    public CreateCommentCommand WithUserId(Guid userId)
+    {
+        UserId = new(userId);
+        return this;
+    }
 }
--- src/Presentation/Api/Endpoints/Comments/CreateCommentEndpoint.cs
+++ src/Presentation/Api/Endpoints/Comments/CreateCommentEndpoint.cs
@@ -9,7 +9,6 @@ public class CreateCommentEndpoint : RequestEndpoint<CreateCommentRequest, Creat
 {
     public override Task HandleAsync(CreateCommentRequest req, CancellationToken ct)
     {
-        var userId = Guid.Parse("de18fec2-aabe-403e-9a5c-ee219203cf5e");
-        return HandleRequestAsync(new CreateCommentCommand(userId, req.PostId, req.Text), ct);
+        return HandleRequestAsync(new CreateCommentCommand(req.PostId, req.Text), ct);
     }
 }
