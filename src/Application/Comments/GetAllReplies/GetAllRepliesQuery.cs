c923d90d6d57d5353dd8644a23922d563767ce5b
unknown <sabgasim@gmail.com>
2025-04-25 02:47:03 +0300

    feat(app): add auth support for GetAllRepliesQueryHandler

--- src/Application/Comments/GetAllReplies/GetAllRepliesQuery.cs
+++ src/Application/Comments/GetAllReplies/GetAllRepliesQuery.cs
@@ -1,11 +1,30 @@
-﻿using SocialMediaBackend.Application.Abstractions.Requests.Queries;
+﻿using SocialMediaBackend.Application.Abstractions.Requests;
+using SocialMediaBackend.Application.Abstractions.Requests.Queries;
 using SocialMediaBackend.Domain.Comments;
+using SocialMediaBackend.Domain.Users;
 
 namespace SocialMediaBackend.Application.Comments.GetAllReplies;
 
-public class GetAllRepliesQuery(Guid parentId, int page, int pageSize) : QueryBase<GetAllRepliesResponse>
+public class GetAllRepliesQuery(Guid parentId, int page, int pageSize) 
+    : QueryBase<GetAllRepliesResponse>, IUserRequest<GetAllRepliesQuery>
 {
     public CommentId ParentId { get; } = new(parentId);
     public int Page { get; } = page;
     public int PageSize { get; } = pageSize;
+
+    public UserId UserId { get; private set; } = default!;
+
+    public bool IsAdmin {  get; private set; }
+
+    public GetAllRepliesQuery AndAdminRole(bool isAdmin)
+    {
+        IsAdmin = isAdmin;
+        return this;
+    }
+
+    public GetAllRepliesQuery WithUserId(Guid userId)
+    {
+        UserId = new(userId);
+        return this;
+    }
 }
--- src/Application/Comments/GetAllReplies/GetAllRepliesQueryHandler.cs
+++ src/Application/Comments/GetAllReplies/GetAllRepliesQueryHandler.cs
@@ -1,16 +1,21 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SocialMediaBackend.Application.Abstractions;
 using SocialMediaBackend.Application.Abstractions.Requests;
 using SocialMediaBackend.Application.Abstractions.Requests.Queries;
 using SocialMediaBackend.Application.Common;
 using SocialMediaBackend.Application.Mappings;
+using SocialMediaBackend.Domain.Comments;
 using SocialMediaBackend.Infrastructure.Data;
 
 namespace SocialMediaBackend.Application.Comments.GetAllReplies;
 
-public class GetAllRepliesQueryHandler(ApplicationDbContext context)
+public class GetAllRepliesQueryHandler(
+    ApplicationDbContext context,
+    IAuthorizationHandler<Comment, CommentId> authorizationHandler)
     : IQueryHandler<GetAllRepliesQuery, GetAllRepliesResponse>
 {
     private readonly ApplicationDbContext _context = context;
+    private readonly IAuthorizationHandler<Comment, CommentId> _authorizationHandler = authorizationHandler;
 
     public async Task<HandlerResponse<GetAllRepliesResponse>> ExecuteAsync(GetAllRepliesQuery query, CancellationToken ct)
     {
@@ -19,16 +24,29 @@ public class GetAllRepliesQueryHandler(ApplicationDbContext context)
         if (comment is null)
             return ("Comment with the given Id was not found", HandlerResponseStatus.NotFound);
 
-        var replies = await _context.Comments
+        var authorized = await _authorizationHandler
+            .AuthorizeAsync(query.UserId, query.ParentId, new(query.IsAdmin), ct);
+
+        if (!authorized)
+            return ("The author limits who can view their comments", HandlerResponseStatus.Unauthorized, query.ParentId);
+
+        var queryable = _context.Comments
             .AsNoTracking()
             .Include(x => x.User)
-            .Where(x => x.ParentCommentId == query.ParentId)
+            .Where(x => x.ParentCommentId == query.ParentId);
+
+        queryable = _authorizationHandler
+            .AuthorizeQueryable(queryable, query.UserId, new(query.IsAdmin));
+
+        var replies = await queryable
             .Skip((query.Page - 1) * query.PageSize)
             .Take(query.PageSize)
             .ToListAsync(ct);
 
+        var count = await queryable.CountAsync(ct);
+
         var response = replies
-            .MapToGetRepliesResponse(query.ParentId, query.Page, query.PageSize, comment.RepliesCount);
+            .MapToGetRepliesResponse(query.ParentId, query.Page, query.PageSize, count);
 
         return (response, HandlerResponseStatus.OK);
     }
