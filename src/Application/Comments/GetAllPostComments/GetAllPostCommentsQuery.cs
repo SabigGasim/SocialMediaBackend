1b8af94e89aaef2fae369ed31024b116aef8a84d
unknown <sabgasim@gmail.com>
2025-04-25 02:46:31 +0300

    feat(app): add auth support for GetAllPostCommentsQueryHandler

--- src/Application/Comments/GetAllPostComments/GetAllPostCommentsQuery.cs
+++ src/Application/Comments/GetAllPostComments/GetAllPostCommentsQuery.cs
@@ -1,11 +1,30 @@
-﻿using SocialMediaBackend.Application.Abstractions.Requests.Queries;
+﻿using SocialMediaBackend.Application.Abstractions.Requests;
+using SocialMediaBackend.Application.Abstractions.Requests.Queries;
 using SocialMediaBackend.Domain.Posts;
+using SocialMediaBackend.Domain.Users;
 
 namespace SocialMediaBackend.Application.Comments.GetAllPostComments;
 
-public class GetAllPostCommentsQuery(Guid postId, int page, int pageSize) : QueryBase<GetAllPostCommentsResponse>
+public class GetAllPostCommentsQuery(Guid postId, int page, int pageSize)
+    : QueryBase<GetAllPostCommentsResponse>, IOptionalUserRequest<GetAllPostCommentsQuery>
 {
     public PostId PostId { get; } = new(postId);
     public int Page { get; } = page;
     public int PageSize { get; } = pageSize;
+
+    public UserId? UserId { get; private set; }
+
+    public bool IsAdmin { get; private set; }
+
+    public GetAllPostCommentsQuery AndAdminRole(bool isAdmin)
+    {
+        IsAdmin = isAdmin;
+        return this;
+    }
+
+    public GetAllPostCommentsQuery WithUserId(Guid userId)
+    {
+        UserId = new(userId);
+        return this;
+    }
 }
--- src/Application/Comments/GetAllPostComments/GetAllPostCommentsQueryHandler.cs
+++ src/Application/Comments/GetAllPostComments/GetAllPostCommentsQueryHandler.cs
@@ -1,16 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SocialMediaBackend.Application.Abstractions.Requests;
 using SocialMediaBackend.Application.Abstractions.Requests.Queries;
+using SocialMediaBackend.Application.Auth;
 using SocialMediaBackend.Application.Common;
 using SocialMediaBackend.Application.Mappings;
+using SocialMediaBackend.Domain.Comments;
+using SocialMediaBackend.Domain.Posts;
 using SocialMediaBackend.Infrastructure.Data;
 
 namespace SocialMediaBackend.Application.Comments.GetAllPostComments;
 
-public class GetAllPostCommentsQueryHandler(ApplicationDbContext context) 
+public class GetAllPostCommentsQueryHandler(
+    ApplicationDbContext context,
+    IAuthorizationService authorizationService
+    ) 
     : IQueryHandler<GetAllPostCommentsQuery, GetAllPostCommentsResponse>
 {
     private readonly ApplicationDbContext _context = context;
+    private readonly IAuthorizationService _authorizationService = authorizationService;
 
     public async Task<HandlerResponse<GetAllPostCommentsResponse>> ExecuteAsync(GetAllPostCommentsQuery query, CancellationToken ct)
     {
@@ -18,11 +25,21 @@ public class GetAllPostCommentsQueryHandler(ApplicationDbContext context)
         if (!postExists)
             return ("Post with the given Id was not found", HandlerResponseStatus.NotFound);
 
+        var authorized = await _authorizationService
+            .AuthorizeAsync<Post, PostId>(query.UserId, query.PostId, new(query.IsAdmin), ct);
+
+        if (!authorized)
+            return ("The author limits who can view there posts", HandlerResponseStatus.Unauthorized, query.PostId);
+
         var sqlQuery = _context.Comments
             .AsNoTracking()
             .Where(c => c.ParentCommentId == null)
             .OrderByDescending(c => c.LikesCount)
-            .OrderByDescending(c => c.Created);
+            .OrderByDescending(c => c.Created)
+            .AsQueryable();
+
+        sqlQuery = _authorizationService
+            .AuthorizeQueryable<Comment, CommentId>(sqlQuery, query.UserId, new(query.IsAdmin));
 
         var totalCount = await sqlQuery.CountAsync(ct);
 
