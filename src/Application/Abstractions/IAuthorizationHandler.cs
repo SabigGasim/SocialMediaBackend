cdf6d3142629c71c9f89ce34353554f336d3ad40
unknown <sabgasim@gmail.com>
2025-04-25 02:35:58 +0300

    refactor(app): seperate IAuthorizationService & IAuthorizationHandler

--- src/Application/Abstractions/IAuthorizationHandler.cs
+++ src/Application/Abstractions/IAuthorizationHandler.cs
@@ -1,16 +1,22 @@
-﻿using SocialMediaBackend.Application.Auth;
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMediaBackend.Application.Auth;
 using SocialMediaBackend.Domain.Common;
 using SocialMediaBackend.Domain.Users;
 
 namespace SocialMediaBackend.Application.Abstractions;
 
-public interface IAuthorizationService<TResource, TResourceId>
+public interface IAuthorizationHandler<TResource, TResourceId>
     where TResource : Entity<TResourceId>
 {
     IQueryable<TResource> AuthorizeQueryable(
         IQueryable<TResource> resource,
         UserId? userId,
         TResourceId resourceId,
+        AuthOptions options);
+    
+    IQueryable<TResource> AuthorizeQueryable(
+        IQueryable<TResource> resource,
+        UserId? userId,
         AuthOptions options);
 
     Task<bool> AuthorizeAsync(
---
+++ src/Application/Auth/IAuthorizationService.cs
@@ -0,0 +1,27 @@
+﻿using SocialMediaBackend.Domain.Common;
+using SocialMediaBackend.Domain.Users;
+
+namespace SocialMediaBackend.Application.Auth;
+
+public interface IAuthorizationService
+{
+    IQueryable<TResource> AuthorizeQueryable<TResource, TResourceId>(
+        IQueryable<TResource> resource,
+        UserId? userId,
+        TResourceId resourceId,
+        AuthOptions options)
+        where TResource : Entity<TResourceId>;
+
+    IQueryable<TResource> AuthorizeQueryable<TResource, TResourceId>(
+        IQueryable<TResource> resource,
+        UserId? userId,
+        AuthOptions options)
+        where TResource : Entity<TResourceId>;
+
+    Task<bool> AuthorizeAsync<TResource, TResourceId>(
+        UserId? userId,
+        TResourceId resourceId,
+        AuthOptions options,
+        CancellationToken ct = default)
+        where TResource : Entity<TResourceId>;
+}
