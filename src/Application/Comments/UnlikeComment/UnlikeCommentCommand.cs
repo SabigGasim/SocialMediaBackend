41a409d074322e5a52d1c53cfb166656ee3419d7
unknown <sabgasim@gmail.com>
2025-04-25 13:33:49 +0300

    feat(app): implement unlike comment feature

---
+++ src/Application/Comments/UnlikeComment/UnlikeCommentCommand.cs
@@ -0,0 +1,25 @@
+﻿using SocialMediaBackend.Application.Abstractions.Requests;
+using SocialMediaBackend.Application.Abstractions.Requests.Commands;
+using SocialMediaBackend.Domain.Comments;
+using SocialMediaBackend.Domain.Users;
+
+namespace SocialMediaBackend.Application.Comments.UnlikeComment;
+public class UnlikeCommentCommand(Guid commentId) : CommandBase, IUserRequest<UnlikeCommentCommand>
+{
+    public CommentId CommentId { get; } = new(commentId);
+
+    public UserId UserId { get; private set; } = default!;
+    public bool IsAdmin { get; private set; }
+
+    public UnlikeCommentCommand AndAdminRole(bool isAdmin)
+    {
+        IsAdmin = isAdmin;
+        return this;
+    }
+
+    public UnlikeCommentCommand WithUserId(Guid userId)
+    {
+        UserId = new(userId);
+        return this;
+    }
+}
---
+++ src/Application/Comments/UnlikeComment/UnlikeCommentCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMediaBackend.Application.Abstractions.Requests;
+using SocialMediaBackend.Application.Abstractions.Requests.Commands;
+using SocialMediaBackend.Application.Common;
+using SocialMediaBackend.Infrastructure.Data;
+
+namespace SocialMediaBackend.Application.Comments.UnlikeComment;
+
+public class UnlikeCommentCommandHandler(ApplicationDbContext context) : ICommandHandler<UnlikeCommentCommand>
+{
+    private readonly ApplicationDbContext _context = context;
+
+    public async Task<HandlerResponse> ExecuteAsync(UnlikeCommentCommand command, CancellationToken ct)
+    {
+        var comment = await _context.Comments
+            .Where(x => x.Id == command.CommentId)
+            .Include(x => x.Likes.Where(x => x.UserId == command.UserId))
+            .FirstOrDefaultAsync(ct);
+
+        if (comment is null)
+            return ("Comment with the given Id was not found", HandlerResponseStatus.NotFound, command.CommentId);
+
+        var removed = comment.RemoveLike(command.UserId);
+        if (!removed)
+            return ("User with the given Id didn't like this comment", HandlerResponseStatus.Conflict, command.UserId);
+
+        await _context.SaveChangesAsync(ct);
+
+        return HandlerResponseStatus.Deleted;
+    }
+}
