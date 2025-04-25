169c67c5697cb279a0364462fe5156a027414e77
unknown <sabgasim@gmail.com>
2025-04-25 07:22:58 +0300

    feat(app): add auth support for CreateCommentCommand

--- src/Application/Comments/CreateComment/CreateCommentHandler.cs
+++ src/Application/Comments/CreateComment/CreateCommentHandler.cs
@@ -1,30 +1,32 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using SocialMediaBackend.Application.Abstractions;
 using SocialMediaBackend.Application.Abstractions.Requests;
 using SocialMediaBackend.Application.Abstractions.Requests.Commands;
 using SocialMediaBackend.Application.Common;
 using SocialMediaBackend.Application.Mappings;
+using SocialMediaBackend.Domain.Posts;
 using SocialMediaBackend.Infrastructure.Data;
 
 namespace SocialMediaBackend.Application.Comments.CreateComment;
 
-public class CreateCommentHandler(ApplicationDbContext context)
+public class CreateCommentHandler(
+    ApplicationDbContext context,
+    IAuthorizationHandler<Post, PostId> authorizationHandler)
     : ICommandHandler<CreateCommentCommand, CreateCommentResponse>
 {
     private readonly ApplicationDbContext _context = context;
+    private readonly IAuthorizationHandler<Post, PostId> _authorizationHandler = authorizationHandler;
 
     public async Task<HandlerResponse<CreateCommentResponse>> ExecuteAsync(CreateCommentCommand command, CancellationToken ct)
     {
-        var userExists = await _context.Users
-            .AsNoTracking()
-            .AnyAsync(x => x.Id == command.UserId, ct);
-
-        if (!userExists)
-            return ("Invalid user Id", HandlerResponseStatus.BadRequest);
-
-        var post = await _context.Posts.FindAsync([command.PostId], cancellationToken: ct);
-
+        var post = await _context.Posts.FindAsync([command.PostId], ct);
         if (post is null)
             return ("Post with the given Id was not found", HandlerResponseStatus.NotFound, command.PostId);
+
+        var authorized = await _authorizationHandler
+            .AuthorizeAsync(command.UserId, command.PostId, new(command.IsAdmin), ct);
+
+        if (!authorized)
+            return ("The author limits who can view their posts", HandlerResponseStatus.Unauthorized, post.UserId);
 
         var comment = post.AddComment(command.Text, command.UserId);
 
