b099e11df939c0fdcfec4ca7cf571eb75f34b39e
unknown <sabgasim@gmail.com>
2025-04-25 07:21:20 +0300

    feat(app): add auth support for EditCommentCommand

--- src/Application/Comments/EditComment/EditCommentCommand.cs
+++ src/Application/Comments/EditComment/EditCommentCommand.cs
@@ -1,10 +1,28 @@
-﻿using SocialMediaBackend.Application.Abstractions.Requests.Commands;
+﻿using SocialMediaBackend.Application.Abstractions.Requests;
+using SocialMediaBackend.Application.Abstractions.Requests.Commands;
 using SocialMediaBackend.Domain.Comments;
+using SocialMediaBackend.Domain.Users;
 
 namespace SocialMediaBackend.Application.Comments.EditComment;
 
-public class EditCommentCommand(Guid commentId, string text) : CommandBase
+public class EditCommentCommand(Guid commentId, string text) : CommandBase, IUserRequest<EditCommentCommand>
 {
     public CommentId CommentId { get; } = new(commentId);
     public string Text { get; } = text;
+
+    public UserId UserId { get; private set; } = default!;
+
+    public bool IsAdmin { get; private set; }
+
+    public EditCommentCommand AndAdminRole(bool isAdmin)
+    {
+        IsAdmin = isAdmin;
+        return this;
+    }
+
+    public EditCommentCommand WithUserId(Guid userId)
+    {
+        UserId = new(userId);
+        return this;
+    }
 }
--- src/Application/Comments/EditComment/EditCommentCommandHandler.cs
+++ src/Application/Comments/EditComment/EditCommentCommandHandler.cs
@@ -1,13 +1,18 @@
-﻿using SocialMediaBackend.Application.Abstractions.Requests;
+﻿using SocialMediaBackend.Application.Abstractions;
+using SocialMediaBackend.Application.Abstractions.Requests;
 using SocialMediaBackend.Application.Abstractions.Requests.Commands;
 using SocialMediaBackend.Application.Common;
+using SocialMediaBackend.Domain.Comments;
 using SocialMediaBackend.Infrastructure.Data;
 
 namespace SocialMediaBackend.Application.Comments.EditComment;
 
-public class EditCommentCommandHandler(ApplicationDbContext context) : ICommandHandler<EditCommentCommand>
+public class EditCommentCommandHandler(
+    ApplicationDbContext context,
+    IAuthorizationHandler<Comment, CommentId> authorizationHandler) : ICommandHandler<EditCommentCommand>
 {
     private readonly ApplicationDbContext _context = context;
+    private readonly IAuthorizationHandler<Comment, CommentId> _authorizationHandler = authorizationHandler;
 
     public async Task<HandlerResponse> ExecuteAsync(EditCommentCommand command, CancellationToken ct)
     {
@@ -15,6 +20,12 @@ public class EditCommentCommandHandler(ApplicationDbContext context) : ICommandH
 
         if(comment is null)
             return ("Comment with the given Id was not found", HandlerResponseStatus.NotFound, command.CommentId);
+
+        var authorized = await _authorizationHandler
+            .IsAdminOrResourceOwnerAsync(command.UserId, command.CommentId, new(command.IsAdmin), ct);
+
+        if (!authorized)
+            return ("Forbidden", HandlerResponseStatus.Unauthorized);
 
         comment.Edit(command.Text);
 
