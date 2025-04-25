ef32d067f7733c2149636fd1678529813e90d1b1
unknown <sabgasim@gmail.com>
2025-04-25 07:11:51 +0300

    feat(app): add auth support for DeleteCommentCommandHandler

--- src/Application/Comments/DeleteComment/DeleteCommentCommand.cs
+++ src/Application/Comments/DeleteComment/DeleteCommentCommand.cs
@@ -1,11 +1,29 @@
-﻿using SocialMediaBackend.Application.Abstractions.Requests.Commands;
+﻿using SocialMediaBackend.Application.Abstractions.Requests;
+using SocialMediaBackend.Application.Abstractions.Requests.Commands;
 using SocialMediaBackend.Domain.Comments;
 using SocialMediaBackend.Domain.Posts;
+using SocialMediaBackend.Domain.Users;
 
 namespace SocialMediaBackend.Application.Comments.DeleteComment;
 
-public class DeleteCommentCommand(Guid commentId, Guid postId) : CommandBase
+public class DeleteCommentCommand(Guid commentId, Guid postId) : CommandBase, IUserRequest<DeleteCommentCommand>
 {
     public CommentId CommentId { get; } = new(commentId);
     public PostId PostId { get; } = new(postId);
+
+    public UserId UserId { get; private set; } = default!;
+
+    public bool IsAdmin { get; private set; }
+
+    public DeleteCommentCommand AndAdminRole(bool isAdmin)
+    {
+        IsAdmin = isAdmin;
+        return this;
+    }
+
+    public DeleteCommentCommand WithUserId(Guid userId)
+    {
+        UserId = new(userId);
+        return this;
+    }
 }
---
+++ src/Application/Comments/DeleteComment/DeleteCommentCommandHandler.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMediaBackend.Application.Abstractions;
+using SocialMediaBackend.Application.Abstractions.Requests;
+using SocialMediaBackend.Application.Abstractions.Requests.Commands;
+using SocialMediaBackend.Application.Common;
+using SocialMediaBackend.Domain.Comments;
+using SocialMediaBackend.Domain.Common;
+using SocialMediaBackend.Infrastructure.Data;
+
+namespace SocialMediaBackend.Application.Comments.DeleteComment;
+
+public class DeleteCommentCommandHandler(
+    ApplicationDbContext context,
+    IUnitOfWork unitOfWork,
+    IAuthorizationHandler<Comment, CommentId> authorizationHandler) : ICommandHandler<DeleteCommentCommand>
+{
+    private readonly ApplicationDbContext _context = context;
+    private readonly IUnitOfWork _unitOfWork = unitOfWork;
+    private readonly IAuthorizationHandler<Comment, CommentId> _authorizationHandler = authorizationHandler;
+
+    public async Task<HandlerResponse> ExecuteAsync(DeleteCommentCommand command, CancellationToken ct)
+    {
+        var authorized = await _authorizationHandler
+            .IsAdminOrResourceOwnerAsync(command.UserId, command.CommentId, new(command.IsAdmin), ct);
+
+        if (!authorized)
+        {
+            return ("Forbidden", HandlerResponseStatus.Unauthorized);
+        }
+
+        var query = _context.Posts
+                .Include(x => x.Comments.Where(p => p.Id == command.CommentId))
+                .Where(x => x.Id == command.PostId)
+                .AsQueryable();
+
+        var post = await query.FirstOrDefaultAsync(ct);
+        if (post is null)
+        {
+            return ("Post with the given Id was not found", HandlerResponseStatus.NotFound, command.PostId);
+        }
+
+        var removed = post.RemoveComment(command.CommentId);
+        if (!removed)
+        {
+            return ("Comment with the given Id was not found", HandlerResponseStatus.NotFound, command.CommentId);
+        }
+
+        await _unitOfWork.CommitAsync(ct);
+
+        return HandlerResponseStatus.NoContent;
+    }
+}
