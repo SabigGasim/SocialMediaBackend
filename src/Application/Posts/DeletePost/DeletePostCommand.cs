8354aa1c43f5683354d5b9246e11fa2133ed93b7
unknown <sabgasim@gmail.com>
2025-04-25 07:22:07 +0300

    feat(app): add auth support for DeletePostCommand

--- src/Application/Posts/DeletePost/DeletePostCommand.cs
+++ src/Application/Posts/DeletePost/DeletePostCommand.cs
@@ -1,9 +1,27 @@
-﻿using SocialMediaBackend.Application.Abstractions.Requests.Commands;
+﻿using SocialMediaBackend.Application.Abstractions.Requests;
+using SocialMediaBackend.Application.Abstractions.Requests.Commands;
 using SocialMediaBackend.Domain.Posts;
+using SocialMediaBackend.Domain.Users;
 
 namespace SocialMediaBackend.Application.Posts.DeletePost;
 
-public class DeletePostCommand(Guid postId) : CommandBase
+public class DeletePostCommand(Guid postId) : CommandBase, IUserRequest<DeletePostCommand>
 {
     public PostId PostId { get; } = new(postId);
+
+    public UserId UserId { get; private set; } = default!;
+
+    public bool IsAdmin { get; private set; }
+
+    public DeletePostCommand AndAdminRole(bool isAdmin)
+    {
+        IsAdmin = isAdmin;
+        return this;
+    }
+
+    public DeletePostCommand WithUserId(Guid userId)
+    {
+        UserId = new(userId);
+        return this;
+    }
 }
--- src/Application/Posts/DeletePost/DeletePostCommandHandler.cs
+++ src/Application/Posts/DeletePost/DeletePostCommandHandler.cs
@@ -1,31 +1,55 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using SocialMediaBackend.Application.Abstractions;
 using SocialMediaBackend.Application.Abstractions.Requests;
 using SocialMediaBackend.Application.Abstractions.Requests.Commands;
 using SocialMediaBackend.Application.Common;
+using SocialMediaBackend.Domain.Posts;
 using SocialMediaBackend.Infrastructure.Data;
 
 namespace SocialMediaBackend.Application.Posts.DeletePost;
 
-public class DeletePostCommandHandler(ApplicationDbContext context) : ICommandHandler<DeletePostCommand>
+public class DeletePostCommandHandler(
+    ApplicationDbContext context,
+    IAuthorizationHandler<Post, PostId> authorizationHandler) : ICommandHandler<DeletePostCommand>
 {
     private readonly ApplicationDbContext _context = context;
+    private readonly IAuthorizationHandler<Post, PostId> _authorizationHandler = authorizationHandler;
 
     public async Task<HandlerResponse> ExecuteAsync(DeletePostCommand command, CancellationToken ct)
-    {
-        //TODO: Get UserId from Authentication
+    {   
+        var authorized = await _authorizationHandler
+            .IsAdminOrResourceOwnerAsync(command.UserId, command.PostId, new(command.IsAdmin), ct);
 
-        var post = await _context.Posts
-            .Include(x => x.User)
-            .FirstOrDefaultAsync(x => x.Id == command.PostId, ct);
+        if (!authorized)
+        {
+            return ("Forbidden", HandlerResponseStatus.Unauthorized);
+        }
 
-        if (post is null)
+        var query = _context.Users
+                .Include(x => x.Posts.Where(p => p.Id == command.PostId))
+                .AsQueryable();
+
+        query = command.IsAdmin
+            ? query.Where(x => x.Id == command.UserId || x.Posts.Any(p => p.Id == command.PostId))
+            : query.Where(x => x.Id == command.UserId);
+
+        var user = await query.FirstOrDefaultAsync(ct);
+        if (user is null)
+        {
+            // This will only execute if the deleter is not the post owner
+            // rather, an admin. If the admin provided an Invalid PostId,
+            // The query will not return the user associated with it
+            // as it runs backwards.
+            // Horrible sql, but the P in EF Core stands for "Performance",
+            // so should we even care? Maybe...
             return ("Post with the given Id was not found", HandlerResponseStatus.NotFound, command.PostId);
-
-        var user = post.User;
+        }
 
         var removed = user.RemovePost(command.PostId);
         if (!removed)
-            return ("An internal error occurred", HandlerResponseStatus.InternalError);
+        {
+            return ("Post with the given Id was not found or is not associated with the given user", HandlerResponseStatus.NotFound, command.PostId);
+        }
 
         await _context.SaveChangesAsync(ct);
 
