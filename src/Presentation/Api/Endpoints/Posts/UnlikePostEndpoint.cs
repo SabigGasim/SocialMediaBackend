b4c0e851ffe7b63cc1abf2b359848da172eedfd3
unknown <sabgasim@gmail.com>
2025-04-25 14:29:51 +0300

    feat(api): add UnlikePostEndpiont

---
+++ src/Presentation/Api/Endpoints/Posts/UnlikePostEndpoint.cs
@@ -0,0 +1,19 @@
+﻿using FastEndpoints;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Application.Posts.UnlikePost;
+
+namespace SocialMediaBackend.Api.Endpoints.Posts;
+
+public class UnlikePostEndpoint : RequestEndpoint<UnlikePostRequest>
+{
+    public override void Configure()
+    {
+        Delete(ApiEndpoints.Posts.Unlike);
+        Description(x => x.Accepts<UnlikePostRequest>());
+    }
+
+    public override Task HandleAsync(UnlikePostRequest req, CancellationToken ct)
+    {
+        return HandleCommandAsync(new UnlikePostCommand(req.PostId), ct);
+    }
+}
