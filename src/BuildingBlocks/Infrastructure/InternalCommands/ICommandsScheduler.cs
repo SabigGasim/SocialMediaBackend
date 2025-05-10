ae3712f63bec6cbafa4cb7bd311a390a65c4af89
unknown <sabgasim@gmail.com>
2025-05-11 02:51:15 +0300

    feat(feed.infra): add CommandsScheduler

---
+++ src/BuildingBlocks/Infrastructure/InternalCommands/ICommandsScheduler.cs
@@ -0,0 +1,8 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+
+namespace SocialMediaBackend.BuildingBlocks.Infrastructure.InternalCommands;
+
+public interface ICommandsScheduler
+{
+    ValueTask EnqueueAsync<T>(ICommand<T> command);
+}
---
+++ src/Modules/Feed/Infrastructure/InternalCommands/CommandsScheduler.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using Newtonsoft.Json;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.BuildingBlocks.Infrastructure;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.InternalCommands;
+using SocialMediaBackend.Modules.Feed.Infrastructure.Data;
+
+namespace SocialMediaBackend.Modules.Feed.Infrastructure.InternalCommands;
+
+public class CommandsScheduler(IDbConnectionFactory connectionFactory) : ICommandsScheduler
+{
+    private readonly IDbConnectionFactory _connectionFactory = connectionFactory;
+
+    public async ValueTask EnqueueAsync<T>(ICommand<T> command)
+    {
+        using (var connection = await _connectionFactory.CreateAsync())
+        {
+            const string sqlInsert =
+                $"""
+                INSERT INTO {Schema.Feed}."InternalCommands" ("Id", "EnqueueDate" , "Type", "Data")
+                VALUES (@Id, @EnqueueDate, @Type, @Data)
+                """;
+
+            await connection.ExecuteAsync(sqlInsert, new
+            {
+                command.Id,
+                EnqueueDate = TimeProvider.System.GetUtcNow(),
+                Type = $"{command.GetType().FullName}, {command.GetType().Assembly.FullName}",
+                Data = JsonConvert.SerializeObject(command)
+            });
+        }
+    }
+}
+
