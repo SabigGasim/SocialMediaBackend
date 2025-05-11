f3a0730d740a59a284fc5f1ce9ae47529ff86bba
unknown <sabgasim@gmail.com>
2025-05-11 03:30:31 +0300

    feat(feed.infra): add QuartzStartup

---
+++ src/Modules/Feed/Infrastructure/Configuration/Quartz/QuartzStartup.cs
@@ -0,0 +1,32 @@
+﻿using Quartz;
+using SocialMediaBackend.Modules.Feed.Infrastructure.InternalCommands;
+
+namespace SocialMediaBackend.Modules.Feed.Infrastructure.Configuration.Quartz;
+
+public static class QuartzStartup
+{
+    public static async Task InitializeAsync()
+    {
+        var trigger = TriggerBuilder.Create()
+           .StartNow()
+           .WithSimpleSchedule(x => x
+               .WithIntervalInSeconds(5)
+               .RepeatForever())
+           .Build();
+
+        var job = JobBuilder.Create<ProcessInternalCommandsJob>()
+            .WithIdentity("ProcessInternalCommandsJob")
+            .Build();
+
+        var schedulerFactory = SchedulerBuilder.Create()
+            .UseInMemoryStore()
+            .UseDefaultThreadPool(1)
+            .Build();
+
+        var scheduler = await schedulerFactory.GetScheduler();
+
+        await scheduler.ScheduleJob(job, trigger);
+
+        await scheduler.Start();
+    }
+}
