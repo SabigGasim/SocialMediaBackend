f0a2fd84476fdb2dc5a1de8672a27624044e81e7
unknown <sabgasim@gmail.com>
2025-05-11 03:32:32 +0300

    feat(feed.infra): add FeedStartup (module Autofac integration done)

---
+++ src/Modules/Feed/Application/Configuration/FeedStartup.cs
@@ -0,0 +1,45 @@
+﻿using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SocialMediaBackend.Modules.Feed.Application.Configuration.Auth;
+using SocialMediaBackend.Modules.Feed.Application.Configuration.Mediator;
+using SocialMediaBackend.Modules.Feed.Infrastructure.Configuration;
+using SocialMediaBackend.Modules.Feed.Infrastructure.Configuration.Persistence;
+using SocialMediaBackend.Modules.Feed.Infrastructure.Configuration.Processing;
+using SocialMediaBackend.Modules.Feed.Infrastructure.Configuration.Quartz;
+
+namespace SocialMediaBackend.Modules.Feed.Application.Configuration;
+
+public static class FeedStartup
+{
+    public static async Task InitializeAsync(
+        IServiceCollection serviceCollection,
+        IConfiguration config,
+        IWebHostEnvironment env)
+    {
+        ConfigureCompositionRoot(serviceCollection, config);
+
+        await QuartzStartup.InitializeAsync();
+        await PersistenceStartup.InitializeAsync(env);
+    }
+
+    private static void ConfigureCompositionRoot(IServiceCollection serviceCollection, IConfiguration config)
+    {
+        var containerBuilder = new ContainerBuilder();
+
+        containerBuilder.Populate(serviceCollection);
+
+        containerBuilder.RegisterModule(new QuartzModule());
+        containerBuilder.RegisterModule(new PersistenceModule(config));
+        containerBuilder.RegisterModule(new CQRSModule());
+        containerBuilder.RegisterModule(new ProcessingModule());
+        containerBuilder.RegisterModule(new AuthModule());
+
+        var container = containerBuilder.Build();
+
+        FeedCompositionRoot.SetContainer(container);
+    }
+}
