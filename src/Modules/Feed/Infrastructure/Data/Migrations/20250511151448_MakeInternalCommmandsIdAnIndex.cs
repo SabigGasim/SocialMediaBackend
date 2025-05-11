265b78f08a50cf65d9e496c33c568f25b90b64ca
unknown <sabgasim@gmail.com>
2025-05-11 18:32:00 +0300

    feat(feed.infra): add MakeInternalCommmandsIdAnIndex migration

---
+++ src/Modules/Feed/Infrastructure/Data/Migrations/20250511151448_MakeInternalCommmandsIdAnIndex.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialMediaBackend.Modules.Feed.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MakeInternalCommmandsIdAnIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_InternalCommands_Id",
+                schema: "feed",
+                table: "InternalCommands",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_InternalCommands_Id",
+                schema: "feed",
+                table: "InternalCommands");
+        }
+    }
+}
