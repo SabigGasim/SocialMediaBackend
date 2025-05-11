a66310d3b4bd3685d6e8a4e38714522050d6fc04
unknown <sabgasim@gmail.com>
2025-05-11 03:33:53 +0300

    feat(users.infra): PersistenceStartup

---
+++ src/Modules/Users/Infrastructure/Configuration/Persistence/PersistenceStartup.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using SocialMediaBackend.Modules.Users.Infrastructure.Data;
+
+namespace SocialMediaBackend.Modules.Users.Infrastructure.Configuration.Persistence;
+public static class PersistenceStartup
+{
+    public static async Task InitializeAsync(IHostEnvironment env)
+    {
+        await using (var scope = UsersCompositionRoot.BeginLifetimeScope())
+        {
+            var context = scope.Resolve<UsersDbContext>();
+
+            await context.Database.MigrateAsync();
+            await context.Database.EnsureCreatedAsync();
+
+            if (env.IsDevelopment())
+            {
+                var schema = context.Database.GenerateCreateScript();
+                Console.WriteLine($"""
+                    ----------------------------------------------
+                    ------------ UsersDbContext Schema ------------
+                    ----------------------------------------------
+
+                    {schema}
+                    """);
+            }
+        }
+    }
+}
