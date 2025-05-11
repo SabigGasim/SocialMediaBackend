8b1a8909d9182ac6bec15937b23a990ad3003b44
unknown <sabgasim@gmail.com>
2025-05-11 03:13:14 +0300

    feat(users.infra): add UsersCompositionRoot in prep for Autofac container

---
+++ src/Modules/Users/Infrastructure/Configuration/UsersCompositionRoot.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+
+namespace SocialMediaBackend.Modules.Users.Infrastructure.Configuration;
+
+public static class UsersCompositionRoot
+{
+    private static IContainer _container = default!;
+
+    public static void SetContainer(IContainer container)
+    {
+        _container = container;
+    }
+
+    public static ILifetimeScope BeginLifetimeScope()
+    {
+        return _container.BeginLifetimeScope();
+    }
+}
