18c8e823c05d5ef457346414412ad3f65f43f75c
unknown <sabgasim@gmail.com>
2025-05-11 03:12:35 +0300

    feat(feed.infra): add FeedCompositionRoot in prep for Autofac container

---
+++ src/Modules/Feed/Infrastructure/Configuration/FeedCompositionRoot.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+
+namespace SocialMediaBackend.Modules.Feed.Infrastructure.Configuration;
+
+public static class FeedCompositionRoot
+{
+    private static ILifetimeScope _container = default!;
+
+    public static void SetContainer(ILifetimeScope container)
+    {
+        _container = container;
+    }
+
+    public static ILifetimeScope BeginLifetimeScope()
+    {
+        return _container.BeginLifetimeScope();
+    }
+}
