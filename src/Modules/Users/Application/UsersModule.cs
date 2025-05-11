1d01ba37dc74e89035063f7562c7df7897773e9c
unknown <sabgasim@gmail.com>
2025-05-11 03:09:55 +0300

    feat(users.app): ass IUsersModule contract

---
+++ src/Modules/Users/Application/UsersModule.cs
@@ -0,0 +1,50 @@
+﻿using Autofac;
+using SocialMediaBackend.BuildingBlocks.Application.Requests;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Queries;
+using SocialMediaBackend.Modules.Users.Application.Contracts;
+using SocialMediaBackend.Modules.Users.Infrastructure.Configuration;
+
+namespace SocialMediaBackend.Modules.Users.Application;
+
+internal class UsersModule : IUsersModule
+{
+    public async Task<HandlerResponse<TResult>> ExecuteCommandAsync<TCommand, TResult>(
+        TCommand command,
+        CancellationToken ct = default)
+        where TCommand : ICommand<HandlerResponse<TResult>>
+    {
+        using (var scope = UsersCompositionRoot.BeginLifetimeScope())
+        {
+            var handler = scope.Resolve<ICommandHandler<TCommand, TResult>>();
+
+            return await handler.ExecuteAsync(command, ct);
+        }
+    }
+
+    public async Task<HandlerResponse> ExecuteCommandAsync<TCommand>(
+        TCommand command,
+        CancellationToken ct = default) 
+        where TCommand : ICommand<HandlerResponse>
+    {
+        using (var scope = UsersCompositionRoot.BeginLifetimeScope())
+        {
+            var handler = scope.Resolve<ICommandHandler<TCommand>>();
+
+            return await handler.ExecuteAsync(command, ct);
+        }
+    }
+
+    public async Task<HandlerResponse<TResult>> ExecuteQueryAsync<TQuery, TResult>(
+        TQuery query,
+        CancellationToken ct = default)
+        where TQuery : IQuery<HandlerResponse<TResult>>
+    {
+        using (var scope = UsersCompositionRoot.BeginLifetimeScope())
+        {
+            var handler = scope.Resolve<IQueryHandler<TQuery, TResult>>();
+
+            return await handler.ExecuteAsync(query, ct);
+        }
+    }
+}
