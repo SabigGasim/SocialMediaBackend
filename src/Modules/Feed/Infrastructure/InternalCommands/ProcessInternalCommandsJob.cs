bbd429a8792603bcc3c78943f2b2a946f6bd288d
unknown <sabgasim@gmail.com>
2025-05-11 03:29:59 +0300

    feat(feed.infra): add ProcessInternalCommandsJob of quartz

---
+++ src/Modules/Feed/Infrastructure/InternalCommands/ProcessInternalCommandsJob.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Quartz;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.Modules.Feed.Infrastructure.Configuration;
+
+namespace SocialMediaBackend.Modules.Feed.Infrastructure.InternalCommands;
+
+[DisallowConcurrentExecution]
+public class ProcessInternalCommandsJob : IJob
+{
+    public async Task Execute(IJobExecutionContext context)
+    {
+        using (var scope = FeedCompositionRoot.BeginLifetimeScope())
+        {
+            var handler = scope.Resolve<ICommandHandler<ProcessInternalCommandsCommand>>();
+            await handler.ExecuteAsync(new ProcessInternalCommandsCommand(), default);
+        }
+    }
+}
