5c265b2ee392586e69b13a15fa768d1d9e151afe
unknown <sabgasim@gmail.com>
2025-05-11 03:33:12 +0300

    refactor(users.infra): move DbContext registeration to PersistenceModule

---
+++ src/Modules/Users/Infrastructure/Configuration/Persistence/PersistenceModule.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.Extensions.Configuration;
+using SocialMediaBackend.Modules.Users.Infrastructure.Data;
+
+namespace SocialMediaBackend.Modules.Users.Infrastructure.Configuration.Persistence;
+
+public class PersistenceModule(IConfiguration config) : Module
+{
+    private readonly IConfiguration _config = config;
+
+    protected override void Load(ContainerBuilder builder)
+    {
+        var connectionString = _config.GetConnectionString("PostgresConnection");
+
+        builder.Register(_ =>
+        {
+            var options = new DbContextOptionsBuilder<UsersDbContext>()
+                .UseNpgsql(
+                    connectionString,
+                    npgsqlOptions => npgsqlOptions.MigrationsHistoryTable(
+                        HistoryRepository.DefaultTableName,
+                        Schema.Users))
+                .Options;
+
+            return new UsersDbContext(options);
+        })
+            .AsSelf()
+            .As<DbContext>()
+            .InstancePerLifetimeScope();
+    }
+}
--- src/Modules/Users/Infrastructure/InfrastructureServiceCollectionExtensions.cs
+++ src/Modules/Users/Infrastructure/InfrastructureServiceCollectionExtensions.cs
@@ -1,30 +1,13 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Migrations;
-using Microsoft.Extensions.DependencyInjection;
-using SocialMediaBackend.BuildingBlocks.Infrastructure;
+﻿using Microsoft.Extensions.DependencyInjection;
 using SocialMediaBackend.Modules.Users.Domain.Users;
-using SocialMediaBackend.Modules.Users.Infrastructure.Data;
 using SocialMediaBackend.Modules.Users.Infrastructure.Domain.Users;
-using SocialMediaBackend.Modules.Users.Infrastructure.Processing;
 
 namespace SocialMediaBackend.Modules.Users.Infrastructure;
 
 public static class InfrastructureServiceCollectionExtensions
 {
-    public static IServiceCollection AddInfrastructure(this IServiceCollection services, string connectionString)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services)
     {
-        return services
-            .AddSingleton<IDbConnectionFactory>(new NpgsqlConnectionFactory(connectionString))
-            .AddSingleton<IUserRepository, UserRepositry>()
-            .AddScoped<IDomainEventsDispatcher, DomainEventsDispatcher>()
-            .AddDbContext<UsersDbContext>(options =>
-            {
-                options.UseNpgsql(
-                    connectionString,
-                    npgsqlOptions => npgsqlOptions.MigrationsHistoryTable(
-                        HistoryRepository.DefaultTableName,
-                        Schema.Users));
-            })
-            .AddScoped<IUnitOfWork, UnitOfWork<UsersDbContext>>();
+        return services.AddSingleton<IUserRepository, UserRepositry>();
     }
 }
