83646eb89414bd0d2ed1779f66a83009a1b94fbd
unknown <sabgasim@gmail.com>
2025-05-11 03:08:50 +0300

    feat(feed.app): impl UserCreatedIntegrationEventHandler

---
+++ src/Modules/Feed/Application/Authors/CreateAuthor/UserCreatedIntegrationEventHandler.cs
@@ -0,0 +1,31 @@
+﻿using Autofac;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.Events;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.InternalCommands;
+using SocialMediaBackend.Modules.Feed.Domain.Authors;
+using SocialMediaBackend.Modules.Feed.Infrastructure.Configuration;
+using SocialMediaBackend.Modules.Users.IntegrationEvents;
+
+namespace SocialMediaBackend.Modules.Feed.Application.Authors.CreateAuthor;
+
+public class UserCreatedIntegrationEventHandler : IIntegrationEventHandler<UserCreatedIntegrationEvent>
+{
+    public async ValueTask Handle(UserCreatedIntegrationEvent notification, CancellationToken cancellationToken)
+    {
+        await using (var scope = FeedCompositionRoot.BeginLifetimeScope())
+        {
+            var scheduler = scope.Resolve<ICommandsScheduler>();
+
+            var command = new CreateAuthorCommand(
+                id: Guid.NewGuid(),
+                authorId: new AuthorId(notification.UserId),
+                notification.Username,
+                notification.Nickname,
+                notification.ProfilePicture,
+                notification.ProfileIsPublic,
+                notification.FollowersCount,
+                notification.FollowingCount);
+
+            await scheduler.EnqueueAsync(command);
+        }
+    }
+}
