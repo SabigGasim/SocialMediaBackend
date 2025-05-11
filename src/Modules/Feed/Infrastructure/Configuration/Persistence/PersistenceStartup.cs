ade679981472a23ee23ee39ed6074e32eaab0c26
unknown <sabgasim@gmail.com>
2025-05-11 03:28:14 +0300

    feat(feed.infra): add PersistenceStartup

---
+++ src/Modules/Feed/Infrastructure/Configuration/Persistence/PersistenceStartup.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using SocialMediaBackend.Modules.Feed.Infrastructure.Data;
+
+namespace SocialMediaBackend.Modules.Feed.Infrastructure.Configuration.Persistence;
+
+public static class PersistenceStartup
+{
+    public static async Task InitializeAsync(IWebHostEnvironment env)
+    {
+        await using (var scope = FeedCompositionRoot.BeginLifetimeScope())
+        {
+            var context = scope.Resolve<FeedDbContext>();
+
+            await context.Database.MigrateAsync();
+            await context.Database.EnsureCreatedAsync();
+
+            if (env.IsDevelopment())
+            {
+                var schema = context.Database.GenerateCreateScript();
+                Console.WriteLine($"""
+                    ----------------------------------------------
+                    ------------ FeedDbContext Schema ------------
+                    ----------------------------------------------
+
+                    {schema}
+                    """);
+            }
+        }
+    }
+}
