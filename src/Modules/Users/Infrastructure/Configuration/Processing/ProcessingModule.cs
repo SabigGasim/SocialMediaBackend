b7fe0bbebc869c13efed65d9d0d7de996dd26500
unknown <sabgasim@gmail.com>
2025-05-11 03:34:28 +0300

    feat(users.infra): add ProcessingModule

---
+++ src/Modules/Users/Infrastructure/Configuration/Processing/ProcessingModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using SocialMediaBackend.BuildingBlocks.Infrastructure;
+
+namespace SocialMediaBackend.Modules.Users.Infrastructure.Configuration.Processing;
+
+public class ProcessingModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<DomainEventsDispatcher>()
+            .As<IDomainEventsDispatcher>()
+            .InstancePerLifetimeScope();
+
+        builder.RegisterType<UnitOfWork>()
+            .As<IUnitOfWork>()
+            .InstancePerLifetimeScope();
+    }
+}
