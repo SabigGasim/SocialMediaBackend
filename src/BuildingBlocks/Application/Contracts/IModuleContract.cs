77f7eb823599ae3e7363d30967b7ad049ebf708f
unknown <sabgasim@gmail.com>
2025-05-11 03:42:04 +0300

    refactor: add IModuleContract to abstract IUsersModule logic

---
+++ src/BuildingBlocks/Application/Contracts/IModuleContract.cs
@@ -0,0 +1,22 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application.Requests;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Queries;
+
+namespace SocialMediaBackend.BuildingBlocks.Application.Contracts;
+public interface IModuleContract
+{
+    Task<HandlerResponse<TResult>> ExecuteCommandAsync<TCommand, TResult>(
+        TCommand command,
+        CancellationToken ct = default)
+        where TCommand : ICommand<HandlerResponse<TResult>>;
+
+    Task<HandlerResponse> ExecuteCommandAsync<TCommand>(
+        TCommand command,
+        CancellationToken ct = default)
+        where TCommand : ICommand<HandlerResponse>;
+
+    Task<HandlerResponse<TResult>> ExecuteQueryAsync<TQuery, TResult>(
+        TQuery query,
+        CancellationToken ct = default)
+        where TQuery : IQuery<HandlerResponse<TResult>>;
+}
--- src/Modules/Users/Application/Contracts/IUsersModule.cs
+++ src/Modules/Users/Application/Contracts/IUsersModule.cs
@@ -1,25 +1,10 @@
-﻿using SocialMediaBackend.BuildingBlocks.Application.Requests;
+﻿using SocialMediaBackend.BuildingBlocks.Application.Contracts;
+using SocialMediaBackend.BuildingBlocks.Application.Requests;
 using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
 using SocialMediaBackend.BuildingBlocks.Application.Requests.Queries;
 
 namespace SocialMediaBackend.Modules.Users.Application.Contracts;
 
-public interface IUsersModule
-{
-    Task<HandlerResponse<TResult>> ExecuteCommandAsync<TCommand, TResult>(
-        TCommand command,
-        CancellationToken ct = default)
-        where TCommand : ICommand<HandlerResponse<TResult>>;
-
-    Task<HandlerResponse> ExecuteCommandAsync<TCommand>(
-        TCommand command,
-        CancellationToken ct = default)
-        where TCommand : ICommand<HandlerResponse>;
-
-    Task<HandlerResponse<TResult>> ExecuteQueryAsync<TQuery, TResult>(
-        TQuery query,
-        CancellationToken ct = default)
-        where TQuery : IQuery<HandlerResponse<TResult>>;
-}
+public interface IUsersModule : IModuleContract;
 
 
