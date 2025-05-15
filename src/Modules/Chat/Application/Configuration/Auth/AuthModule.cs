708a2d5dab8e2bc37b8864c5eddd3b6d48149cac
unknown <sabgasim@gmail.com>
2025-05-16 02:13:52 +0300

    feat(chat.app): add Auth autofac module

---
+++ src/Modules/Chat/Application/Configuration/Auth/AuthModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+
+namespace SocialMediaBackend.Modules.Chat.Application.Configuration.Auth;
+
+public class AuthModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        //builder.RegisterType<>()
+        //    .As<IChatterizationHandler<, >>()
+        //    .InstancePerDependency();
+    }
+}
