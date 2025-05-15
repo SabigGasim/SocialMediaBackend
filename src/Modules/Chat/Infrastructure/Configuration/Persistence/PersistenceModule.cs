da6ef20fa82255b500497359e1b8e5c70f4fa83c
unknown <sabgasim@gmail.com>
2025-05-16 02:14:28 +0300

    feat(chat.infra): add Persistence autofac module

---
+++ src/Modules/Chat/Infrastructure/Configuration/Persistence/PersistenceModule.cs
@@ -0,0 +1,24 @@
+﻿using Autofac;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using SocialMediaBackend.Modules.Chat.Infrastructure.Data;
+
+namespace SocialMediaBackend.Modules.Chat.Infrastructure.Configuration.Persistence;
+
+public class PersistenceModule(IConfiguration config) : Module
+{
+    private readonly IConfiguration _config = config;
+
+    protected override void Load(ContainerBuilder builder)
+    {
+        var connectionString = _config.GetConnectionString("PostgresConnection")!;
+
+        builder.Register(_ => new ChatDbContext(
+            ChatDbContextOptionsBuilderFactory
+                .Create(connectionString)
+                .Options))
+            .AsSelf()
+            .As<DbContext>()
+            .InstancePerLifetimeScope();
+    }
+}
