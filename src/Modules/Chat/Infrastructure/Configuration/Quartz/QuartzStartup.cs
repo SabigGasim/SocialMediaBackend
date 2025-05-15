ff9834d92aa46d8f2467daf9a31c97c36cacfe4d
unknown <sabgasim@gmail.com>
2025-05-16 02:17:34 +0300

    feat(chat.infra): add Quartz startup

---
+++ src/Modules/Chat/Infrastructure/Configuration/Quartz/QuartzStartup.cs
@@ -0,0 +1,32 @@
+﻿using Quartz;
+using SocialMediaBackend.Modules.Chat.Infrastructure.InternalCommands;
+
+namespace SocialMediaBackend.Modules.Chat.Infrastructure.Configuration.Quartz;
+
+public static class QuartzStartup
+{
+    public static async Task InitializeAsync()
+    {
+        var trigger = TriggerBuilder.Create()
+           .StartNow()
+           .WithSimpleSchedule(x => x
+               .WithIntervalInSeconds(2)
+               .RepeatForever())
+           .Build();
+
+        var job = JobBuilder.Create<ProcessInternalCommandsJob>()
+            .WithIdentity("Chat.ProcessInternalCommandsJob")
+            .Build();
+
+        var schedulerFactory = SchedulerBuilder.Create()
+            .UseInMemoryStore()
+            .UseDefaultThreadPool(1)
+            .Build();
+
+        var scheduler = await schedulerFactory.GetScheduler();
+
+        await scheduler.ScheduleJob(job, trigger);
+
+        await scheduler.Start();
+    }
+}
