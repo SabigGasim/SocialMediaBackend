96968741beec2ed63b5e54bed3ee0e05a28d2122
unknown <sabgasim@gmail.com>
2025-05-16 02:16:55 +0300

    feat(chat.infra): add ProcessInternalCommandsJob

---
+++ src/Modules/Chat/Infrastructure/InternalCommands/ProcessInternalCommandsJob.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Quartz;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.Modules.Chat.Infrastructure.Configuration;
+
+namespace SocialMediaBackend.Modules.Chat.Infrastructure.InternalCommands;
+
+[DisallowConcurrentExecution]
+public class ProcessInternalCommandsJob : IJob
+{
+    public async Task Execute(IJobExecutionContext context)
+    {
+        using (var scope = ChatCompositionRoot.BeginLifetimeScope())
+        {
+            var handler = scope.Resolve<ICommandHandler<ProcessInternalCommandsCommand>>();
+            await handler.ExecuteAsync(new ProcessInternalCommandsCommand(), default);
+        }
+    }
+}
