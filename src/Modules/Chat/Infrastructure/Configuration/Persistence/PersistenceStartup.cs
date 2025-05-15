f0d0f820b22be1456f8f91ea10a8707b8c37a63f
unknown <sabgasim@gmail.com>
2025-05-16 02:14:52 +0300

    feat(chat.infra): add Persistence startup

---
+++ src/Modules/Chat/Infrastructure/Configuration/Persistence/PersistenceStartup.cs
@@ -0,0 +1,33 @@
+﻿using Autofac;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Hosting;
+using SocialMediaBackend.Modules.Chat.Infrastructure.Data;
+
+namespace SocialMediaBackend.Modules.Chat.Infrastructure.Configuration.Persistence;
+
+public static class PersistenceStartup
+{
+    public static async Task InitializeAsync(IWebHostEnvironment env)
+    {
+        await using (var scope = ChatCompositionRoot.BeginLifetimeScope())
+        {
+            var context = scope.Resolve<ChatDbContext>();
+
+            await context.Database.MigrateAsync();
+            await context.Database.EnsureCreatedAsync();
+
+            if (env.IsDevelopment())
+            {
+                var schema = context.Database.GenerateCreateScript();
+                Console.WriteLine($"""
+                    ----------------------------------------------
+                    ------------ ChatDbContext Schema ------------
+                    ----------------------------------------------
+
+                    {schema}
+                    """);
+            }
+        }
+    }
+}
