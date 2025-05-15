42dadeee6bbccb12c6f99977991eb93f94ecc9e5
unknown <sabgasim@gmail.com>
2025-05-16 02:21:07 +0300

    feat(chat.app): add ApplicationServiceCollectionExtensions

---
+++ src/Modules/Chat/Application/ApplicationServiceCollectionExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using SocialMediaBackend.Modules.Chat.Application.Contracts;
+
+namespace SocialMediaBackend.Modules.Chat.Application;
+
+public static class ApplicationServiceCollectionExtensions
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services)
+    {
+        return services.AddSingleton<IChatModule, ChatModule>();
+    }
+}
