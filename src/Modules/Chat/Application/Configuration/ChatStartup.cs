ced1b535c0c6fe0dd366f745d74e41f75b884edd
unknown <sabgasim@gmail.com>
2025-05-16 02:20:11 +0300

    feat(chat.app): add Chat startup

---
+++ src/Modules/Chat/Application/Configuration/ChatStartup.cs
@@ -0,0 +1,41 @@
+﻿using Autofac;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using Autofac.Extensions.DependencyInjection;
+using SocialMediaBackend.Modules.Chat.Infrastructure.Configuration;
+using SocialMediaBackend.Modules.Chat.Application.Configuration.Mediator;
+using SocialMediaBackend.Modules.Chat.Infrastructure.Configuration.Persistence;
+using SocialMediaBackend.Modules.Chat.Infrastructure.Configuration.Quartz;
+
+namespace SocialMediaBackend.Modules.Chat.Application.Configuration;
+
+public static class ChatStartup
+{
+    public static async Task InitializeAsync(
+        IServiceCollection serviceCollection,
+        IConfiguration config,
+        IWebHostEnvironment env)
+    {
+        ConfigureCompositionRoot(serviceCollection, config);
+
+        await PersistenceStartup.InitializeAsync(env);
+        await QuartzStartup.InitializeAsync();
+    }
+
+    private static void ConfigureCompositionRoot(IServiceCollection serviceCollection, IConfiguration config)
+    {
+        var containerBuilder = new ContainerBuilder();
+
+        containerBuilder.Populate(serviceCollection);
+
+        containerBuilder.RegisterModule(new PersistenceModule(config));
+        containerBuilder.RegisterModule(new CQRSModule());
+        containerBuilder.RegisterModule(new QuartzModule());
+        containerBuilder.RegisterModule(new ProcessingModule());
+
+        var container = containerBuilder.Build();
+
+        ChatCompositionRoot.SetContainer(container);
+    }
+}
