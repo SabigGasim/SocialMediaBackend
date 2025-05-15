7f54da537cbd60ecadd463ced4ebaacd5dde83e3
unknown <sabgasim@gmail.com>
2025-05-16 02:28:13 +0300

    feat(chat): add mappings for chatter response contracts

---
+++ src/Modules/Chat/Application/Chatters/GetAllChatters/GetAllChattersResponse.cs
@@ -0,0 +1,16 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application;
+using SocialMediaBackend.Modules.Chat.Application.Chatters.GetChatter;
+
+namespace SocialMediaBackend.Modules.Chat.Application.Chatters.GetAllChatters;
+
+public record GetAllChattersResponse : PagedResponse<GetChatterResponse>
+{
+    public GetAllChattersResponse(
+        int PageNumber,
+        int PageSize,
+        int TotalCount,
+        IEnumerable<GetChatterResponse> Items) : base(PageNumber, PageSize, TotalCount, Items)
+    {
+        
+    }
+}
---
+++ src/Modules/Chat/Application/Mappings/DomainToApiContractMapper.cs
@@ -0,0 +1,29 @@
+﻿using SocialMediaBackend.Modules.Chat.Application.Chatters.GetAllChatters;
+using SocialMediaBackend.Modules.Chat.Application.Chatters.GetChatter;
+using SocialMediaBackend.Modules.Chat.Domain.Chatters;
+
+namespace SocialMediaBackend.Modules.Chat.Application.Mappings;
+
+public static class DomainToApiContractMapper
+{
+    public static GetChatterResponse MapToGetResponse(this Chatter chatter)
+    {
+        return new GetChatterResponse(
+            chatter.Id.Value,
+            chatter.Username,
+            chatter.Nickname,
+            chatter.FollowersCount,
+            chatter.FollowingCount,
+            chatter.ProfilePicture.Url
+            );
+    }
+
+    public static GetAllChattersResponse MapToResponse(this IEnumerable<Chatter> chatters, int pageNumber, int pageSize, int totalCount)
+    {
+        return new GetAllChattersResponse(
+            pageNumber,
+            pageSize,
+            totalCount,
+            chatters.Select(MapToGetResponse));
+    }
+}
