28b11a8a0e07d548340ec0f1d0ff19400d122a72
unknown <sabgasim@gmail.com>
2025-05-16 02:12:41 +0300

    feat(chat.app): add auth decorators

---
+++ src/Modules/Chat/Application/Auth/AuthCommandHandlerDecorator.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using SocialMediaBackend.BuildingBlocks.Application.Requests;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.BuildingBlocks.Infrastructure;
+
+namespace SocialMediaBackend.Modules.Chat.Application.Auth;
+
+internal sealed class AuthCommandHandlerDecorator<TRequest>
+    : AuthRequestHandlerDecoratorBase<TRequest, ICommandHandler<TRequest>>, ICommandHandler<TRequest>
+    where TRequest : ICommand<HandlerResponse>, IUserRequestBase
+{
+    public AuthCommandHandlerDecorator(
+        ICommandHandler<TRequest> decorated,
+        IDbConnectionFactory dbConnectionFactory,
+        IHttpContextAccessor httpContextAccessor) : base(decorated, dbConnectionFactory, httpContextAccessor)
+    {
+    }
+}
---
+++ src/Modules/Chat/Application/Auth/AuthQueryHandlerDecorator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using SocialMediaBackend.BuildingBlocks.Application.Requests;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Queries;
+using SocialMediaBackend.BuildingBlocks.Infrastructure;
+
+namespace SocialMediaBackend.Modules.Chat.Application.Auth;
+
+internal sealed class AuthQueryHandlerDecorator<TRequest, TResult>
+    : AuthRequestHandlerWithResultDecoratorBase<TRequest, TResult, IQueryHandler<TRequest, TResult>>,
+    IQueryHandler<TRequest, TResult>
+    where TRequest : IQuery<HandlerResponse<TResult>>, IUserRequestBase
+{
+    public AuthQueryHandlerDecorator(
+        IQueryHandler<TRequest, TResult> decorated,
+        IDbConnectionFactory dbConnectionFactory,
+        IHttpContextAccessor httpContextAccessor) : base(decorated, dbConnectionFactory, httpContextAccessor)
+    {
+    }
+}
