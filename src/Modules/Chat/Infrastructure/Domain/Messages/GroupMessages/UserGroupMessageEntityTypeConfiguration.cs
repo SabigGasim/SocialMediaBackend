25ae4b9c0fef1eedcdd9f65741d3f846fa494a7c
unknown <sabgasim@gmail.com>
2025-05-17 17:29:23 +0300

    feat(chat.infra): add UserGroupMessageEntityTypeConfiguration

---
+++ src/Modules/Chat/Infrastructure/Domain/Messages/GroupMessages/UserGroupMessageEntityTypeConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SocialMediaBackend.Modules.Chat.Domain.Messages.GroupMessages;
+
+namespace SocialMediaBackend.Modules.Chat.Infrastructure.Domain.Messages.GroupMessages;
+
+internal class UserGroupMessageEntityTypeConfiguration : IEntityTypeConfiguration<UserGroupMessage>
+{
+    public void Configure(EntityTypeBuilder<UserGroupMessage> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.HasIndex(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasConversion(
+                id => id.Value,
+                value => new(value));
+
+        builder.HasOne(x => x.Sender)
+            .WithMany()
+            .HasForeignKey(x => x.SenderId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(x => x.GroupMessage)
+            .WithMany()
+            .HasForeignKey(x => x.GroupMessageId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(x => x.UserGroupChat)
+            .WithMany(x => x.Messages)
+            .HasForeignKey(x => x.UserGroupChatId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
