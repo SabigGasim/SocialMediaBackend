d79b0b4234de126f6dd13d3ed84f434058763af0
unknown <sabgasim@gmail.com>
2025-05-17 17:28:02 +0300

    feat(chat.infra): add DirectMessageEntityTypeConfiguration

---
+++ src/Modules/Chat/Infrastructure/Domain/Messages/DirectMessages/DirectMessageEntityTypeConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SocialMediaBackend.Modules.Chat.Domain.Messages.DirectMessages;
+
+namespace SocialMediaBackend.Modules.Chat.Infrastructure.Domain.Messages.DirectMessages;
+
+internal class DirectMessageEntityTypeConfiguration : IEntityTypeConfiguration<DirectMessage>
+{
+    public void Configure(EntityTypeBuilder<DirectMessage> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.HasIndex(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasConversion(
+                id => id.Value,
+                value => new(value));
+
+        builder.HasOne(x => x.Sender)
+            .WithMany()
+            .HasForeignKey(x => x.SenderId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(x => x.Chat)
+            .WithMany(x => x.Messages)
+            .HasForeignKey(x => x.ChatId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
