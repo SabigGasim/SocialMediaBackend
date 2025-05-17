6a91d402c64d15ea6b6717738b3d3b3a0eae6828
unknown <sabgasim@gmail.com>
2025-05-17 17:23:57 +0300

    feat(chat.domain): add UserDirectMessage entity

---
+++ src/Modules/Chat/Domain/Messages/DirectMessages/UserDirectMessage.cs
@@ -0,0 +1,34 @@
+﻿using SocialMediaBackend.BuildingBlocks.Domain;
+using SocialMediaBackend.Modules.Chat.Domain.Chatters;
+using SocialMediaBackend.Modules.Chat.Domain.Conversations.DirectChats;
+
+namespace SocialMediaBackend.Modules.Chat.Domain.Messages.DirectMessages;
+
+public class UserDirectMessage : Entity<UserDirectMessageId>
+{
+    private UserDirectMessage() { }
+
+    private UserDirectMessage(ChatterId senderId, DirectMessageId directMessageId, UserDirectChatId chatId)
+    {
+        Id = UserDirectMessageId.New();
+        SenderId = senderId;
+        DirectMessageId = directMessageId;
+        UserDirectChatId = chatId;
+    }
+
+    public ChatterId SenderId { get; private set; } = default!;
+    public DirectMessageId DirectMessageId { get; private set; } = default!;
+    public UserDirectChatId UserDirectChatId { get; private set; } = default!;
+
+    public Chatter Sender { get; private set; } = default!;
+    public DirectMessage DirectMessage { get; private set; } = default!;
+    public UserDirectChat UserDirectChat { get; private set; } = default!;
+
+    internal static UserDirectMessage Create(
+        ChatterId senderId, 
+        DirectMessageId directMessageId,
+        UserDirectChatId chatId)
+    {
+        return new UserDirectMessage(senderId, directMessageId, chatId);
+    }
+}
---
+++ src/Modules/Chat/Domain/Messages/DirectMessages/UserDirectMessageId.cs
@@ -0,0 +1,8 @@
+﻿using SocialMediaBackend.BuildingBlocks.Domain;
+
+namespace SocialMediaBackend.Modules.Chat.Domain.Messages.DirectMessages;
+
+public record UserDirectMessageId(Guid Value) : TypedIdValueBase<Guid>(Value)
+{
+    public static UserDirectMessageId New() => new UserDirectMessageId(Guid.CreateVersion7());
+}
