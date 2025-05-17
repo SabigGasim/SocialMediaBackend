cfba28aaedc9dd69ccb60ed4856dd0fe8ec4c594
unknown <sabgasim@gmail.com>
2025-05-17 17:26:02 +0300

    feat(chat.domain): add GroupMessage entity

---
+++ src/Modules/Chat/Domain/Messages/GroupMessages/GroupMessage.cs
@@ -0,0 +1,47 @@
+﻿using SocialMediaBackend.BuildingBlocks.Domain;
+using SocialMediaBackend.Modules.Chat.Domain.Chatters;
+using SocialMediaBackend.Modules.Chat.Domain.Conversations.GroupChats;
+
+namespace SocialMediaBackend.Modules.Chat.Domain.Messages.GroupMessages;
+
+public class GroupMessage : AuditableEntity<GroupMessageId>
+{
+    private readonly List<Chatter> _seenBy = new();
+
+    private GroupMessage() { }
+
+    private GroupMessage(ChatterId senderId, GroupChatId chatId, string text, DateTimeOffset sentAt)
+        : base()
+    {
+        Id = GroupMessageId.New();
+        SenderId = senderId;
+        ChatId = chatId;
+        Text = text;
+        SentAt = sentAt;
+
+        var now = DateTimeOffset.UtcNow;
+
+        Created = now;
+        CreatedBy = senderId.ToString();
+        LastModified = now;
+        LastModifiedBy = senderId.ToString();
+    }
+
+    public ChatterId SenderId { get; private set; } = default!;
+    public GroupChatId ChatId { get; private set; } = default!;
+    public string Text { get; private set; } = default!;
+    public DateTimeOffset SentAt { get; private set; }
+
+    public Chatter Sender {  get; private set; } = default!;
+    public GroupChat Chat { get; private set; } = default!;
+    public IReadOnlyCollection<Chatter> SeenBy => _seenBy.AsReadOnly();
+
+    public static GroupMessage Create(
+        ChatterId senderId,
+        GroupChatId chatId,
+        string text,
+        DateTimeOffset sentAt)
+    {
+        return new GroupMessage(senderId, chatId, text, sentAt);
+    }
+}
---
+++ src/Modules/Chat/Domain/Messages/GroupMessages/GroupMessageId.cs
@@ -0,0 +1,8 @@
+﻿using SocialMediaBackend.BuildingBlocks.Domain;
+
+namespace SocialMediaBackend.Modules.Chat.Domain.Messages.GroupMessages;
+
+public record GroupMessageId(Guid Value) : TypedIdValueBase<Guid>(Value)
+{
+    public static GroupMessageId New() => new GroupMessageId(Guid.CreateVersion7());
+}
