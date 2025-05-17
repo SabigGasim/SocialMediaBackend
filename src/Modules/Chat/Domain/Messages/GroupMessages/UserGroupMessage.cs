ef4f23e2191307de61b4148e83bcfe7d4de02e4a
unknown <sabgasim@gmail.com>
2025-05-17 17:26:21 +0300

    feat(chat.domain): add UserGroupMessage entity

---
+++ src/Modules/Chat/Domain/Messages/GroupMessages/UserGroupMessage.cs
@@ -0,0 +1,34 @@
+﻿using SocialMediaBackend.BuildingBlocks.Domain;
+using SocialMediaBackend.Modules.Chat.Domain.Chatters;
+using SocialMediaBackend.Modules.Chat.Domain.Conversations.GroupChats;
+
+namespace SocialMediaBackend.Modules.Chat.Domain.Messages.GroupMessages;
+
+public class UserGroupMessage : Entity<UserGroupMessageId>
+{
+    private UserGroupMessage() { }
+
+    private UserGroupMessage(ChatterId senderId, GroupMessageId groupMessageId, UserGroupChatId chatId)
+    {
+        Id = UserGroupMessageId.New();
+        SenderId = senderId;
+        GroupMessageId = groupMessageId;
+        UserGroupChatId = chatId;
+    }
+
+    public ChatterId SenderId { get; private set; } = default!;
+    public GroupMessageId GroupMessageId { get; private set; } = default!;
+    public UserGroupChatId UserGroupChatId { get; private set; } = default!;
+
+    public Chatter Sender { get; private set; } = default!;
+    public GroupMessage GroupMessage { get; private set; } = default!;
+    public UserGroupChat UserGroupChat { get; private set; } = default!;
+
+    internal static UserGroupMessage Create(
+        ChatterId senderId, 
+        GroupMessageId groupMessageId,
+        UserGroupChatId chatId)
+    {
+        return new UserGroupMessage(senderId, groupMessageId, chatId);
+    }
+}
---
+++ src/Modules/Chat/Domain/Messages/GroupMessages/UserGroupMessageId.cs
@@ -0,0 +1,8 @@
+﻿using SocialMediaBackend.BuildingBlocks.Domain;
+
+namespace SocialMediaBackend.Modules.Chat.Domain.Messages.GroupMessages;
+
+public record UserGroupMessageId(Guid Value) : TypedIdValueBase<Guid>(Value)
+{
+    public static UserGroupMessageId New() => new UserGroupMessageId(Guid.CreateVersion7());
+}
