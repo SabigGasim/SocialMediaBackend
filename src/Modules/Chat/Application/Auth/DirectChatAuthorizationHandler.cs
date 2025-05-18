0ffd3c89745bd4ac35478ea269cc0408234be4f5
unknown <sabgasim@gmail.com>
2025-05-19 02:39:13 +0300

    feat(chat.app): add DirectChatAuthorizationHandler

---
+++ src/Modules/Chat/Application/Auth/DirectChatAuthorizationHandler.cs
@@ -0,0 +1,13 @@
+﻿using SocialMediaBackend.Modules.Chat.Domain.Chatters;
+using SocialMediaBackend.Modules.Chat.Domain.Conversations.DirectChats;
+
+namespace SocialMediaBackend.Modules.Chat.Application.Auth;
+
+internal class DirectChatAuthorizationHandler : IAuthorizationHandler<DirectChat>
+{
+    public bool Authorize(ChatterId? chatterId, DirectChat resource)
+    {
+        return chatterId == resource.FirstChatterId
+            || chatterId == resource.SecondChatterId;
+    }
+}
---
+++ src/Modules/Chat/Application/Auth/IAuthorizationHandler.cs
@@ -0,0 +1,10 @@
+﻿using SocialMediaBackend.Modules.Chat.Domain.Chatters;
+
+namespace SocialMediaBackend.Modules.Chat.Application.Auth;
+
+public interface IAuthorizationHandler<TResource>
+{
+    bool Authorize(
+        ChatterId? chatterId,
+        TResource resource);
+}
