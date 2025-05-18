0ba69b2891a56edf1a4eca94da4294c80fbf8742
unknown <sabgasim@gmail.com>
2025-05-19 02:42:49 +0300

    refactor(chat.app): utilize auth handler in CreateDirectMessageCommandHandler

--- src/Modules/Chat/Application/DirectMessaging/CreateDirectMessage/CreateDirectMessageCommandHandler.cs
+++ src/Modules/Chat/Application/DirectMessaging/CreateDirectMessage/CreateDirectMessageCommandHandler.cs
@@ -1,33 +1,38 @@
-﻿using SocialMediaBackend.BuildingBlocks.Application;
+﻿using Microsoft.AspNetCore.Authorization;
+using SocialMediaBackend.BuildingBlocks.Application;
 using SocialMediaBackend.BuildingBlocks.Application.Requests;
 using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.Modules.Chat.Application.Auth;
 using SocialMediaBackend.Modules.Chat.Application.Mappings;
 using SocialMediaBackend.Modules.Chat.Domain.Chatters;
+using SocialMediaBackend.Modules.Chat.Domain.Conversations.DirectChats;
 using SocialMediaBackend.Modules.Chat.Infrastructure.Data;
 
 namespace SocialMediaBackend.Modules.Chat.Application.DirectMessaging.SendDirectMessage;
 
-public class CreateDirectMessageCommandHandler(ChatDbContext context) : ICommandHandler<CreateDirectMessageCommand, SendDirectMessageResponse>
+public class CreateDirectMessageCommandHandler(
+    ChatDbContext context,
+    IAuthorizationHandler<DirectChat> authorizationHandler) : ICommandHandler<CreateDirectMessageCommand, SendDirectMessageResponse>
 {
     private readonly ChatDbContext _context = context;
-
-    private static HandlerResponse<SendDirectMessageResponse> DirectChatNotFoundError => ("You have to create a chat with the user before sending a message", HandlerResponseStatus.Conflict);
+    private readonly IAuthorizationHandler<DirectChat> _authorizationHandler = authorizationHandler;
 
     public async Task<HandlerResponse<SendDirectMessageResponse>> ExecuteAsync(CreateDirectMessageCommand command, CancellationToken ct)
     {
         var chat = await _context.DirectChats.FindAsync([command.DirectChatId], ct);
         if (chat is null)
         {
-            return DirectChatNotFoundError;
+            return ("You have to create a chat with the user before sending a message", HandlerResponseStatus.Conflict);
         }
 
         var senderId = new ChatterId(command.UserId);
-        
-        var message = chat.AddMessage(senderId, command.Text);
-        if (message is null)
+
+        if(!_authorizationHandler.Authorize(senderId, chat))
         {
-            return DirectChatNotFoundError;
+            return ("You're unauthorized to send messages in this chat", HandlerResponseStatus.Unauthorized);
         }
+
+        var message = chat.AddMessage(senderId, command.Text);
 
         _context.Add(message);
 
