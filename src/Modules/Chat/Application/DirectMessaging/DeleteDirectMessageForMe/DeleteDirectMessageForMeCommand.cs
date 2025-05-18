3e7053032cc7767b657492500e615ce0f15a335a
unknown <sabgasim@gmail.com>
2025-05-19 02:49:07 +0300

    feat(chat.app): implement delete direct messsage for me functionality

---
+++ src/Modules/Chat/Application/DirectMessaging/DeleteDirectMessageForMe/DeleteDirectMessageForMeCommand.cs
@@ -0,0 +1,24 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application.Requests;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.Modules.Chat.Domain.Conversations.DirectChats;
+using SocialMediaBackend.Modules.Chat.Domain.Messages.DirectMessages;
+
+namespace SocialMediaBackend.Modules.Chat.Application.DirectMessaging.DeleteDirectMessageForMe;
+
+public class DeleteDirectMessageForMeCommand(Guid messageId, Guid directChatId) : CommandBase, IUserRequest
+{
+    public Guid UserId { get; private set; }
+    public bool IsAdmin {  get; private set; }
+    public DirectMessageId MessageId { get; } = new(messageId);
+    public DirectChatId DirectChatId { get; } = new(directChatId);
+
+    public void WithAdminRole(bool isAdmin)
+    {
+        IsAdmin = isAdmin;
+    }
+
+    public void WithUserId(Guid userId)
+    {
+        UserId = userId;
+    }
+}
---
+++ src/Modules/Chat/Application/DirectMessaging/DeleteDirectMessageForMe/DeleteDirectMessageForMeRequest.cs
@@ -0,0 +1,3 @@
+﻿namespace SocialMediaBackend.Modules.Chat.Application.DirectMessaging.DeleteDirectMessageForMe;
+
+public record DeleteDirectMessageForMeRequest(Guid ChatId, Guid MessageId);
