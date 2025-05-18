fd14ccac4a63efa476056036ae69d0533d5a0c86
unknown <sabgasim@gmail.com>
2025-05-18 23:53:37 +0300

    feat(api): add CreateDirectMessageEndpoint

---
+++ src/Presentation/Api/Modules/Chat/Endpoints/SendDirectMessageEndpiont.cs
@@ -0,0 +1,15 @@
+﻿using FastEndpoints;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Modules.Chat.Application.Contracts;
+using SocialMediaBackend.Modules.Chat.Application.DirectMessaging.SendDirectMessage;
+
+namespace SocialMediaBackend.Api.Modules.Chat.Endpoints;
+
+[HttpPost(ApiEndpoints.DirectChat.SendDirectMessage)]
+public class SendDirectMessageEndpiont(IChatModule module) : RequestEndpoint<SendDirectMessageRequest, SendDirectMessageResponse>(module)
+{
+    public override async Task HandleAsync(SendDirectMessageRequest req, CancellationToken ct)
+    {
+        await HandleCommandAsync(new CreateDirectMessageCommand(req.ChatId, req.Text), ct);
+    }
+}
