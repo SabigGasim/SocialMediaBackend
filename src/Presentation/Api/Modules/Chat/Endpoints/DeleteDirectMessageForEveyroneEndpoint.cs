4e1959d972a06702ea95a930c85afed0b91f39bf
unknown <sabgasim@gmail.com>
2025-05-19 03:06:42 +0300

    feat(api): add DeleteDirectMessageForEveryoneEndpoint

---
+++ src/Presentation/Api/Modules/Chat/Endpoints/DeleteDirectMessageForEveyroneEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using FastEndpoints;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Modules.Chat.Application.Contracts;
+using SocialMediaBackend.Modules.Chat.Application.DirectMessaging.DeleteDirectMessageForEveryone;
+
+namespace SocialMediaBackend.Api.Modules.Chat.Endpoints;
+
+[HttpDelete(ApiEndpoints.DirectChat.DeleteMessageForEveryone)]
+public class DeleteDirectMessageForEveyroneEndpoint(IChatModule module) 
+    : RequestEndpoint<DeleteDirectMessageForEveryoneRequest>(module)
+{
+    public override Task HandleAsync(DeleteDirectMessageForEveryoneRequest req, CancellationToken ct)
+    {
+        return HandleCommandAsync(new DeleteDirectMessageForEveryoneCommand(req.ChatId, req.MessageId), ct);
+    }
+}
