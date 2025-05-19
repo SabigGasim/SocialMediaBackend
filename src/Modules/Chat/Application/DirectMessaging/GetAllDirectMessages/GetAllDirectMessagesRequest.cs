fff90a93c63160d12f3e3431918b82869ee4685c
unknown <sabgasim@gmail.com>
2025-05-19 06:56:06 +0300

    feat(chat.app): impl get all direct messages functionality

---
+++ src/Modules/Chat/Application/DirectMessaging/GetAllDirectMessages/GetAllDirectMessagesRequest.cs
@@ -0,0 +1,5 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application;
+
+namespace SocialMediaBackend.Modules.Chat.Application.DirectMessaging.GetAllDirectMessages;
+
+public record GetAllDirectMessagesRequest(Guid ChatId) : PagedRequest;
---
+++ src/Modules/Chat/Application/Mappings/DtoToApiContractMapper.cs
@@ -0,0 +1,23 @@
+﻿using SocialMediaBackend.Modules.Chat.Application.DirectMessaging.GetAllDirectMessages;
+using SocialMediaBackend.Modules.Chat.Infrastructure.Domain.Conversations;
+using System.Reflection.Metadata.Ecma335;
+
+namespace SocialMediaBackend.Modules.Chat.Application.Mappings;
+
+internal static class DtoToApiContractMapper
+{
+    public static GetDirectMessageResponse MapToResponse(this DirectMessageDto message)
+    {
+        return new GetDirectMessageResponse(
+            message.MessageId,
+            message.SenderId, 
+            message.Text, 
+            message.SentAt, 
+            message.Status);
+    }
+
+    public static GetAllDirectMessagesResponse MapToResponse(this IEnumerable<DirectMessageDto> messages)
+    {
+        return new GetAllDirectMessagesResponse(messages.Select(x => x.MapToResponse()));
+    }
+}
