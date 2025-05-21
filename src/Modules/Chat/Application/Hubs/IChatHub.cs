48d5f0b84147cd5e6ef082f2c30083d2e1cc6650
unknown <sabgasim@gmail.com>
2025-05-21 07:22:37 +0300

    feat(chat): add SignalR ChatHub

---
+++ src/Modules/Chat/Application/Hubs/IChatHub.cs
@@ -0,0 +1,7 @@
+﻿namespace SocialMediaBackend.Modules.Chat.Application.Hubs;
+
+public interface IChatHub
+{
+    Task UserConnected(Guid userId);
+    Task UserDisconnected(Guid userId);
+}
---
+++ src/Presentation/Api/ChatHub.cs
@@ -0,0 +1,52 @@
+﻿using Autofac;
+using Microsoft.AspNetCore.SignalR;
+using SocialMediaBackend.Modules.Chat.Application.Hubs;
+using SocialMediaBackend.Modules.Chat.Domain.Chatters;
+using SocialMediaBackend.Modules.Chat.Infrastructure.Configuration;
+using SocialMediaBackend.Modules.Chat.Infrastructure.Domain.Chatters;
+using SocialMediaBackend.Modules.Chat.Infrastructure.Domain.Conversations;
+
+namespace SocialMediaBackend.Api;
+
+public class ChatHub : Hub<IChatHub>
+{
+    private readonly IChatterRepository _chatterRepository;
+    private readonly IChatRepository _chatRepository;
+    private readonly ILifetimeScope _scope;
+
+    public ChatHub()
+    {
+        _scope = ChatCompositionRoot.BeginLifetimeScope();
+        _chatterRepository = _scope.Resolve<IChatterRepository>();
+        _chatRepository = _scope.Resolve<IChatRepository>();
+    }
+
+    public override async Task OnConnectedAsync()
+    {
+        var chatterId = new ChatterId(Guid.Parse(Context.UserIdentifier!));
+
+        var users = await _chatRepository.GetChattersWithDirectOrGroupChatWith(chatterId);
+
+        await Clients.Users(users).UserConnected(chatterId.Value);
+
+        await _chatterRepository.SetOnlineStatus(chatterId, true);
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        var chatterId = new ChatterId(Guid.Parse(Context.UserIdentifier!));
+
+        var users = await _chatRepository.GetChattersWithDirectOrGroupChatWith(chatterId);
+
+        await Clients.Users(users).UserDisconnected(chatterId.Value);
+
+        await _chatterRepository.SetOnlineStatus(chatterId, false);
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+
+        _scope.Dispose();
+    }
+}
