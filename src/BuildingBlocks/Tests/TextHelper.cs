16c567b48604e33769de30ac16cdc4cc87bc6a65
unknown <sabgasim@gmail.com>
2025-05-21 06:09:46 +0300

    refactor(tests): move TextHelper to building blocks

---
+++ src/BuildingBlocks/Tests/TextHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Text;
+
+namespace SocialMediaBackend.BuildingBlocks.Tests;
+
+public static class TextHelper
+{
+    public static string CreateRandom(int length)
+    {
+        var builder = new StringBuilder(Guid.NewGuid().ToString().Replace("-", ""));
+
+        for (int i = 1; i * 32 < length; i++)
+        {
+            builder.Append(Guid.NewGuid().ToString().Replace("-", ""));
+        }
+
+        return builder.ToString()[..length];
+    }
+}
