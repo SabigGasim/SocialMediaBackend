14a20277139a2d29bbfc54631cc556cafb1dcbaa
unknown <sabgasim@gmail.com>
2025-05-21 07:28:49 +0300

    feat(api): add authorization for SignalR hubs

--- src/Presentation/Api/ApiServiceCollectionExtensions.cs
+++ src/Presentation/Api/ApiServiceCollectionExtensions.cs
@@ -31,6 +31,23 @@ public static class ApiServiceCollectionExtensions
                     ValidateIssuer = true,
                     ValidateAudience = true
                 };
+
+                o.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+
+                        var path = context.HttpContext.Request.Path;
+                        if (!string.IsNullOrEmpty(accessToken) &&
+                            path.StartsWithSegments($"/{ApiEndpoints.ChatHub.Base}"))
+                        {
+                            context.Token = accessToken;
+                        }
+
+                        return Task.CompletedTask;
+                    }
+                };
             });
 
         services.AddAuthorization(options =>
