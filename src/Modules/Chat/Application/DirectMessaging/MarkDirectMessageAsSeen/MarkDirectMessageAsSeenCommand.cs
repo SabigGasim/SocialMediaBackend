21e9cba8f17e021397eced1589f452d004fb4eb6
unknown <sabgasim@gmail.com>
2025-05-22 02:44:03 +0300

    feat(chat.app): add MarkDirectMessageAsSeen functionality

---
+++ src/Modules/Chat/Application/DirectMessaging/MarkDirectMessageAsSeen/MarkDirectMessageAsSeenCommand.cs
@@ -0,0 +1,28 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application.Requests;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.Modules.Chat.Domain.Conversations.DirectChats;
+using SocialMediaBackend.Modules.Chat.Domain.Messages.DirectMessages;
+
+namespace SocialMediaBackend.Modules.Chat.Application.DirectMessaging.MarkDirectMessageAsSeen;
+
+public class MarkDirectMessageAsSeenCommand(
+    DirectChatId directChatId, 
+    DirectMessageId directMessageId) : CommandBase, IUserRequest
+{
+    public DirectChatId DirectChatId { get; } = directChatId;
+
+    public DirectMessageId DirectMessageId { get; } = directMessageId;
+
+    public Guid UserId { get; private set; }
+    public bool IsAdmin { get; private set; }
+
+    public void WithAdminRole(bool isAdmin)
+    {
+        IsAdmin = isAdmin;
+    }
+
+    public void WithUserId(Guid userId)
+    {
+        UserId = userId;
+    }
+}
---
+++ src/Modules/Chat/Application/DirectMessaging/MarkDirectMessageAsSeen/MarkDirectMessageAsSeenMessage.cs
@@ -0,0 +1,5 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands.Realtime;
+
+namespace SocialMediaBackend.Modules.Chat.Application.DirectMessaging.MarkDirectMessageAsSeen;
+
+public record MarkDirectMessageAsSeenMessage(Guid DirectChatId, Guid LastSeenMessagId) : IRealtimeMessage;
