56f4fde5c62942144dd001759c14911c76996d30
unknown <sabgasim@gmail.com>
2025-05-22 23:11:13 +0300

    feat(chat.app): add GroupChatAuthorizationHandler

---
+++ src/Modules/Chat/Application/Auth/GroupChatAuthorizationHandler.cs
@@ -0,0 +1,35 @@
+﻿using Dapper;
+using SocialMediaBackend.BuildingBlocks.Infrastructure;
+using SocialMediaBackend.Modules.Chat.Domain.Chatters;
+using SocialMediaBackend.Modules.Chat.Domain.Conversations.GroupChats;
+using SocialMediaBackend.Modules.Chat.Infrastructure.Data;
+
+namespace SocialMediaBackend.Modules.Chat.Application.Auth;
+
+internal class GroupChatAuthorizationHandler(
+    IDbConnectionFactory factory)
+    : IAuthorizationHandler<GroupChat, GroupChatId>
+{
+    private readonly IDbConnectionFactory _factory = factory;
+
+    public async Task<bool> AuthorizeAsync(ChatterId chatterId, GroupChatId resourceId, CancellationToken ct = default)
+    {
+        const string sql = $"""
+            SELECT EXISTS (
+                SELECT 1
+                FROM {Schema.Chat}."GroupChatMembers"
+                WHERE "GroupChatId" = @GroupChatId
+                  AND "ChatterId" = @ChatterId
+            );
+            """;
+
+        using (var connection = await _factory.CreateAsync(ct))
+        {
+            return await connection.ExecuteScalarAsync<bool>(new CommandDefinition(sql, new
+            {
+                ChatterId = chatterId,
+                GroupChatId = resourceId
+            }, cancellationToken: ct));
+        }
+    }
+}
