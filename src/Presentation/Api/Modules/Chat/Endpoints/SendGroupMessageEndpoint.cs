baa035881fc0eab8e8af74372766d7c6fd7cf8c6
unknown <sabgasim@gmail.com>
2025-05-22 23:32:02 +0300

    feat(api): add SendDirectMessageEndpoint

---
+++ src/Presentation/Api/Modules/Chat/Endpoints/SendGroupMessageEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using FastEndpoints;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Api.Services;
+using SocialMediaBackend.Modules.Chat.Application.Contracts;
+using SocialMediaBackend.Modules.Chat.Application.GroupMessaging.CreateGroupMessage;
+using SocialMediaBackend.Modules.Chat.Application.Hubs;
+
+namespace SocialMediaBackend.Api.Modules.Chat.Endpoints;
+
+[HttpPost(ApiEndpoints.GroupChat.SendMessage)]
+public class SendGroupMessageEndpoint(
+    IChatModule module,
+    IRealtimeMessageSender<ChatHub> sender) 
+    : RealtimeEndpoint<SendGroupMessageRequest, SendGroupMessageResponse, CreateGroupMessageMessage, ChatHub>(module, sender)
+{
+    public override Task HandleAsync(SendGroupMessageRequest req, CancellationToken ct)
+    {
+        return HandleMultipleUsersCommandAsync(new CreateGroupMessageCommand(req.GroupId, req.Text), ct);
+    }
+}
