120528d711f8b79a8c18afa6f02b1499ac6072b5
unknown <sabgasim@gmail.com>
2025-05-23 05:26:49 +0300

    feat(chat.app): add InMemoryHubConnectionTracker

---
+++ src/BuildingBlocks/Infrastructure/IHubConnectionTracker.cs
@@ -0,0 +1,8 @@
+﻿namespace SocialMediaBackend.BuildingBlocks.Infrastructure;
+
+public interface IHubConnectionTracker
+{
+    Task<IEnumerable<string>> GetConnectionsAsync(string userId);
+    Task AddConnectionAsync(string userId, string connectionId);
+    Task RemoveConnectionAsync(string userId, string connectionId);
+}
---
+++ src/Modules/Chat/Infrastructure/InMemoryHubConnectionTracker.cs
@@ -0,0 +1,42 @@
+﻿using SocialMediaBackend.BuildingBlocks.Infrastructure;
+using System.Collections.Concurrent;
+
+namespace SocialMediaBackend.Modules.Chat.Infrastructure;
+
+public class InMemoryHubConnectionTracker : IHubConnectionTracker
+{
+    private readonly ConcurrentDictionary<string, ConcurrentDictionary<string, byte>> _connections = new();
+
+    public Task AddConnectionAsync(string userId, string connectionId)
+    {
+        var userConnections = _connections.GetOrAdd(userId, _ => new ConcurrentDictionary<string, byte>());
+        userConnections.TryAdd(connectionId, 0);
+        return Task.CompletedTask;
+    }
+
+    public Task<IEnumerable<string>> GetConnectionsAsync(string userId)
+    {
+        if (_connections.TryGetValue(userId, out var userConnections))
+        {
+            return Task.FromResult(userConnections.Keys.AsEnumerable());
+        }
+
+        return Task.FromResult(Enumerable.Empty<string>());
+    }
+
+    public Task RemoveConnectionAsync(string userId, string connectionId)
+    {
+        if (_connections.TryGetValue(userId, out var userConnections))
+        {
+            userConnections.TryRemove(connectionId, out _);
+
+            if (userConnections.IsEmpty)
+            {
+                _connections.TryRemove(userId, out _);
+            }
+        }
+
+        return Task.CompletedTask;
+    }
+}
+
