5071c80eb9613bc1e6a1f531d09ed119552ce577
unknown <sabgasim@gmail.com>
2025-05-23 20:24:14 +0300

    feat(api): impl RealtimeSideEffect dispatching in IRealtimeMessageSender

--- src/Presentation/Api/Services/IRealtimeMessageSender.cs
+++ src/Presentation/Api/Services/IRealtimeMessageSender.cs
@@ -1,11 +1,16 @@
 ﻿using Microsoft.AspNetCore.SignalR;
 using SocialMediaBackend.BuildingBlocks.Application.Requests;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands.Realtime;
 
 namespace SocialMediaBackend.Api.Services;
 
 public interface IRealtimeMessageSender<THub>
     where THub : Hub
 {
-    Task SendAsync<TResponse, TMessage, TIdentifier>(TResponse response) where TResponse : IRealtimeResponse<TMessage, TIdentifier>;
-    Task SendAllAsync<TResponse, TMessage>(TResponse response) where TResponse : IRealtimeResponse<TMessage>;
+    Task SendAsync<TResponse, TMessage, TIdentifier>(TResponse response) 
+        where TResponse : IRealtimeResponse<TMessage, TIdentifier>
+        where TMessage : IRealtimeMessage;
+    Task SendAllAsync<TResponse, TMessage>(TResponse response)
+        where TResponse : IRealtimeResponse<TMessage>
+        where TMessage : IRealtimeMessage;
 }
--- src/Presentation/Api/Services/RealtimeMessageSender.cs
+++ src/Presentation/Api/Services/RealtimeMessageSender.cs
@@ -1,17 +1,27 @@
-﻿using Microsoft.AspNetCore.SignalR;
+﻿using Autofac;
+using Mediator;
+using Microsoft.AspNetCore.SignalR;
+using SocialMediaBackend.BuildingBlocks.Application;
 using SocialMediaBackend.BuildingBlocks.Application.Requests;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands.Realtime;
 
 namespace SocialMediaBackend.Api.Services;
 
-public class RealtimeMessageSender<THub>(IHubContext<THub> context) : IRealtimeMessageSender<THub>
+public class RealtimeMessageSender<THub>(
+    IHubContext<THub> context,
+    ILifetimeScope lifetimeScope,
+    IMediator mediator) : IRealtimeMessageSender<THub>
     where THub : Hub
 {
     private readonly IHubContext<THub> _context = context;
+    private readonly ILifetimeScope _scope = lifetimeScope;
+    private readonly IMediator _mediator = mediator;
 
     private static InvalidCastException IdentifierException => new("Couldn't cast Message.Identifier");
 
     public async Task SendAsync<TResponse, TMessage, TIdentifier>(TResponse response)
         where TResponse : IRealtimeResponse<TMessage, TIdentifier>
+        where TMessage : IRealtimeMessage
     {
         var task = response.Recipients switch
         {
@@ -31,10 +41,24 @@ public class RealtimeMessageSender<THub>(IHubContext<THub> context) : IRealtimeM
         };
 
         await task;
+
+        using (var scope = _scope.BeginLifetimeScope())
+        {
+            var sideEffect = _scope.ResolveOptional(typeof(IRealtimeSideEffect<TMessage>),
+                new TypedParameter(typeof(TMessage), response.Message));
+
+            if (sideEffect is null)
+            {
+                return;
+            }
+
+            await _mediator.Publish(sideEffect);
+        }
     }
 
     public async Task SendAllAsync<TResponse, TMessage>(TResponse response)
         where TResponse : IRealtimeResponse<TMessage>
+        where TMessage : IRealtimeMessage
     {
         await _context.Clients.All.SendAsync(response.Method, response.Message);
     }
