fd52b7a47e1b83a911aa3c072245e1df6d0fd958
unknown <sabgasim@gmail.com>
2025-05-23 06:02:25 +0300

    feat(api): add GetAllGroupMessagesEndpoint

---
+++ src/Presentation/Api/Modules/Chat/Endpoints/GetAllGroupMessagesEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using FastEndpoints;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Modules.Chat.Application.Contracts;
+using SocialMediaBackend.Modules.Chat.Application.GroupMessaging.GetAllGroupMessages;
+
+namespace SocialMediaBackend.Api.Modules.Chat.Endpoints;
+
+[HttpGet(ApiEndpoints.GroupChat.GetAllMessages)]
+public class GetAllGroupMessagesEndpoint(IChatModule module)
+    : RequestEndpoint<GetAllGroupMessagesRequest, GetAllGroupMessagesResponse>(module)
+{
+    public override async Task HandleAsync(GetAllGroupMessagesRequest req, CancellationToken ct)
+    {
+        await HandleQueryAsync(new GetAllGroupMessagesQuery(req.ChatId, req.Page, req.PageSize), ct);
+    }
+}
