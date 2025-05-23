f17b2a3d58b9b49e145130dd4cf3f19fbe0d147d
unknown <sabgasim@gmail.com>
2025-05-23 06:00:39 +0300

    feat(api): add DeleteGroupMessageForEveryoneEndpoint

--- src/Modules/Chat/Application/GroupMessaging/DeleteGroupMessageForEveryone/DeleteGroupMessageForEveryoneRequest.cs
+++ src/Modules/Chat/Application/GroupMessaging/DeleteGroupMessageForEveryone/DeleteGroupMessageForEveryoneRequest.cs
@@ -1,10 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace SocialMediaBackend.Modules.Chat.Application.GroupMessaging.DeleteGroupMessageForEveryone;
 
-namespace SocialMediaBackend.Modules.Chat.Application.GroupMessaging.DeleteGroupMessageForEveryone;
-internal class DeleteGroupMessageForEveryoneRequest
-{
-}
+public record DeleteGroupMessageForEveryoneRequest(Guid ChatId, Guid MessageId);
---
+++ src/Presentation/Api/Modules/Chat/Endpoints/DeleteGroupMessageForEveryoneEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using FastEndpoints;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Api.Services;
+using SocialMediaBackend.Modules.Chat.Application.Contracts;
+using SocialMediaBackend.Modules.Chat.Application.GroupMessaging.DeleteGroupMessageForEveryone;
+using SocialMediaBackend.Modules.Chat.Application.Hubs;
+
+namespace SocialMediaBackend.Api.Modules.Chat.Endpoints;
+
+[HttpDelete(ApiEndpoints.GroupChat.DeleteMessageForEveryone)]
+public class DeleteGroupMessageForEveryoneEndpoint(
+    IChatModule module,
+    IRealtimeMessageSender<ChatHub> sender)
+    : RealtimeEndpoint<DeleteGroupMessageForEveryoneRequest, DeleteGroupMessageMessage, ChatHub>(module, sender)
+{
+    public override async Task HandleAsync(DeleteGroupMessageForEveryoneRequest req, CancellationToken ct)
+    {
+        await HandleGroupCommandAsync(new DeleteGroupMessageForEveryoneCommand(req.ChatId, req.MessageId), ct);
+    }
+}
