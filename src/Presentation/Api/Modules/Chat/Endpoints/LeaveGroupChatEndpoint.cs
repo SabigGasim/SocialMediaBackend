081727605b7e52844cda69c1c0c91ff951b01bb0
unknown <sabgasim@gmail.com>
2025-05-24 08:04:31 +0300

    feat(api): add LeaveGroupChatEndpoint

---
+++ src/Presentation/Api/Modules/Chat/Endpoints/LeaveGroupChatEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using FastEndpoints;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Api.Services;
+using SocialMediaBackend.Modules.Chat.Application.Contracts;
+using SocialMediaBackend.Modules.Chat.Application.Conversations.GroupMessaging.LeaveGroupChat;
+using SocialMediaBackend.Modules.Chat.Application.Hubs;
+
+namespace SocialMediaBackend.Api.Modules.Chat.Endpoints;
+
+public class LeaveGroupChatEndpoint(
+    IChatModule module,
+    IRealtimeMessageSender<ChatHub> sender)
+    : RealtimeEndpoint<LeaveGroupChatRequest, GroupChatLeftMessage, ChatHub>(module, sender)
+{
+    public override void Configure()
+    {
+        Delete(ApiEndpoints.GroupChat.LeaveGroupChat);
+        Description(x => x.Accepts<LeaveGroupChatRequest>());
+    }
+
+    public override async Task HandleAsync(LeaveGroupChatRequest req, CancellationToken ct)
+    {
+        await HandleGroupCommandAsync(new LeaveGroupChatCommand(req.ChatId), ct);
+    }
+}
