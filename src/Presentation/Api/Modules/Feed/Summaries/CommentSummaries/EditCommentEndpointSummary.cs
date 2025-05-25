e258f0f8bb0dea7089c0a5df8e9b884a7ff239c5
unknown <sabgasim@gmail.com>
2025-05-25 20:06:55 +0300

    feat(api): add EditCommentEndpointSummary

---
+++ src/Presentation/Api/Modules/Feed/Summaries/CommentSummaries/EditCommentEndpointSummary.cs
@@ -0,0 +1,18 @@
+﻿using FastEndpoints;
+using SocialMediaBackend.Api.Modules.Feed.Endpoints.Comments;
+
+namespace SocialMediaBackend.Api.Modules.Feed.Summaries.CommentSummaries;
+
+public class EditCommentEndpointSummary : Summary<EditCommentEndpoint>
+{
+    public EditCommentEndpointSummary()
+    {
+        Summary = "Edit a comment";
+        Description = "Allows users to edit their existing comments.";
+        Response(204, "Comment edited successfully.");
+        Response<ErrorResponse>(404, "Comment not found.");
+        Response<ErrorResponse>(401, "User is not authenticated.");
+        Response<ErrorResponse>(403, "User is not authorized to view this comment.");
+        Response<ErrorResponse>(400, "Invalid request parameters.");
+    }
+}
