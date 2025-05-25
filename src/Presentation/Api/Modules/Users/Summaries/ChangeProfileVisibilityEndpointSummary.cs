59aa245f4a98096636a0d830f33aba7889758a89
unknown <sabgasim@gmail.com>
2025-05-25 18:23:47 +0300

    feat(api): add ChangeProfileVisibilityEndpointSummary

---
+++ src/Presentation/Api/Modules/Users/Summaries/ChangeProfileVisibilityEndpointSummary.cs
@@ -0,0 +1,16 @@
+﻿using FastEndpoints;
+using SocialMediaBackend.Api.Modules.Users.Endpoints.Users.Privacy;
+
+namespace SocialMediaBackend.Api.Modules.Users.Summaries;
+
+public class ChangeProfileVisibilityEndpointSummary : Summary<ChangeProfileVisibilityEndpoint>  
+{
+    public ChangeProfileVisibilityEndpointSummary()
+    {
+        Summary = "Change the visibility of the user's profile";
+        Description = "Allows the user to change their profile visibility settings, making their profile either public or private.";
+        Response(204, "Profile visibility changed successfully.");
+        Response(400, "Bad Request - The request was invalid or could not be processed.");
+        Response(401, "Unauthorized - The request is not authenticated.");
+    }
+}
