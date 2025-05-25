5ccdbce9ffe25ec5ebccb1c9a6cbc336e9c6b4c0
unknown <sabgasim@gmail.com>
2025-05-25 17:59:43 +0300

    feat(api): add GetThisUserEndpointSummary

---
+++ src/Presentation/Api/Modules/Users/Summaries/GetThisUserEndpiontSummary.cs
@@ -0,0 +1,16 @@
+﻿using FastEndpoints;
+using SocialMediaBackend.Api.Modules.Users.Endpoints.Users;
+using SocialMediaBackend.Modules.Users.Application.Users.GetFullUserDetails;
+
+namespace SocialMediaBackend.Api.Modules.Users.Summaries;
+
+public class GetThisUserEndpiontSummary : Summary<GetThisUserEndpiont>
+{
+    public GetThisUserEndpiontSummary()
+    {
+        Summary = "Get the details of the currently authenticated user";
+        Description = "Retrieves the full details of the user who is currently authenticated, including their profile information and any other relevant data.";
+        Response<GetFullUserDetailsResponse>(200, "The details of the authenticated user.");
+        Response(401, "Unauthorized - The request is not authenticated.");
+    }
+}
