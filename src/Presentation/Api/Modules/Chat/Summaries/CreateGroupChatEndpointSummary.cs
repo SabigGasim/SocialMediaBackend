5d1ab1118149ad1a81b728afadfe29011018538c
unknown <sabgasim@gmail.com>
2025-05-25 20:52:19 +0300

    feat(api): add CreateGroupChatEndpointSummary

---
+++ src/Presentation/Api/Modules/Chat/Summaries/CreateGroupChatEndpointSummary.cs
@@ -0,0 +1,17 @@
+﻿using FastEndpoints;
+using SocialMediaBackend.Api.Modules.Chat.Endpoints;
+using SocialMediaBackend.Modules.Chat.Application.Conversations.GroupMessaging.CreateGroupChat;
+
+namespace SocialMediaBackend.Api.Modules.Chat.Summaries;
+
+public class CreateGroupChatEndpointSummary : Summary<CreateGroupChatEndpoint>
+{
+    public CreateGroupChatEndpointSummary()
+    {
+        Summary = "Create a new group chat";
+        Description = "This endpoint allows users to create a new group chat by providing the necessary details.";
+        Response<CreateGroupChatResponse>(201, "Group chat created successfully.");
+        Response<ErrorResponse>(400, "Bad request. Invalid input data.");
+        Response<ErrorResponse>(401, "User is not authenticated.");
+    }
+}
