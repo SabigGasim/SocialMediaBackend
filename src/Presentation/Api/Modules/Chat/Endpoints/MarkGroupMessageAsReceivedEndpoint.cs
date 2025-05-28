ba5eee7dbfcae96b2a8f0253bf945103148c8bdc
SabigGasim <sabgasim@gmail.com>
2025-05-28 09:12:23 +0300

    feat(api): add MarkGroupMessageAsReceivedEndpoint

---
+++ src/Presentation/Api/Modules/Chat/Endpoints/MarkGroupMessageAsReceivedEndpoint.cs
@@ -0,0 +1,14 @@
+﻿using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Modules.Chat.Application.Contracts;
+using SocialMediaBackend.Modules.Chat.Application.Conversations.GroupMessaging.MarkGroupMessageAsReceived;
+
+namespace SocialMediaBackend.Api.Modules.Chat.Endpoints;
+
+public class MarkGroupMessageAsReceivedEndpoint(IChatModule module)
+    : RequestEndpoint<MarkGroupMessageAsReceivedRequest>(module)
+{
+    public override async Task HandleAsync(MarkGroupMessageAsReceivedRequest req, CancellationToken ct)
+    {
+        await HandleCommandAsync(new MarkGroupMessageAsReceivedCommand(req.ChatId, req.MessageId), ct);
+    }
+}
