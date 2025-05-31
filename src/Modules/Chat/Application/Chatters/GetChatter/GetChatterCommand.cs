dfae593be51d3b60ae602956176097d0b8a35fe4
SabigGasim <sabgasim@gmail.com>
2025-05-31 17:52:39 +0300

    feat(chat.app): add GetChatter feature

---
+++ src/Modules/Chat/Application/Chatters/GetChatter/GetChatterCommand.cs
@@ -0,0 +1,9 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.Modules.Chat.Domain.Chatters;
+
+namespace SocialMediaBackend.Modules.Chat.Application.Chatters.GetChatter;
+
+public class GetChatterCommand(Guid chatterId) : CommandBase<GetChatterResponse>
+{
+    public ChatterId ChatterId { get; } = new(chatterId);
+}
---
+++ src/Modules/Chat/Application/Chatters/GetChatter/GetChatterCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application;
+using SocialMediaBackend.BuildingBlocks.Application.Requests;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.Modules.Chat.Application.Mappings;
+using SocialMediaBackend.Modules.Chat.Infrastructure.Domain.Chatters;
+
+namespace SocialMediaBackend.Modules.Chat.Application.Chatters.GetChatter;
+
+public class GetChatterCommandHandler(IChatterRepository chatterRepository)
+    : ICommandHandler<GetChatterCommand, GetChatterResponse>
+{
+    private readonly IChatterRepository _chatterRepository = chatterRepository;
+
+    public async Task<HandlerResponse<GetChatterResponse>> ExecuteAsync(GetChatterCommand command, CancellationToken ct)
+    {
+        var chatter = await _chatterRepository.GetByIdAsync(command.ChatterId, ct); ;
+        if (chatter is null)
+        {
+            return ("Chatter with the given Id was not found", HandlerResponseStatus.NotFound, command.ChatterId.Value);
+        }
+
+        return chatter.MapToResponse();
+    }
+}
