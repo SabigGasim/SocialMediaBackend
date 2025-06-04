3f2cdf09496fe9355f1b84f1cb958f6fd78ab325
SabigGasim <sabgasim@gmail.com>
2025-06-04 17:27:51 +0300

    feat(payments.infra): add initial migration for PaymentsDbContext

--- src/Database/DatabaseMigrator/Program.cs
+++ src/Database/DatabaseMigrator/Program.cs
@@ -4,6 +4,8 @@ using SocialMediaBackend.Modules.Feed.Infrastructure.Configuration.Persistence;
 using SocialMediaBackend.Modules.Feed.Infrastructure.Data;
 using SocialMediaBackend.Modules.Users.Infrastructure.Configuration.Persistence;
 using SocialMediaBackend.Modules.Users.Infrastructure.Data;
+using SocialMediaBackend.Modules.Payments.Infrastructure.Configuration.Persistence;
+using SocialMediaBackend.Modules.Payments.Infrastructure.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,6 +16,8 @@ builder.Services.AddDbContext<UsersDbContext>(builder => builder.ConfigureUsersO
 builder.Services.AddDbContext<FeedDbContext>(builder => builder.ConfigureFeedOptionsBuilder(connectionString));
 
 builder.Services.AddDbContext<ChatDbContext>(builder => builder.ConfigureChatOptionsBuilder(connectionString));
+
+builder.Services.AddDbContext<PaymentsDbContext>(builder => builder.ConfigurePaymentsOptionsBuilder(connectionString));
 
 var app = builder.Build();
 
---
+++ src/Modules/Payments/Infrastructure/Data/Migrations/20250604131200_InitMigration.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialMediaBackend.Modules.Payments.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "payments");
+
+            migrationBuilder.CreateTable(
+                name: "InternalCommands",
+                schema: "payments",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    Data = table.Column<string>(type: "text", nullable: false),
+                    Error = table.Column<string>(type: "text", nullable: true),
+                    Processed = table.Column<bool>(type: "boolean", nullable: false, defaultValue: false),
+                    ProcessedDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true),
+                    EnqueueDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InternalCommands", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InternalCommands_Id",
+                schema: "payments",
+                table: "InternalCommands",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InternalCommands",
+                schema: "payments");
+        }
+    }
+}
