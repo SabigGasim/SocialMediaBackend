b2cee45ac20c37c4eae3005063ab33ab6ec56f46
SabigGasim <sabgasim@gmail.com>
2025-06-12 22:09:28 +0300

    feat(payments.domain): add AggregateRoot base class

---
+++ src/Modules/Payments/Domain/Common/AggregateRoot.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using SocialMediaBackend.BuildingBlocks.Domain;
+
+namespace SocialMediaBackend.Modules.Payments.Domain.Common;
+
+public abstract class AggregateRoot : IStreamAggregate
+{
+    private List<IDomainEvent>? _domainEvents;
+    private readonly List<IStreamEvent> _events = new();
+
+    public Guid Id { get; protected set; }
+    [JsonIgnore]
+    public IReadOnlyCollection<IStreamEvent> Events => _events.AsReadOnly();
+    [JsonIgnore]
+    public IReadOnlyCollection<IDomainEvent>? DomainEvents => _domainEvents?.AsReadOnly();
+
+    protected void AddDomainEvent(IDomainEvent domainEvent)
+    {
+        _domainEvents ??= new();
+        _domainEvents.Add(domainEvent);
+    }
+    public void ClearDomainEvents() => _domainEvents?.Clear();
+
+    protected void AddEvent(IStreamEvent streamEvent) => _events.Add(streamEvent);
+}
