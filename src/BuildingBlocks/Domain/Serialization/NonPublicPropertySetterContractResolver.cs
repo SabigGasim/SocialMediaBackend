bc5e5f1690f1c8b1bee6c0a9fa8e9b203442f9c7
SabigGasim <sabgasim@gmail.com>
2025-06-12 21:35:53 +0300

    feat(buildingblocks): add NonPublicPropertySetterContractResolver

---
+++ src/BuildingBlocks/Domain/Serialization/NonPublicPropertySetterContractResolver.cs
@@ -0,0 +1,20 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Reflection;
+
+namespace SocialMediaBackend.BuildingBlocks.Domain.Serialization;
+
+public class NonPublicPropertySetterContractResolver : DefaultContractResolver
+{
+    protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+    {
+        var property = base.CreateProperty(member, memberSerialization);
+
+        if (member is PropertyInfo propInfo && propInfo.GetSetMethod(true) is not null)
+        {
+            property.Writable = true;
+        }
+
+        return property;
+    }
+}
