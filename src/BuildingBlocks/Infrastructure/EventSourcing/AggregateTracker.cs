967d1d39a1c673bedd71f8214251b439856f7cef
SabigGasim <sabgasim@gmail.com>
2025-06-12 22:06:16 +0300

    feat(buildingblocks): add AggregateTracker for marten

---
+++ src/BuildingBlocks/Infrastructure/EventSourcing/AggregateTracker.cs
@@ -0,0 +1,26 @@
+﻿using SocialMediaBackend.BuildingBlocks.Domain;
+
+namespace SocialMediaBackend.BuildingBlocks.Infrastructure.EventSourcing;
+
+public class AggregateTracker : IAggregateTracker
+{
+    private readonly List<IStreamAggregate> _trackedAggregates = new();
+
+    public void Track<TAggregate>(TAggregate aggregate) where TAggregate : IStreamAggregate
+    {
+        if (!_trackedAggregates.Contains(aggregate))
+        {
+            _trackedAggregates.Add(aggregate);
+        }
+    }
+
+    public void ClearTrackedAggregates()
+    {
+        _trackedAggregates.Clear();
+    }
+
+    public IReadOnlyCollection<IStreamAggregate> GetTrackedAggregates()
+    {
+        return _trackedAggregates.AsReadOnly();
+    }
+}
---
+++ src/BuildingBlocks/Infrastructure/EventSourcing/IAggregateTracker.cs
@@ -0,0 +1,10 @@
+﻿using SocialMediaBackend.BuildingBlocks.Domain;
+
+namespace SocialMediaBackend.BuildingBlocks.Infrastructure.EventSourcing;
+
+public interface IAggregateTracker
+{
+    void Track<TAggregate>(TAggregate aggregate) where TAggregate : IStreamAggregate;
+    IReadOnlyCollection<IStreamAggregate> GetTrackedAggregates();
+    void ClearTrackedAggregates();
+}
