4d67f671f58386194e77b90c3814bb2c96f1d749
SabigGasim <sabgasim@gmail.com>
2025-06-13 19:32:17 +0300

    feat(api): add StripeWebhooksEndpoint

---
+++ src/Modules/Payments/Application/Stripe/WebhookEventReceived/StripeWebhookEventReceivedCommand.cs
@@ -0,0 +1,12 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+
+namespace SocialMediaBackend.Modules.Payments.Application.Stripe.WebhookEventReceived;
+
+public class StripeWebhookEventReceivedCommand : CommandBase
+{
+    public string EventType { get; set; } = default!;
+    public string EventId { get; set; } = default!;
+    public string Data { get; set; } = default!;
+    public string Signature { get; set; } = default!;
+    public string WebHookSecret { get; set; } = default!;
+}
---
+++ src/Presentation/Api/Modules/Payments/Endpoints/StripeWebHooksEndpoint.cs
@@ -0,0 +1,67 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Options;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Modules.Payments.Application.Contracts;
+using SocialMediaBackend.Modules.Payments.Application.Stripe.WebhookEventReceived;
+using SocialMediaBackend.Modules.Payments.Infrastructure;
+using Stripe;
+using System.Buffers;
+using System.Net;
+using System.Text;
+
+namespace SocialMediaBackend.Api.Modules.Payments.Endpoints;
+
+[HttpPost(ApiEndpoints.Payments.StripeWebHooks)]
+[AllowAnonymous]
+public class StripeWebHooksEndpoint : RequestEndpoint
+{
+    private readonly StripeSettings _stripeSettings;
+    private readonly IPaymentsModule _module;
+
+    public StripeWebHooksEndpoint(IOptions<StripeSettings> stripeSettings, IPaymentsModule module) 
+        : base(module)
+    {
+        _stripeSettings = stripeSettings.Value;
+        _module = module;
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        string jsonString;
+        using (var reader = new StreamReader(HttpContext.Request.Body, Encoding.UTF8))
+        {
+            jsonString = await reader.ReadToEndAsync(ct);
+        }
+
+        try
+        {
+            string signature = HttpContext.Request.Headers["Stripe-Signature"]!;
+
+            var stripeEvent = EventUtility.ConstructEvent(
+                jsonString,
+                signature,
+                _stripeSettings.WebHookSecret
+            );
+
+            var command = new StripeWebhookEventReceivedCommand
+            {
+                EventType = stripeEvent.Type,
+                EventId = stripeEvent.Id,
+                Data = jsonString,
+                Signature = signature,
+                WebHookSecret = _stripeSettings.WebHookSecret
+            };
+
+            await _module.ExecuteCommandAsync(command, ct);
+
+            await SendOkAsync(ct);
+        }
+        catch (StripeException e)
+        {
+            AddError(e.Message, HttpStatusCode.BadRequest.ToString());
+            await SendErrorsAsync((int)HttpStatusCode.BadRequest, cancellation: ct);
+        }
+    }
+
+}
