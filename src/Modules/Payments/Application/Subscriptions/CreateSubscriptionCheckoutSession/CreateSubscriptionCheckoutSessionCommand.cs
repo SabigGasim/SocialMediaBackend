1731da7fb1b8d6ca27988c176f9c823e6bab3562
SabigGasim <sabgasim@gmail.com>
2025-06-23 02:03:37 +0300

    feat(payments.app): add CreateSubscriptionCheckoutSession feature

---
+++ src/Modules/Payments/Application/Subscriptions/CreateSubscriptionCheckoutSession/CreateSubscriptionCheckoutSessionCommand.cs
@@ -0,0 +1,20 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.Modules.Payments.Contracts.Gateway;
+using SocialMediaBackend.Modules.Payments.Domain.Payers;
+using SocialMediaBackend.Modules.Payments.Domain.Products;
+
+namespace SocialMediaBackend.Modules.Payments.Application.Subscriptions.CreateSubscriptionCheckoutSession;
+
+public class CreateSubscriptionCheckoutSessionCommand(
+    PayerId payerId,
+    string productReference,
+    ProductPriceId priceId,
+    string successUrl,
+    string cancelUrl) : CommandBase<CreateCheckoutSessionResponse>
+{
+    public PayerId PayerId { get; } = payerId;
+    public string ProductReference { get; } = productReference;
+    public ProductPriceId PriceId { get; } = priceId;
+    public string SuccessUrl { get; } = successUrl;
+    public string CancelUrl { get; } = cancelUrl;
+}
---
+++ src/Modules/Payments/Application/Subscriptions/CreateSubscriptionCheckoutSession/CreateSubscriptionCheckoutSessionCommandHandler.cs
@@ -0,0 +1,43 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application.Requests;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.EventSourcing;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.Exceptions;
+using SocialMediaBackend.Modules.Payments.Contracts.Gateway;
+using SocialMediaBackend.Modules.Payments.Domain.Payers;
+using SocialMediaBackend.Modules.Payments.Domain.Products;
+using SocialMediaBackend.Modules.Payments.Domain.Subscriptions;
+using SocialMediaBackend.Modules.Payments.Infrastructure.Gateway;
+
+namespace SocialMediaBackend.Modules.Payments.Application.Subscriptions.CreateSubscriptionCheckoutSession;
+
+public class CreateSubscriptionCheckoutSessionCommandHandler(
+    IAggregateRepository repository,
+    IPaymentGateway paymentGateway)
+    : ICommandHandler<CreateSubscriptionCheckoutSessionCommand, CreateCheckoutSessionResponse>
+{
+    private readonly IAggregateRepository _repository = repository;
+    private readonly IPaymentGateway _paymentGateway = paymentGateway;
+
+    public async Task<HandlerResponse<CreateCheckoutSessionResponse>> ExecuteAsync(CreateSubscriptionCheckoutSessionCommand command, CancellationToken ct)
+    {
+        var payer = await _repository.LoadAsync<Payer>(command.PayerId.Value, ct);
+        var product = await _repository.LoadAsync<Product>(x => x.Reference == command.ProductReference, ct);
+
+        NotFoundException.ThrowIfNull(payer, product);
+
+        var subscription = Subscription.Initiate(command.PayerId, command.ProductReference);
+
+        _repository.StartStream(subscription);
+
+        var (_, gatwayPriceId, _) = product.Prices.First(x => x.Id == command.PriceId);
+
+        return await _paymentGateway.CreateSubscriptionCheckoutSessionAsync(
+            payer.GatewayCustomerId,
+            product.Reference,
+            gatwayPriceId,
+            command.SuccessUrl,
+            command.CancelUrl,
+            subscription.Id,
+            ct);
+    }
+}
