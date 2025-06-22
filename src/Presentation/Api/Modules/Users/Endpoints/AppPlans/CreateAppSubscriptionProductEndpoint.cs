a54dda87860cac42e713252e14aa3d1ba4da7c6d
SabigGasim <sabgasim@gmail.com>
2025-06-23 02:15:33 +0300

    feat(api): add CreateAppSubscriptionProductEndpoint

---
+++ src/Presentation/Api/Modules/Users/Endpoints/AppPlans/CreateAppSubscriptionProductEndpoint.cs
@@ -0,0 +1,16 @@
+﻿using FastEndpoints;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Modules.Users.Application.AppPlans.CreateAppSubscriptionProduct;
+using SocialMediaBackend.Modules.Users.Application.Contracts;
+
+namespace SocialMediaBackend.Api.Modules.Users.Endpoints.AppPlans;
+
+[HttpPost(ApiEndpoints.Payments.CreateAppSubscriptionProduct)]
+public class CreateAppSubscriptionProductEndpoint(IUsersModule module)
+    : RequestEndpoint<CreateAppSubscriptionRequest>(module)
+{
+    public override async Task HandleAsync(CreateAppSubscriptionRequest req, CancellationToken ct)
+    {
+        await HandleCommandAsync(new CreateAppSubscriptionProductCommand(req.Tier), ct);
+    }
+}
