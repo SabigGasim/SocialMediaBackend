03f31abca91ca7c34cbf882ff2eac8f872cc2fc7
SabigGasim <sabgasim@gmail.com>
2025-06-23 02:17:01 +0300

    feat(api): add SubscribeToAppPlanEndpoint

--- src/Presentation/Api/ApiEndpoints.cs
+++ src/Presentation/Api/ApiEndpoints.cs
@@ -104,12 +104,14 @@ public static class ApiEndpoints
 
     public static class Payments
     {
-        private const string Base = $"{ApiBase}/payments";
+        public const string Base = $"{ApiBase}/payments";
         public const string CreatePaymentIntent = $"{Base}/create-payment-intent";
         public const string StripeWebHooks = $"{ApiBase}/webhooks/stripe";
         public const string GetPaymentStatus = $"{Base}/status/{{PaymentId}}";
         public const string GetPayer = $"{Base}/payers/{{PayerId}}";
 
         public const string SubscriteToAppPlan = $"{Base}/subscribe-to-app-plan";
+        public const string CreateAppPlan = $"{Base}/create-app-plan";
+        public const string CreateAppSubscriptionProduct = $"{Base}/create-app-sub-product";
     }
 }
---
+++ src/Presentation/Api/Modules/Users/Endpoints/AppPlans/SubscribeToAppPlanEndpoint.cs
@@ -0,0 +1,17 @@
+﻿using FastEndpoints;
+using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Modules.Payments.Contracts.Gateway;
+using SocialMediaBackend.Modules.Users.Application.AppPlans.SubscribeToAppPlan;
+using SocialMediaBackend.Modules.Users.Application.Contracts;
+
+namespace SocialMediaBackend.Api.Modules.Users.Endpoints.AppPlans;
+
+[HttpPost(ApiEndpoints.Payments.SubscriteToAppPlan)]
+public class SubscribeToAppPlanEndpoint(IUsersModule module)
+    : RequestEndpoint<SubscribeToAppPlanRequest, CreateCheckoutSessionResponse>(module)
+{
+    public override async Task HandleAsync(SubscribeToAppPlanRequest req, CancellationToken ct)
+    {
+        await HandleCommandAsync(new SubscribeToAppPlanCommand(req.Tier, req.Interval), ct);
+    }
+}
