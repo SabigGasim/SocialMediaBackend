f65cb25450aa4380f4b68e7ff239fbd4771d9a94
SabigGasim <sabgasim@gmail.com>
2025-06-23 02:16:07 +0300

    feat(users.app): add CreateAppPlan feature

---
+++ src/Modules/Users/Application/AppPlans/CreateAppPlan/CreateAppPlanCommand.cs
@@ -0,0 +1,32 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application.Requests;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.Modules.Payments.Contracts;
+using SocialMediaBackend.Modules.Users.Domain.AppPlan;
+
+namespace SocialMediaBackend.Modules.Users.Application.AppPlans.CreateAppPlan;
+
+public class CreateAppPlanCommand : CommandBase, IUserRequest
+{
+    public CreateAppPlanCommand(IEnumerable<PriceRequest> prices, string tier)
+    {
+        Prices = prices.Select(x =>
+        {
+            var currency = Enum.Parse<Currency>(x.Currency, ignoreCase: true);
+            var interval = Enum.Parse<PaymentInterval>(x.Interval, ignoreCase: true);
+
+            return new ProductPrice(new(x.Amount, currency), interval);
+        })
+            .ToArray();
+
+        Tier = Enum.Parse<AppSubscriptionTier>(tier, ignoreCase: true);
+    }
+
+    public ProductPrice[] Prices { get; private set; }
+    public AppSubscriptionTier Tier { get; private set; }
+
+    public Guid UserId { get; private set; }
+    public bool IsAdmin { get; private set; }
+
+    public void WithAdminRole(bool isAdmin) => IsAdmin = isAdmin;
+    public void WithUserId(Guid userId) => UserId = userId;
+}
