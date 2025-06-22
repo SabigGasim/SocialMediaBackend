08bcfcd3897e33701e03915dbe5d7f87d92de184
SabigGasim <sabgasim@gmail.com>
2025-06-23 02:01:05 +0300

    feat(payments.app): add CreateProduct feature

---
+++ src/Modules/Payments/Application/Products/CreateProduct/CreateProductCommand.cs
@@ -0,0 +1,13 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.Modules.Payments.Domain.Products;
+
+namespace SocialMediaBackend.Modules.Payments.Application.Products.CreateProduct;
+
+public class CreateProductCommand(string productReference, string name, string description, string owner)
+    : CommandBase<ProductId>
+{
+    public string ProductReference { get; } = productReference;
+    public string Name { get; } = name;
+    public string Description { get; } = description;
+    public string Owner { get; } = owner;
+}
---
+++ src/Modules/Payments/Application/Products/CreateProduct/CreateProductCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application.Requests;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.EventSourcing;
+using SocialMediaBackend.Modules.Payments.Domain.Products;
+using SocialMediaBackend.Modules.Payments.Infrastructure;
+
+namespace SocialMediaBackend.Modules.Payments.Application.Products.CreateProduct;
+
+public class CreateProductCommandHandler(
+    IPaymentService paymentService,
+    IAggregateRepository repository)
+    : ICommandHandler<CreateProductCommand, ProductId>
+{
+    private readonly IPaymentService _paymentService = paymentService;
+    private readonly IAggregateRepository _repository = repository;
+
+    public async Task<HandlerResponse<ProductId>> ExecuteAsync(CreateProductCommand command, CancellationToken ct)
+    {
+        var gatewayProductId = await _paymentService.CreateProductAsync(
+            command.ProductReference, 
+            command.Name, 
+            command.Description
+            );
+
+        var product = Product.Create(
+            command.ProductReference,
+            gatewayProductId,
+            command.Owner
+            );
+
+        _repository.StartStream(product);
+
+        return new ProductId(product.Id);
+    }
+}
