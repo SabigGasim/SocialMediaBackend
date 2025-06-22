6d3320062d29544a08986d1a0c39eb32afaa393c
SabigGasim <sabgasim@gmail.com>
2025-06-23 02:13:14 +0300

    feat(users.domain): add AppSubscriptionPlan

---
+++ src/Modules/Users/Domain/AppPlan/AppSubscriptionPlan.cs
@@ -0,0 +1,21 @@
+﻿using SocialMediaBackend.BuildingBlocks.Domain;
+using SocialMediaBackend.Modules.Payments.Contracts;
+
+namespace SocialMediaBackend.Modules.Users.Domain.AppPlan;
+
+public class AppSubscriptionPlan : Entity<AppSubscriptionPlanId>
+{
+    public ProductPrice Price { get; init; } = default!;
+
+    private AppSubscriptionPlan() { }
+    public AppSubscriptionPlan(AppSubscriptionPlanId priceId, ProductPrice price)
+    {
+        this.Id = priceId;
+        this.Price = price;
+    }
+
+    internal static AppSubscriptionPlan Create(AppSubscriptionPlanId planId, ProductPrice price)
+    {
+        return new AppSubscriptionPlan(planId, price);
+    }
+}
---
+++ src/Modules/Users/Domain/AppPlan/AppSubscriptionPlanId.cs
@@ -0,0 +1,5 @@
+﻿using SocialMediaBackend.BuildingBlocks.Domain;
+
+namespace SocialMediaBackend.Modules.Users.Domain.AppPlan;
+
+public record AppSubscriptionPlanId(Guid Value) : TypedIdValueBase<Guid>(Value);
