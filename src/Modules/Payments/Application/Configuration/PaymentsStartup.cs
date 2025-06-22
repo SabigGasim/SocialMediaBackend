71308b43bd6999ead8cb7287d084c097be20d792
SabigGasim <sabgasim@gmail.com>
2025-06-23 02:07:10 +0300

    feat(payments): add PaymentsAutofacModule

--- src/Modules/Payments/Application/Configuration/PaymentsStartup.cs
+++ src/Modules/Payments/Application/Configuration/PaymentsStartup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using SocialMediaBackend.Modules.Payments.Application.Configuration.Mediator;
 using SocialMediaBackend.Modules.Payments.Infrastructure.Configuration;
+using SocialMediaBackend.Modules.Payments.Infrastructure.Configuration.Payments;
 using SocialMediaBackend.Modules.Payments.Infrastructure.Configuration.Persistence;
 using SocialMediaBackend.Modules.Payments.Infrastructure.Configuration.Processing;
 using SocialMediaBackend.Modules.Payments.Infrastructure.Configuration.Quartz;
@@ -33,6 +34,7 @@ public static class PaymentsStartup
         builder.RegisterModule(new ProcessingModule());
         builder.RegisterModule(new QuartzModule());
         builder.RegisterModule(new CQRSModule());
+        builder.RegisterModule(new PaymentsAutofacModule());
 
         var container = builder.Build();
 
---
+++ src/Modules/Payments/Infrastructure/Configuration/Payments/PaymentsAutofacModule.cs
@@ -0,0 +1,18 @@
+﻿using Autofac;
+using SocialMediaBackend.Modules.Payments.Infrastructure.Gateway;
+
+namespace SocialMediaBackend.Modules.Payments.Infrastructure.Configuration.Payments;
+
+public class PaymentsAutofacModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        builder.RegisterType<StripePaymentService>()
+            .As<IPaymentService>()
+            .SingleInstance();
+
+        builder.RegisterType<StripeGateway>()
+            .As<IPaymentGateway>()
+            .SingleInstance();
+    }
+}
