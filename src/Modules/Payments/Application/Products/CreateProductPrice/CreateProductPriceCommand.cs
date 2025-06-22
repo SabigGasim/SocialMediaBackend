1582e6bdaef8fa073afaa5598c10541e0f11245f
SabigGasim <sabgasim@gmail.com>
2025-06-23 02:01:26 +0300

    feat(payments.app): add CreateProductPrice feature

---
+++ src/Modules/Payments/Application/Products/CreateProductPrice/CreateProductPriceCommand.cs
@@ -0,0 +1,11 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.Modules.Payments.Contracts;
+using SocialMediaBackend.Modules.Payments.Domain.Products;
+
+namespace SocialMediaBackend.Modules.Payments.Application.Products.CreateProductPrice;
+
+public class CreateProductPriceCommand(Guid productId, ProductPrice productPrice) : CommandBase<ProductPriceId>
+{
+    public Guid ProductId { get; } = productId;
+    public ProductPrice ProductPrice { get; } = productPrice;
+}
---
+++ src/Modules/Payments/Application/Products/CreateProductPrice/CreateProductPriceCommandHandler.cs
@@ -0,0 +1,33 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application.Requests;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.EventSourcing;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.Exceptions;
+using SocialMediaBackend.Modules.Payments.Domain.Products;
+using SocialMediaBackend.Modules.Payments.Infrastructure;
+
+namespace SocialMediaBackend.Modules.Payments.Application.Products.CreateProductPrice;
+
+public class CreateProductPriceCommandHandler(
+    IAggregateRepository repository,
+    IPaymentService paymentService)
+    : ICommandHandler<CreateProductPriceCommand, ProductPriceId>
+{
+    private readonly IAggregateRepository _repository = repository;
+    private readonly IPaymentService _paymentService = paymentService;
+
+    public async Task<HandlerResponse<ProductPriceId>> ExecuteAsync(CreateProductPriceCommand command, CancellationToken ct)
+    {
+        var product = await _repository.LoadAsync<Product>(command.ProductId, ct);
+
+        NotFoundException.ThrowIfNull(nameof(product), product);
+
+        var gatewayPriceId = await _paymentService.CreatePriceAsync(
+            product.GatewayProductId,
+            command.ProductPrice
+            );
+
+        var priceId = product.AddPrice(gatewayPriceId, command.ProductPrice);
+
+        return priceId;
+    }
+}
