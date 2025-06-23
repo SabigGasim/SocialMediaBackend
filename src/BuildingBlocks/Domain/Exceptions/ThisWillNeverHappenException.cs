97a43d0c86715ae3023ba926496b14c5dcc53440
SabigGasim <sabgasim@gmail.com>
2025-06-23 10:22:21 +0300

    feat: add ThisWillNeverHappenException

---
+++ src/BuildingBlocks/Domain/Exceptions/ThisWillNeverHappenException.cs
@@ -0,0 +1,7 @@
+﻿namespace SocialMediaBackend.BuildingBlocks.Domain.Exceptions;
+
+public class ThisWillNeverHappenException : Exception
+{
+    public ThisWillNeverHappenException() { }
+    public ThisWillNeverHappenException(string message) : base(message) { }
+}
