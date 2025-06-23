de232f8628191ea37e5998d16619f1218f98559e
SabigGasim <sabgasim@gmail.com>
2025-06-23 10:27:47 +0300

    feat(payments.app): add MarkSubscriptionAsIncomplete feature

---
+++ src/Modules/Payments/Application/Subscriptions/MarkSubscriptionAsIncomplete/MarkSubscriptionAsIncompleteCommand.cs
@@ -0,0 +1,10 @@
+﻿using SocialMediaBackend.BuildingBlocks.Infrastructure.InternalCommands;
+
+namespace SocialMediaBackend.Modules.Payments.Application.Subscriptions.MarkSubscriptionAsIncomplete;
+
+public class MarkSubscriptionAsIncompleteCommand(Guid internalSubscriptionId, string eventJson, Guid id = default)
+    : InternalCommandBase(id)
+{
+    public Guid InternalSubscriptionId { get; } = internalSubscriptionId;
+    public string EventJson { get; } = eventJson;
+}
---
+++ src/Modules/Payments/Application/Subscriptions/MarkSubscriptionAsIncomplete/MarkSubscriptionAsIncompleteCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application;
+using SocialMediaBackend.BuildingBlocks.Application.Requests;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.EventSourcing;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.Exceptions;
+using SocialMediaBackend.Modules.Payments.Domain.Subscriptions;
+
+namespace SocialMediaBackend.Modules.Payments.Application.Subscriptions.MarkSubscriptionAsIncomplete;
+
+public class MarkSubscriptionAsIncompleteCommandHandler(IAggregateRepository repository)
+    : ICommandHandler<MarkSubscriptionAsIncompleteCommand>
+{
+    private readonly IAggregateRepository _repository = repository;
+
+    public async Task<HandlerResponse> ExecuteAsync(MarkSubscriptionAsIncompleteCommand command, CancellationToken ct)
+    {
+        var subscription = await _repository.LoadAsync<Subscription>(command.InternalSubscriptionId, ct);
+
+        NotFoundException.ThrowIfNull(nameof(Subscription), subscription);
+
+        subscription.MarkAsIncomplete();
+
+        return HandlerResponseStatus.OK;
+    }
+}
