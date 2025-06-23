a95c90fb9e688a9f2a5d3bcebde32eef9e821dea
SabigGasim <sabgasim@gmail.com>
2025-06-23 10:27:56 +0300

    feat(payments.app): add MarkSubscriptionAsPastDue feature

---
+++ src/Modules/Payments/Application/Subscriptions/MarkSubscriptionAsPastDue/MarkSubscriptionAsPastDueCommand.cs
@@ -0,0 +1,10 @@
+﻿using SocialMediaBackend.BuildingBlocks.Infrastructure.InternalCommands;
+
+namespace SocialMediaBackend.Modules.Payments.Application.Subscriptions.MarkSubscriptionAsPastDue;
+
+public class MarkSubscriptionAsPastDueCommand(Guid internalSubscriptionId, string eventJson, Guid id = default)
+    : InternalCommandBase(id)
+{
+    public Guid InternalSubscriptionId { get; } = internalSubscriptionId;
+    public string EventJson { get; } = eventJson;
+}
---
+++ src/Modules/Payments/Application/Subscriptions/MarkSubscriptionAsPastDue/MarkSubscriptionAsPastDueCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application;
+using SocialMediaBackend.BuildingBlocks.Application.Requests;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.EventSourcing;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.Exceptions;
+using SocialMediaBackend.Modules.Payments.Domain.Subscriptions;
+
+namespace SocialMediaBackend.Modules.Payments.Application.Subscriptions.MarkSubscriptionAsPastDue;
+
+public class MarkSubscriptionAsPastDueCommandHandler(IAggregateRepository repository)
+    : ICommandHandler<MarkSubscriptionAsPastDueCommand>
+{
+    private readonly IAggregateRepository _repository = repository;
+
+    public async Task<HandlerResponse> ExecuteAsync(MarkSubscriptionAsPastDueCommand command, CancellationToken ct)
+    {
+        var subscription = await _repository.LoadAsync<Subscription>(command.InternalSubscriptionId, ct);
+
+        NotFoundException.ThrowIfNull(nameof(Subscription), subscription);
+
+        subscription.MarkAsPastDue();
+
+        return HandlerResponseStatus.OK;
+    }
+}
