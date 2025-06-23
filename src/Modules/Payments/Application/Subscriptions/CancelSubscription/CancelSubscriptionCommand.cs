5cc4d402eebffc68030bf750947f4a14ae5123e2
SabigGasim <sabgasim@gmail.com>
2025-06-23 10:27:24 +0300

    feat(payments.app): add CancelSubscription feature

---
+++ src/Modules/Payments/Application/Subscriptions/CancelSubscription/CancelSubscriptionCommand.cs
@@ -0,0 +1,10 @@
+﻿using SocialMediaBackend.BuildingBlocks.Infrastructure.InternalCommands;
+
+namespace SocialMediaBackend.Modules.Payments.Application.Subscriptions.CancelSubscription;
+
+public class CancelSubscriptionCommand(Guid internalSubscriptionId, string eventJson, Guid id = default)
+    : InternalCommandBase(id)
+{
+    public Guid InternalSubscriptionId { get; } = internalSubscriptionId;
+    public string EventJson { get; } = eventJson;
+}
---
+++ src/Modules/Payments/Application/Subscriptions/CancelSubscription/CancelSubscriptionCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application;
+using SocialMediaBackend.BuildingBlocks.Application.Requests;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.EventSourcing;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.Exceptions;
+using SocialMediaBackend.Modules.Payments.Domain.Subscriptions;
+
+namespace SocialMediaBackend.Modules.Payments.Application.Subscriptions.CancelSubscription;
+
+public class CancelSubscriptionCommandHandler(IAggregateRepository repository) : ICommandHandler<CancelSubscriptionCommand>
+{
+    private readonly IAggregateRepository _repository = repository;
+
+    public async Task<HandlerResponse> ExecuteAsync(CancelSubscriptionCommand command, CancellationToken ct)
+    {
+        var subscription = await _repository.LoadAsync<Subscription>(command.InternalSubscriptionId, ct);
+
+        NotFoundException.ThrowIfNull(nameof(Subscription), subscription);
+
+        subscription.Cancel();
+
+        return HandlerResponseStatus.OK;
+    }
+}
