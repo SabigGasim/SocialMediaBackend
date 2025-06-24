8dd43d5dc12cf9a4a322134fd723b8defbde2bdc
SabigGasim <sabgasim@gmail.com>
2025-06-24 05:44:54 +0300

    feat(payments.app): add RenewSubscription feature

---
+++ src/Modules/Payments/Application/Subscriptions/RenewSubscription/RenewSubscriptionCommand.cs
@@ -0,0 +1,16 @@
+﻿using SocialMediaBackend.BuildingBlocks.Infrastructure.InternalCommands;
+
+namespace SocialMediaBackend.Modules.Payments.Application.Subscriptions.RenewSubscription;
+
+public class RenewSubscriptionCommand(
+    Guid internalSubscriptionId,
+    DateTimeOffset startDate,
+    DateTimeOffset expirationDate,
+    string eventId,
+    Guid id = default) : InternalCommandBase(id)
+{
+    public Guid InternalSubscriptionId { get; } = internalSubscriptionId;
+    public DateTimeOffset StartDate { get; } = startDate;
+    public DateTimeOffset ExpirationDate { get; } = expirationDate;
+    public string EventId { get; } = eventId;
+}
---
+++ src/Modules/Payments/Application/Subscriptions/RenewSubscription/RenewSubscriptionCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application;
+using SocialMediaBackend.BuildingBlocks.Application.Requests;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.EventSourcing;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.Exceptions;
+using SocialMediaBackend.Modules.Payments.Domain.Subscriptions;
+
+namespace SocialMediaBackend.Modules.Payments.Application.Subscriptions.RenewSubscription;
+
+public class RenewSubscriptionCommandHandler(IAggregateRepository repository)
+    : ICommandHandler<RenewSubscriptionCommand>
+{
+    private readonly IAggregateRepository _repository = repository;
+
+    public async Task<HandlerResponse> ExecuteAsync(RenewSubscriptionCommand command, CancellationToken ct)
+    {
+        var subscription = await _repository.LoadAsync<Subscription>(command.InternalSubscriptionId, CancellationToken.None);
+
+        NotFoundException.ThrowIfNull(nameof(Subscription), subscription);
+
+        subscription.Renew(command.StartDate, command.ExpirationDate);
+
+        return HandlerResponseStatus.OK;
+    }
+}
