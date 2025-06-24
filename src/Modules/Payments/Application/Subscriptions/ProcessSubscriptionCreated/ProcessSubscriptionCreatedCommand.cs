3b254f64f90deb2da0cd22eea9405dc3cfa3eb06
SabigGasim <sabgasim@gmail.com>
2025-06-24 05:45:21 +0300

    feat(payments.app): add ProcessSubscriptionCreated feature

---
+++ src/Modules/Payments/Application/Subscriptions/ProcessSubscriptionCreated/ProcessSubscriptionCreatedCommand.cs
@@ -0,0 +1,18 @@
+﻿using SocialMediaBackend.BuildingBlocks.Infrastructure.InternalCommands;
+using SocialMediaBackend.Modules.Payments.Domain.Subscriptions;
+
+namespace SocialMediaBackend.Modules.Payments.Application.Subscriptions.ProcessSubscriptionCreated;
+
+[method: System.Text.Json.Serialization.JsonConstructor]
+[method: Newtonsoft.Json.JsonConstructor]
+
+public class ProcessSubscriptionCreatedCommand(
+    Guid internalSubscriptionId,
+    string gatewaySubscriptionId,
+    string eventId,
+    Guid id = default) : InternalCommandBase(id)
+{
+    public Guid InternalSubscriptionId { get; } = internalSubscriptionId;
+    public string GatewaySubscriptionId { get; } = gatewaySubscriptionId;
+    public string EventId { get; } = eventId;
+}
---
+++ src/Modules/Payments/Application/Subscriptions/ProcessSubscriptionCreated/ProcessSubscriptionCreatedCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application;
+using SocialMediaBackend.BuildingBlocks.Application.Requests;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.EventSourcing;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.Exceptions;
+using SocialMediaBackend.Modules.Payments.Domain.Subscriptions;
+
+namespace SocialMediaBackend.Modules.Payments.Application.Subscriptions.ProcessSubscriptionCreated;
+
+public class ProcessSubscriptionCreatedCommandHandler(IAggregateRepository repository)
+    : ICommandHandler<ProcessSubscriptionCreatedCommand>
+{
+    private readonly IAggregateRepository _repository = repository;
+
+    public async Task<HandlerResponse> ExecuteAsync(ProcessSubscriptionCreatedCommand command, CancellationToken ct)
+    {
+        var subscription = await _repository.LoadAsync<Subscription>(command.InternalSubscriptionId, CancellationToken.None);
+
+        NotFoundException.ThrowIfNull(nameof(Subscription), subscription);
+
+        subscription.MarkCreated(command.GatewaySubscriptionId);
+
+        return HandlerResponseStatus.OK;
+    }
+}
