3ed63483f5a45a63a8ad51158f8948e96d9e61c3
SabigGasim <sabgasim@gmail.com>
2025-06-24 11:02:37 +0300

    feat(payment.app): add PayerDeletedDomainEventNotificationHandler

---
+++ src/Modules/Payments/Application/Payers/DeletePayer/PayerDeletedDomainEventNotification.cs
@@ -0,0 +1,7 @@
+﻿using SocialMediaBackend.BuildingBlocks.Domain;
+using SocialMediaBackend.Modules.Payments.Domain.Payers.Events;
+
+namespace SocialMediaBackend.Modules.Payments.Application.Payers.DeletePayer;
+
+public class PayerDeletedDomainEventNotification(PayerDeletedDomainEvent domainEvent, Guid id) 
+    : DomainNotificationBase<PayerDeletedDomainEvent>(domainEvent, id);
---
+++ src/Modules/Payments/Application/Payers/DeletePayer/PayerDeletedDomainEventNotificationHandler.cs
@@ -0,0 +1,19 @@
+﻿using Mediator;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.InternalCommands;
+using SocialMediaBackend.Modules.Payments.Application.Payers.CleanUpPayerResources;
+
+namespace SocialMediaBackend.Modules.Payments.Application.Payers.DeletePayer;
+
+public class PayerDeletedDomainEventNotificationHandler(ICommandsScheduler commandsScheduler)
+    : INotificationHandler<PayerDeletedDomainEventNotification>
+{
+    private readonly ICommandsScheduler _scheduler = commandsScheduler;
+
+    public async ValueTask Handle(PayerDeletedDomainEventNotification notification, CancellationToken cancellationToken)
+    {
+        await _scheduler.EnqueueAsync(new CleanUpPayerResourcesCommand(
+            notification.Event.PayerId,
+            notification.Event.GatewayCustomerId)
+            );
+    }
+}
