151e86ff409e1cb37136ab4ed12c5e51866d327c
SabigGasim <sabgasim@gmail.com>
2025-06-24 10:59:59 +0300

    feat: add Task.WhenEach try catch wrapper helper

---
+++ src/BuildingBlocks/Infrastructure/Helpers/TaskHelpers.cs
@@ -0,0 +1,40 @@
+﻿namespace SocialMediaBackend.BuildingBlocks.Infrastructure.Helpers;
+
+public static class TaskHelpers
+{
+    public static async Task ProcessIndividually(
+        this IEnumerable<Task> tasks,
+        string? errorMessageContext = null)
+    {
+        await foreach (var task in Task.WhenEach(tasks))
+        {
+            try
+            {
+                await task;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"{errorMessageContext ?? "Error handling task"}: {ex.Message}");
+            }
+        }
+    }
+
+    public static async Task ProcessIndividually<T>(
+        this IEnumerable<Task<T>> tasks,
+        Action<T> processResult,
+        string? errorMessageContext = null)
+    {
+        await foreach (var task in Task.WhenEach(tasks))
+        {
+            try
+            {
+                var result = await task;
+                processResult(result);
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"{errorMessageContext ?? "Error handling task"}: {ex.Message}");
+            }
+        }
+    }
+}
