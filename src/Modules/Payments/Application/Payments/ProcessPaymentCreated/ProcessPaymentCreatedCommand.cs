efbb5b2ba91f0e83d1b41dffee1f9fa43dce5461
SabigGasim <sabgasim@gmail.com>
2025-06-26 03:56:04 +0300

    feat(payments.app): add ProcessPaymentCreated feature

---
+++ src/Modules/Payments/Application/Payments/ProcessPaymentCreated/ProcessPaymentCreatedCommand.cs
@@ -0,0 +1,13 @@
+﻿using SocialMediaBackend.BuildingBlocks.Infrastructure.InternalCommands;
+using SocialMediaBackend.Modules.Payments.Domain.Purchase;
+
+namespace SocialMediaBackend.Modules.Payments.Application.Payments.ProcessPaymentCreated;
+
+public class ProcessPaymentCreatedCommand(
+    PurchaseId purchaseId,
+    string paymentGatewayId,
+    Guid id = default) : InternalCommandBase(id)
+{
+    public PurchaseId PurchaseId { get; } = purchaseId;
+    public string PaymentGatewayId { get; } = paymentGatewayId;
+}
---
+++ src/Modules/Payments/Application/Payments/ProcessPaymentCreated/ProcessPaymentCreatedCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application;
+using SocialMediaBackend.BuildingBlocks.Application.Requests;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.EventSourcing;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.Exceptions;
+using SocialMediaBackend.Modules.Payments.Domain.Purchase;
+
+namespace SocialMediaBackend.Modules.Payments.Application.Payments.ProcessPaymentCreated;
+
+public class ProcessPaymentCreatedCommandHandler(IAggregateRepository repository)
+    : ICommandHandler<ProcessPaymentCreatedCommand>
+{
+    private readonly IAggregateRepository _repository = repository;
+
+    public async Task<HandlerResponse> ExecuteAsync(ProcessPaymentCreatedCommand command, CancellationToken ct)
+    {
+        var purchase = await _repository.LoadAsync<Purchase>(command.PurchaseId.Value);
+
+        NotFoundException.ThrowIfNull(nameof(Purchase), purchase);
+
+        purchase.MarkCreated(command.PaymentGatewayId);
+
+        return HandlerResponseStatus.OK;
+    }
+}
