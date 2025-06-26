0601f5684cb07dc22b4ae9af4e8207c065cfb5ff
SabigGasim <sabgasim@gmail.com>
2025-06-26 03:53:22 +0300

    feat(payments.app): add CancelPurchase feature

---
+++ src/Modules/Payments/Application/Payments/CancelPurchase/CancelPurchaseCommand.cs
@@ -0,0 +1,11 @@
+﻿using SocialMediaBackend.BuildingBlocks.Infrastructure.InternalCommands;
+using SocialMediaBackend.Modules.Payments.Domain.Purchase;
+
+namespace SocialMediaBackend.Modules.Payments.Application.Payments.CancelPurchase;
+
+public class CancelPurchaseCommand(
+    PurchaseId purchaseId,
+    Guid id = default) : InternalCommandBase(id)
+{
+    public PurchaseId PurchaseId { get; } = purchaseId;
+}
---
+++ src/Modules/Payments/Application/Payments/CancelPurchase/CancelPurchaseCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application;
+using SocialMediaBackend.BuildingBlocks.Application.Requests;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.EventSourcing;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.Exceptions;
+using SocialMediaBackend.Modules.Payments.Domain.Purchase;
+
+namespace SocialMediaBackend.Modules.Payments.Application.Payments.CancelPurchase;
+
+public class CancelPurchaseCommandHandler(IAggregateRepository repository)
+    : ICommandHandler<CancelPurchaseCommand>
+{
+    private readonly IAggregateRepository _repository = repository;
+
+    public async Task<HandlerResponse> ExecuteAsync(CancelPurchaseCommand command, CancellationToken ct)
+    {
+        var purchase = await _repository.LoadAsync<Purchase>(command.PurchaseId.Value);
+
+        NotFoundException.ThrowIfNull(nameof(Purchase), purchase);
+
+        purchase.Cancel();
+
+        return HandlerResponseStatus.OK;
+    }
+}
