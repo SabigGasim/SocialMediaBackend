b8e95b9196810d3ea1603b3ba4b126bfa8efbffb
SabigGasim <sabgasim@gmail.com>
2025-06-28 13:54:10 +0300

    feat(payments.infra): add InboxOutboxMigration

--- src/Modules/Payments/Infrastructure/Data/Migrations/20250627073207_InboxOutboxMigration.Designer.cs
+++ src/Modules/Payments/Infrastructure/Data/Migrations/20250627073207_InboxOutboxMigration.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using SocialMediaBackend.Modules.Payments.Infrastructure.Data;
@@ -11,9 +12,11 @@ using SocialMediaBackend.Modules.Payments.Infrastructure.Data;
 namespace SocialMediaBackend.Modules.Payments.Infrastructure.Data.Migrations
 {
     [DbContext(typeof(PaymentsDbContext))]
-    partial class PaymentsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250627073207_InboxOutboxMigration")]
+    partial class InboxOutboxMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
