846d7b7e6b85b35f1666cc4f5e2bc20b788b5033
SabigGasim <sabgasim@gmail.com>
2025-06-28 13:45:59 +0300

    feat(feed.infra): add ProcessInboxCommand/Job

---
+++ src/Modules/Feed/Infrastructure/Messaging/Inbox/ProcessInboxMessagesCommand.cs
@@ -0,0 +1,5 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+
+namespace SocialMediaBackend.Modules.Feed.Infrastructure.Messaging.Inbox;
+
+public class ProcessInboxMessagesCommand : CommandBase;
