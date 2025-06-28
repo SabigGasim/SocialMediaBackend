9cef7d39bf857cc73a4cad3cc10c1623c8e2980f
SabigGasim <sabgasim@gmail.com>
2025-06-28 13:47:59 +0300

    feat(payments.infra): add Inbox/OutboxMessageEntityTypeConfiguration

--- src/Modules/Payments/Infrastructure/Data/PaymentsDbContext.cs
+++ src/Modules/Payments/Infrastructure/Data/PaymentsDbContext.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using SocialMediaBackend.BuildingBlocks.Infrastructure.InternalCommands;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.Messaging;
 
 namespace SocialMediaBackend.Modules.Payments.Infrastructure.Data;
 
 public class PaymentsDbContext : DbContext
 {
     public DbSet<InternalCommand> InternalCommands { get; set; } = default!;
+    public DbSet<OutboxMessage> OutboxMessages { get; set; } = default!;
+    public DbSet<InboxMessage> InboxMessages { get; set; } = default!;
 
     protected PaymentsDbContext() { }
 
