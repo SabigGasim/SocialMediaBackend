8b4a8a8fd1b91b46ab66423daaf4ce66825f5b15
SabigGasim <sabgasim@gmail.com>
2025-06-28 13:46:18 +0300

    feat(feed.infra): add InboxIntegrationEventHandler

---
+++ src/Modules/Feed/Infrastructure/Messaging/Inbox/InboxIntegrationEventHandler.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using Newtonsoft.Json;
+using SocialMediaBackend.BuildingBlocks.Infrastructure;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.Events;
+using SocialMediaBackend.Modules.Feed.Infrastructure.Data;
+
+namespace SocialMediaBackend.Modules.Feed.Infrastructure.Messaging.Inbox;
+
+public class InboxIntegrationEventHandler<TEvent>(IDbConnectionFactory factory) 
+    : IIntegrationEventHandler<TEvent> where TEvent : IntegrationEvent
+{
+    private readonly IDbConnectionFactory _factory = factory;
+
+    public async ValueTask Handle(TEvent notification, CancellationToken cancellationToken)
+    {
+        const string sqlInsert =
+                $"""
+                INSERT INTO {Schema.Feed}."InboxMessages" ("Id", "Type" , "Content", "OccurredOn", "Processed")
+                VALUES (@Id, @Type, @Content, @OccurredOn, @Processed)
+                """;
+
+        using (var connection = await _factory.CreateAsync(CancellationToken.None))
+        {
+            await connection.ExecuteAsync(sqlInsert, new
+            {
+                Id = notification.Id,
+                OccurredOn = notification.OccurredOn,
+                Type = notification.GetType().AssemblyQualifiedName,
+                Content = JsonConvert.SerializeObject(notification),
+                Processed = false
+            });
+        }
+    }
+}
