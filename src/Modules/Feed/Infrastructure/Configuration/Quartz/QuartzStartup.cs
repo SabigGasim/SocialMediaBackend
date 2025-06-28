c3f6bde473de44f9a481e5262ea44ad6bff208f8
SabigGasim <sabgasim@gmail.com>
2025-06-28 13:47:11 +0300

    feat(feed.infra): schedule Quartz inbox/outbox jobs

--- src/Modules/Feed/Infrastructure/Configuration/Quartz/QuartzStartup.cs
+++ src/Modules/Feed/Infrastructure/Configuration/Quartz/QuartzStartup.cs
@@ -1,5 +1,7 @@
 ﻿using Quartz;
 using SocialMediaBackend.Modules.Feed.Infrastructure.InternalCommands;
+using SocialMediaBackend.Modules.Feed.Infrastructure.Messaging.Inbox;
+using SocialMediaBackend.Modules.Feed.Infrastructure.Messaging.Outbox;
 
 namespace SocialMediaBackend.Modules.Feed.Infrastructure.Configuration.Quartz;
 
@@ -7,20 +9,33 @@ public static class QuartzStartup
 {
     public static async Task InitializeAsync()
     {
+        await Task.WhenAll(
+            SchduleJobAsync<ProcessInternalCommandsJob>(),
+            SchduleJobAsync<ProcessInboxMessagesJob>(),
+            SchduleJobAsync<ProcessOutboxMessagesJob>()
+            );
+    }
+
+    private static async Task SchduleJobAsync<TJob>()
+        where TJob : IJob
+    {
+        var name = typeof(TJob).Name;
+
         var trigger = TriggerBuilder.Create()
            .StartNow()
            .WithSimpleSchedule(x => x
-               .WithIntervalInSeconds(2)
+               .WithIntervalInSeconds(4)
                .RepeatForever())
            .Build();
 
-        var job = JobBuilder.Create<ProcessInternalCommandsJob>()
-            .WithIdentity("Feed.ProcessInternalCommandsJob")
+        var job = JobBuilder.Create<TJob>()
+            .WithIdentity($"Feed.{name}")
             .Build();
 
         var schedulerFactory = SchedulerBuilder.Create()
             .UseInMemoryStore()
             .UseDefaultThreadPool(1)
+            .WithName($"Feed.{name}Scheduler")
             .Build();
 
         var scheduler = await schedulerFactory.GetScheduler();
