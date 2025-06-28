6ce0c2814f140051e4e84ffed93ca290fdcfb2d5
SabigGasim <sabgasim@gmail.com>
2025-06-28 13:53:13 +0300

    feat(payments.infra): add ProcessInboxCommand/Job

---
+++ src/Modules/Payments/Infrastructure/Messaging/Inbox/ProcessInboxMessagesCommand.cs
@@ -0,0 +1,5 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+
+namespace SocialMediaBackend.Modules.Payments.Infrastructure.Messaging.Inbox;
+
+public class ProcessInboxMessagesCommand : CommandBase;
---
+++ src/Modules/Payments/Infrastructure/Messaging/Inbox/ProcessInboxMessagesJob.cs
@@ -0,0 +1,13 @@
+﻿using Quartz;
+using SocialMediaBackend.Modules.Payments.Infrastructure.Helpers;
+
+namespace SocialMediaBackend.Modules.Payments.Infrastructure.Messaging.Inbox;
+
+[DisallowConcurrentExecution]
+public class ProcessInboxMessagesJob : IJob
+{
+    public async Task Execute(IJobExecutionContext context)
+    {
+        await CommandExecutor.ExecuteAsync(new ProcessInboxMessagesCommand());
+    }
+}
