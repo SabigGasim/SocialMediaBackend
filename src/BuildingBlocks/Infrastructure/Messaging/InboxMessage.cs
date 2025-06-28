2508e8c390510ada9f78988c3d1eed6c4779ad3f
SabigGasim <sabgasim@gmail.com>
2025-06-28 13:25:34 +0300

    feat(buildingblocks): add Inbox/Outbox message entities

---
+++ src/BuildingBlocks/Infrastructure/Messaging/InboxMessage.cs
@@ -0,0 +1,12 @@
+﻿namespace SocialMediaBackend.BuildingBlocks.Infrastructure.Messaging;
+
+public class InboxMessage
+{
+    public Guid Id { get; set; }
+    public string Type { get; set; } = default!;
+    public string Content { get; set; } = default!;
+    public string? Error { get; set; }
+    public bool Processed { get; set; }
+    public DateTimeOffset OccurredOn { get; set; }
+    public DateTimeOffset? ProcessedDate { get; set; }
+}
