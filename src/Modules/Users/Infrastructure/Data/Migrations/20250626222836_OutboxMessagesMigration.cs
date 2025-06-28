18d89c2eac79207ac8595f5f672f8c97170c046f
SabigGasim <sabgasim@gmail.com>
2025-06-28 13:56:40 +0300

    feat(users.infra): add OutboxMigration

---
+++ src/Modules/Users/Infrastructure/Data/Migrations/20250626222836_OutboxMessagesMigration.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialMediaBackend.Modules.Users.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class OutboxMessagesMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "OutboxMessages",
+                schema: "users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    Error = table.Column<string>(type: "text", nullable: true),
+                    Processed = table.Column<bool>(type: "boolean", nullable: false),
+                    OccurredOn = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    ProcessedDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OutboxMessages", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OutboxMessages_Processed",
+                schema: "users",
+                table: "OutboxMessages",
+                column: "Processed");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "OutboxMessages",
+                schema: "users");
+        }
+    }
+}
