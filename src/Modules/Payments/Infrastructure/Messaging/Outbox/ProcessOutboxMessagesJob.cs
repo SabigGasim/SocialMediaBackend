74256a9c55c955ff0a8a0b8a914d4dddf757de26
SabigGasim <sabgasim@gmail.com>
2025-06-28 13:53:49 +0300

    feat(payments.infra): add ProcessOutboxCommand/Job

---
+++ src/Modules/Payments/Infrastructure/Messaging/Outbox/ProcessOutboxMessagesJob.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Quartz;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.Modules.Payments.Infrastructure.Configuration;
+
+namespace SocialMediaBackend.Modules.Payments.Infrastructure.Messaging.Outbox;
+
+[DisallowConcurrentExecution]
+public class ProcessOutboxMessagesJob : IJob
+{
+    public async Task Execute(IJobExecutionContext context)
+    {
+        using (var scope = PaymentsCompositionRoot.BeginLifetimeScope())
+        {
+            var handler = scope.Resolve<ICommandHandler<ProcessOutboxMessagesCommand>>();
+            await handler.ExecuteAsync(new ProcessOutboxMessagesCommand(), default);
+        }
+    }
+}
