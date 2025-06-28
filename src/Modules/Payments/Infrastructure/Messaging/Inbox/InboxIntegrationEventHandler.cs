6b639af3d0b83fa4ea7a3be704c0d5a6042e3636
SabigGasim <sabgasim@gmail.com>
2025-06-28 13:53:29 +0300

    feat(payments.infra): add InboxIntegrationEventHandler

---
+++ src/Modules/Payments/Infrastructure/Messaging/Inbox/InboxIntegrationEventHandler.cs
@@ -0,0 +1,34 @@
+﻿using Dapper;
+using Newtonsoft.Json;
+using SocialMediaBackend.BuildingBlocks.Infrastructure;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.Events;
+
+namespace SocialMediaBackend.Modules.Payments.Infrastructure.Messaging.Inbox;
+
+public class InboxIntegrationEventHandler<TEvent>(IDbConnectionFactory factory) 
+    : IIntegrationEventHandler<TEvent> where TEvent : IntegrationEvent
+{
+    private readonly IDbConnectionFactory _factory = factory;
+
+    public async ValueTask Handle(TEvent notification, CancellationToken cancellationToken)
+    {
+        const string sqlInsert =
+                $"""
+                INSERT INTO {Schema.Payments}."InboxMessages" ("Id", "Type" , "Content", "OccurredOn", "Processed")
+                VALUES (@Id, @Type, @Content, @OccurredOn, @Processed)
+                """;
+
+        using (var connection = await _factory.CreateAsync(CancellationToken.None))
+        {
+            await connection.ExecuteAsync(sqlInsert, new
+            {
+                notification.Id,
+                notification.OccurredOn,
+                Type = notification.GetType().AssemblyQualifiedName,
+                Content = JsonConvert.SerializeObject(notification),
+                Processed = false
+            });
+        }
+
+    }
+}
