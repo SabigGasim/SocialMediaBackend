0fe2549705a100c45cfbd1839e59f4eedf28c278
SabigGasim <sabgasim@gmail.com>
2025-06-28 13:46:33 +0300

    feat(feed.infra): add ProcessOutboxCommand/Job

---
+++ src/Modules/Feed/Infrastructure/Messaging/Outbox/ProcessOutboxMessagesJob.cs
@@ -0,0 +1,19 @@
+﻿using Autofac;
+using Quartz;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.Modules.Feed.Infrastructure.Configuration;
+
+namespace SocialMediaBackend.Modules.Feed.Infrastructure.Messaging.Outbox;
+
+[DisallowConcurrentExecution]
+public class ProcessOutboxMessagesJob : IJob
+{
+    public async Task Execute(IJobExecutionContext context)
+    {
+        using (var scope = FeedCompositionRoot.BeginLifetimeScope())
+        {
+            var handler = scope.Resolve<ICommandHandler<ProcessOutboxMessagesCommand>>();
+            await handler.ExecuteAsync(new ProcessOutboxMessagesCommand(), default);
+        }
+    }
+}
