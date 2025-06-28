f589f7c2214848f70249d642268b251c7788b262
SabigGasim <sabgasim@gmail.com>
2025-06-28 13:45:29 +0300

    feat(feed.infra): add InboxOutboxMigration

---
+++ src/Modules/Feed/Infrastructure/Data/Migrations/20250627073322_InboxOutboxMigration.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SocialMediaBackend.Modules.Feed.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class InboxOutboxMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "InboxMessages",
+                schema: "feed",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    Error = table.Column<string>(type: "text", nullable: true),
+                    Processed = table.Column<bool>(type: "boolean", nullable: false),
+                    OccurredOn = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    ProcessedDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InboxMessages", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "OutboxMessages",
+                schema: "feed",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    Content = table.Column<string>(type: "text", nullable: false),
+                    Error = table.Column<string>(type: "text", nullable: true),
+                    Processed = table.Column<bool>(type: "boolean", nullable: false),
+                    OccurredOn = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    ProcessedDate = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_OutboxMessages", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InboxMessages_Processed",
+                schema: "feed",
+                table: "InboxMessages",
+                column: "Processed");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OutboxMessages_Processed",
+                schema: "feed",
+                table: "OutboxMessages",
+                column: "Processed");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "InboxMessages",
+                schema: "feed");
+
+            migrationBuilder.DropTable(
+                name: "OutboxMessages",
+                schema: "feed");
+        }
+    }
+}
