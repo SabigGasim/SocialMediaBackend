c52e2e574c7c478421ea22fe6b63bafc45bbc72e
SabigGasim <sabgasim@gmail.com>
2025-06-28 13:54:40 +0300

    feat(payments.infra): schedule Quartz inbox/outbox jobs

--- src/Modules/Payments/Infrastructure/Configuration/Quartz/QuartzStartup.cs
+++ src/Modules/Payments/Infrastructure/Configuration/Quartz/QuartzStartup.cs
@@ -1,11 +1,21 @@
 ﻿using Quartz;
 using SocialMediaBackend.Modules.Payments.Infrastructure.InternalCommands;
+using SocialMediaBackend.Modules.Payments.Infrastructure.Messaging.Outbox;
 
 namespace SocialMediaBackend.Modules.Payments.Infrastructure.Configuration.Quartz;
 
 public static class QuartzStartup
 {
     public static async Task InitializeAsync()
+    {
+        await Task.WhenAll(
+            SchduleJobAsync<ProcessInternalCommandsJob>(nameof(ProcessInternalCommandsJob)),
+            SchduleJobAsync<ProcessOutboxMessagesJob>(nameof(ProcessOutboxMessagesJob))
+            );
+    }
+
+    private static async Task SchduleJobAsync<TJob>(string jobName)
+        where TJob : IJob
     {
         var trigger = TriggerBuilder.Create()
            .StartNow()
@@ -15,12 +25,13 @@ public static class QuartzStartup
            .Build();
 
         var job = JobBuilder.Create<ProcessInternalCommandsJob>()
-            .WithIdentity("Payments.ProcessInternalCommandsJob")
+            .WithIdentity($"Payments.{jobName}")
             .Build();
 
         var schedulerFactory = SchedulerBuilder.Create()
             .UseInMemoryStore()
             .UseDefaultThreadPool(1)
+            .WithName($"Payments.{jobName}Scheduler")
             .Build();
 
         var scheduler = await schedulerFactory.GetScheduler();
