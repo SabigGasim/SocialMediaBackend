21d5b6c35de0c3b7c4f026188ab1e462e492ffdb
SabigGasim <sabgasim@gmail.com>
2025-06-29 18:47:43 +0300

    feat: add InboxMessage & OutboxMessage marten-specific

---
+++ src/BuildingBlocks/Infrastructure/EventSourcing/Messaging/InboxMessage.cs
@@ -0,0 +1,29 @@
+﻿using SocialMediaBackend.BuildingBlocks.Domain.EventSourcing;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.Events;
+
+namespace SocialMediaBackend.BuildingBlocks.Infrastructure.EventSourcing.Messaging;
+
+public class InboxMessage : AggregateRoot
+{
+    public IntegrationEvent Notification { get; set; } = default!;
+    public string? Error { get; set; }
+    public bool Processed { get; set; }
+    public DateTimeOffset OccurredOn { get; set; }
+    public DateTimeOffset? ProcessedDate { get; set; }
+
+    private InboxMessage() { }
+
+    public static InboxMessage Create<TNotification>(TNotification notification)
+        where TNotification : IntegrationEvent
+    {
+        return new InboxMessage
+        {
+            Id = notification.Id,
+            Notification = notification,
+            OccurredOn = notification.OccurredOn,
+            Processed = false,
+            ProcessedDate = null,
+            Error = null
+        };
+    }
+}
---
+++ src/BuildingBlocks/Infrastructure/EventSourcing/Messaging/OutboxMessage.cs
@@ -0,0 +1,30 @@
+﻿using SocialMediaBackend.BuildingBlocks.Domain;
+using SocialMediaBackend.BuildingBlocks.Domain.EventSourcing;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.Events;
+
+namespace SocialMediaBackend.BuildingBlocks.Infrastructure.EventSourcing.Messaging;
+
+public class OutboxMessage : AggregateRoot
+{
+    public IDomainEventNotification Notification { get; set; } = default!;
+    public string? Error { get; set; }
+    public bool Processed { get; set; }
+    public DateTimeOffset OccurredOn { get; set; }
+    public DateTimeOffset? ProcessedDate { get; set; }
+
+    private OutboxMessage() { }
+
+    public static OutboxMessage Create<TNotification>(TNotification notification)
+        where TNotification : IDomainEventNotification
+    {
+        return new OutboxMessage
+        {
+            Id = notification.Id,
+            Notification = notification,
+            OccurredOn = notification.Event.OccurredOn,
+            Processed = false,
+            ProcessedDate = null,
+            Error = null
+        };
+    }
+}
