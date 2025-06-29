c5e20297e9eaa154546a108b47583dc6f37cd587
SabigGasim <sabgasim@gmail.com>
2025-06-29 18:51:55 +0300

    feat(payments.infra): add marten specific InternalCommand document

---
+++ src/Modules/Payments/Infrastructure/InternalCommands/InternalCommand.cs
@@ -0,0 +1,36 @@
+﻿using SocialMediaBackend.BuildingBlocks.Domain.EventSourcing;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.InternalCommands;
+
+namespace SocialMediaBackend.Modules.Payments.Infrastructure.InternalCommands;
+
+public class InternalCommand : AggregateRoot
+{
+    public InternalCommandBase Command { get; set; } = default!;
+    public string? Error { get; set; }
+    public string? IdempotencyKey { get; set; }
+    public bool Processed { get; set; }
+    public DateTimeOffset? ProcessedDate { get; set; }
+    public DateTimeOffset EnqueueDate { get; set; }
+
+    private InternalCommand() { }
+
+    public static InternalCommand Create<TCommand>(
+        TCommand command,
+        bool processed,
+        DateTimeOffset enqueueDate,
+        DateTimeOffset? processedDate = null,
+        string? error = null,
+        string? idempotencyKey = null) where TCommand : InternalCommandBase
+    {
+        return new InternalCommand()
+        {
+            Id = command.Id,
+            Command = command,
+            Processed = processed,
+            EnqueueDate = enqueueDate,
+            ProcessedDate = processedDate,
+            Error = error,
+            IdempotencyKey = idempotencyKey
+        };
+    }
+}
