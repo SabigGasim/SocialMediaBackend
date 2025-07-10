2b20b5e391967e000b8852d379ed0eea98ca0153
SabigGasim <sabgasim@gmail.com>
2025-07-10 10:26:18 +0300

    feat: add IExecutionContextAccessor

---
+++ src/BuildingBlocks/Application/Auth/IExecutionContextAccessor.cs
@@ -0,0 +1,8 @@
+﻿namespace SocialMediaBackend.BuildingBlocks.Application.Auth;
+
+public interface IExecutionContextAccessor
+{
+    Guid UserId { get; }
+
+    bool IsAvailable { get; }
+}
---
+++ src/Presentation/Api/Configuration/ExecutionContextAccessor.cs
@@ -0,0 +1,31 @@
+﻿using SocialMediaBackend.BuildingBlocks.Application.Auth;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.Exceptions;
+
+namespace SocialMediaBackend.Api.Configuration;
+
+public class ExecutionContextAccessor(IHttpContextAccessor accessor) : IExecutionContextAccessor
+{
+    private readonly IHttpContextAccessor _accessor = accessor;
+
+    public Guid UserId => GetUserId();
+    public bool IsAvailable => _accessor.HttpContext is not null;
+
+    private Guid GetUserId()
+    {
+        if (!IsAvailable)
+        {
+            throw new NotFoundException(nameof(UserId));
+        }
+
+        var userIdClaim = _accessor
+            .HttpContext!
+            .User
+            .Claims
+            .FirstOrDefault(x => x.Type == "userid")?
+            .Value;
+
+        return Guid.TryParse(userIdClaim, out var userId)
+            ? userId
+            : throw new NotFoundException(nameof(UserId));
+    }
+}
