5bcb94fa944f05e94ab095c7ea641d92c56b751c
SabigGasim <sabgasim@gmail.com>
2025-07-10 10:53:01 +0300

    feat(chat.domain): add Role entity with default roles

---
+++ src/Modules/Chat/Domain/Authorization/Role.cs
@@ -0,0 +1,31 @@
+﻿using SocialMediaBackend.Modules.Chat.Domain.Chatters;
+
+namespace SocialMediaBackend.Modules.Chat.Domain.Authorization;
+
+public enum Roles
+{
+    Chatter, AdminChatter, AppBasicPlan, AppPlusPlan
+}
+
+public sealed class Role
+{
+    private readonly List<Chatter> _chatters = [];
+    private readonly List<Permission> _permissions = [];
+
+    public static readonly Role Chatter = new Role(Roles.Chatter);
+    public static readonly Role AdminChatter = new Role(Roles.AdminChatter);
+    public static readonly Role BasicPlan = new Role(Roles.AppBasicPlan);
+    public static readonly Role PlusPlan = new Role(Roles.AppPlusPlan);
+
+    public Roles Id { get; private set; }
+    public string Name { get; private set; } = default!;
+    public IReadOnlyCollection<Chatter> Chatters => _chatters.AsReadOnly();
+    public IReadOnlyCollection<Permission> Permissions => _permissions.AsReadOnly();
+
+    private Role() { }
+    public Role(Roles roleId)
+    {
+        Id = roleId;
+        Name = $"{roleId}Role";
+    }
+}
