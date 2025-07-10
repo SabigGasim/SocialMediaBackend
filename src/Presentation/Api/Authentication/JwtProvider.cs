874092eb47e13bf03b83537937b8419639eb8042
SabigGasim <sabgasim@gmail.com>
2025-07-10 10:16:29 +0300

    feat(api): add JwtProvider

---
+++ src/Presentation/Api/Authentication/JwtProvider.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+
+namespace SocialMediaBackend.Api.Authentication;
+
+public interface IJwtProvider
+{
+    string GenerateToken(TokenGenerationRequest request);
+}
+
+public sealed class JwtProvider : IJwtProvider
+{
+    private const string TokenSecret = "ForTheLoveOfGodStoreAndLoadThisSecurely";
+    private static readonly TimeSpan TokenLifetime = TimeSpan.FromDays(365 * 10);
+
+    public string GenerateToken(TokenGenerationRequest request)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.UTF8.GetBytes(TokenSecret);
+
+        var claims = new List<Claim>
+        {
+            new(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+            new(JwtRegisteredClaimNames.Sub, request.Email),
+            new(JwtRegisteredClaimNames.Email, request.Email),
+            new("userid", request.UserId.ToString())
+        };
+
+        foreach (var claimPair in request.CustomClaims)
+        {
+            var jsonElement = (JsonElement)claimPair.Value;
+            var valueType = jsonElement.ValueKind switch
+            {
+                JsonValueKind.True => ClaimValueTypes.Boolean,
+                JsonValueKind.False => ClaimValueTypes.Boolean,
+                JsonValueKind.Number => ClaimValueTypes.Double,
+                _ => ClaimValueTypes.String
+            };
+
+            var claim = new Claim(claimPair.Key, claimPair.Value.ToString()!, valueType);
+            claims.Add(claim);
+        }
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(claims),
+            Expires = DateTime.UtcNow.Add(TokenLifetime),
+            Issuer = "https://id.sabiggasim.com",
+            Audience = "https://social.sabiggasim.com",
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
