1b9053f2332a444cfe1f647b26f4a82fb03f938b
SabigGasim <sabgasim@gmail.com>
2025-07-10 10:48:10 +0300

    feat(feed.domain): map permissions to their roles

---
+++ src/Modules/Feed/Domain/Authorization/RolePermissionsData.cs
@@ -0,0 +1,33 @@
+﻿namespace SocialMediaBackend.Modules.Feed.Domain.Authorization;
+
+public static class RolePermissionData
+{
+    private static readonly Dictionary<Role, Permission[]> _mappings;
+
+    public static IReadOnlyDictionary<Role, Permission[]> Mappings => _mappings.AsReadOnly();
+
+    static RolePermissionData()
+    {
+        _mappings = [];
+        
+        _mappings[Role.Author] = [
+            Permission.GetPost,
+            Permission.CreatePost,
+            Permission.GetAllPosts,
+            Permission.DeletePost,
+            Permission.UpdatePost,
+            Permission.LikePost,
+            Permission.UnlikePost
+        ];
+
+        _mappings[Role.BasicPlan] = [];
+        _mappings[Role.PlusPlan] = [.. _mappings[Role.BasicPlan]];
+
+        _mappings[Role.AdminAuthor] = new HashSet<Permission>(
+            _mappings[Role.Author]
+                .Concat(_mappings[Role.BasicPlan])
+                .Concat(_mappings[Role.PlusPlan])
+        )
+            .ToArray();
+    }
+}
