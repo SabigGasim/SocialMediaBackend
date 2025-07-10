e7c990dad5abd84d9446dbc730b8dfe41172fbeb
SabigGasim <sabgasim@gmail.com>
2025-07-10 10:54:46 +0300

    feat(chat.infra): add RoleEntityTypeConfiguration

---
+++ src/Modules/Chat/Infrastructure/Domain/Roles/RoleEntityTypeConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SocialMediaBackend.Modules.Chat.Domain.Authorization;
+
+namespace SocialMediaBackend.Modules.Chat.Infrastructure.Domain.Roles;
+
+internal class RoleEntityTypeConfiguration : IEntityTypeConfiguration<Role>
+{
+    public void Configure(EntityTypeBuilder<Role> builder)
+    {
+        builder.HasKey(x => x.Name);
+
+        builder.HasKey(p => p.Id);
+
+        builder.HasIndex(p => p.Name)
+            .IsUnique();
+
+        builder.Property(p => p.Name)
+            .IsRequired();
+
+        builder.HasMany(x => x.Permissions)
+            .WithMany()
+            .UsingEntity<RolePermission>();
+
+        builder.HasMany(x => x.Chatters)
+            .WithMany()
+            .UsingEntity<ChatterRole>();
+
+        var rolesToSeed = RolePermissionData
+            .Mappings
+            .Select(mapping => mapping.Key);
+
+        builder.HasData(rolesToSeed);
+    }
+}
