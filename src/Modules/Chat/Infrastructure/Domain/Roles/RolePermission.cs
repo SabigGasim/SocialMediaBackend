c91c6107b1909a57a755fc6b35fb2d31030b5155
SabigGasim <sabgasim@gmail.com>
2025-07-10 10:54:36 +0300

    feat(chat.infra): add RolePermission with EntityTypeConfiguration

---
+++ src/Modules/Chat/Infrastructure/Domain/Roles/RolePermission.cs
@@ -0,0 +1,19 @@
+﻿using SocialMediaBackend.Modules.Chat.Domain.Authorization;
+
+namespace SocialMediaBackend.Modules.Chat.Infrastructure.Domain.Roles;
+
+internal class RolePermission
+{
+    public Chat.Domain.Authorization.Roles RoleId { get; private set; } = default!;
+    public Permissions PermissionId { get; private set; } = default!;
+
+    public Role Role { get; private set; } = default!;
+    public Permission Permission { get; private set; } = default!;
+
+    private RolePermission() { }
+    public RolePermission(Chat.Domain.Authorization.Roles roleId, Permissions permissionId)
+    {
+        RoleId = roleId;
+        PermissionId = permissionId;
+    }
+}
