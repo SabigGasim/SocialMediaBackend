3cf9023a97547cc7e1beab4c5a79141a65c43214
SabigGasim <sabgasim@gmail.com>
2025-07-10 10:49:20 +0300

    feat(feed.infra): add PermissionRole with EntityTypeConfiguration

---
+++ src/Modules/Feed/Infrastructure/Domain/Roles/RolePermission.cs
@@ -0,0 +1,19 @@
+﻿using SocialMediaBackend.Modules.Feed.Domain.Authorization;
+
+namespace SocialMediaBackend.Modules.Feed.Infrastructure.Domain.Roles;
+
+internal class RolePermission
+{
+    public Feed.Domain.Authorization.Roles RoleId { get; private set; } = default!;
+    public Feed.Domain.Authorization.Permissions PermissionId { get; private set; } = default!;
+
+    public Role Role { get; private set; } = default!;
+    public Permission Permission { get; private set; } = default!;
+
+    private RolePermission() { }
+    public RolePermission(Feed.Domain.Authorization.Roles roleId, Feed.Domain.Authorization.Permissions permissionId)
+    {
+        RoleId = roleId;
+        PermissionId = permissionId;
+    }
+}
