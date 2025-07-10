a1f5cc6951518f5c24ba823728ea86dba08ec656
SabigGasim <sabgasim@gmail.com>
2025-07-10 10:53:34 +0300

    feat(chat.domain): map permissions to their roles

---
+++ src/Modules/Chat/Domain/Authorization/RolePermissionsData.cs
@@ -0,0 +1,43 @@
+﻿namespace SocialMediaBackend.Modules.Chat.Domain.Authorization;
+
+public static class RolePermissionData
+{
+    private static readonly Dictionary<Role, Permission[]> _mappings;
+
+    public static IReadOnlyDictionary<Role, Permission[]> Mappings => _mappings.AsReadOnly();
+
+    static RolePermissionData()
+    {
+        _mappings = [];
+        
+        _mappings[Role.Chatter] = [
+            Permission.CreateDirectChat,
+            Permission.CreateDirectMessage,
+            Permission.DeleteDirectMessageForEveryone,
+            Permission.DeleteDirectMessageForMe,
+            Permission.GetAllDirectMessages,
+            Permission.MarkDirectMessageAsSeen,
+            Permission.CreateGroupChat,
+            Permission.JoinGroupChat,
+            Permission.KickGroupMember,
+            Permission.LeaveGroupChat,
+            Permission.PromoteMember,
+            Permission.CreateGroupMessage,
+            Permission.DeleteGroupMessageForEveryone,
+            Permission.DeleteGroupMessageForMe,
+            Permission.GetAllGroupMessages,
+            Permission.MarkGroupMessageAsSeen,
+            Permission.MarkGroupMessageAsReceived,
+        ];
+
+        _mappings[Role.BasicPlan] = [];
+        _mappings[Role.PlusPlan] = [.. _mappings[Role.BasicPlan]];
+
+        _mappings[Role.AdminChatter] = new HashSet<Permission>(
+            _mappings[Role.Chatter]
+                .Concat(_mappings[Role.BasicPlan])
+                .Concat(_mappings[Role.PlusPlan])
+        )
+            .ToArray();
+    }
+}
