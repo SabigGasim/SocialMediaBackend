d33bbe0190abe32ce211b7fb656a249c44111a95
SabigGasim <sabgasim@gmail.com>
2025-07-10 10:48:58 +0300

    feat(feed.infra): add AuthorRole with EntityTypeConfiguration

---
+++ src/Modules/Feed/Infrastructure/Domain/Roles/AuthorRole.cs
@@ -0,0 +1,20 @@
+﻿using SocialMediaBackend.Modules.Feed.Domain.Authorization;
+using SocialMediaBackend.Modules.Feed.Domain.Authors;
+
+namespace SocialMediaBackend.Modules.Feed.Infrastructure.Domain.Roles;
+
+public class AuthorRole
+{
+    public Feed.Domain.Authorization.Roles RoleId { get; private set; } = default!;
+    public AuthorId AuthorId { get; private set; } = default!;
+    
+    public Role Role { get; private set; } = default!;
+    public Author Author { get; private set; } = default!;
+
+    private AuthorRole() { }
+    public AuthorRole(Feed.Domain.Authorization.Roles roleId, AuthorId authorId)
+    {
+        RoleId = roleId;
+        AuthorId = authorId;
+    }
+}
---
+++ src/Modules/Feed/Infrastructure/Domain/Roles/AuthorRoleEntityTypeConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace SocialMediaBackend.Modules.Feed.Infrastructure.Domain.Roles;
+
+internal class AuthorRoleEntityTypeConfiguration : IEntityTypeConfiguration<AuthorRole>
+{
+    public void Configure(EntityTypeBuilder<AuthorRole> builder)
+    {
+        builder.ToTable("AuthorRoles");
+
+        builder.HasKey(x => new { x.AuthorId, x.RoleId });
+
+        builder.HasOne(x => x.Author)
+            .WithMany()
+            .HasForeignKey(x => x.AuthorId);
+
+        builder.HasOne(x => x.Role)
+            .WithMany()
+            .HasForeignKey(x => x.RoleId);
+    }
+}
