d6f69d885c715dc416465222e3f956ea8c81e5ac
SabigGasim <sabgasim@gmail.com>
2025-07-10 10:37:14 +0300

    feat(users.infra): add RolePermission with EntityTypeConfiguration

---
+++ src/Modules/Users/Infrastructure/Domain/Roles/RolePermission.cs
@@ -0,0 +1,20 @@
+﻿using SocialMediaBackend.Modules.Users.Domain.Authorization;
+using DomainAuth = SocialMediaBackend.Modules.Users.Domain.Authorization;
+
+namespace SocialMediaBackend.Modules.Users.Infrastructure.Domain.Roles;
+
+internal class RolePermission
+{
+    public DomainAuth.Roles RoleId { get; private set; } = default!;
+    public DomainAuth.Permissions PermissionId { get; private set; } = default!;
+
+    public Role Role { get; private set; } = default!;
+    public Permission Permission { get; private set; } = default!;
+
+    private RolePermission() { }
+    public RolePermission(DomainAuth.Roles roleId, DomainAuth.Permissions permissionId)
+    {
+        RoleId = roleId;
+        PermissionId = permissionId;
+    }
+}
