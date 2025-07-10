8fdc46fe321291f87a9a4dc7d2030b79d030657c
SabigGasim <sabgasim@gmail.com>
2025-07-10 10:31:14 +0300

    feat: add HasPermissionAttribute

---
+++ src/BuildingBlocks/Application/Auth/HasPermissionAttribute.cs
@@ -0,0 +1,19 @@
+﻿namespace SocialMediaBackend.BuildingBlocks.Application.Auth;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+public sealed class HasPermissionAttribute : Attribute
+{
+    public int[] Permissions { get; }
+
+    /// <param name="permissionIds">These are the enum Ids defined by the calling module</param>
+    public HasPermissionAttribute(params object[] permissionIds)
+    {
+        Permissions = [..permissionIds.Select(x => (int)x)];
+    }
+
+    /// <param name="permissionId">This is the enum Id defnied by the calling module</param>
+    public HasPermissionAttribute(object permissionId)
+    {
+        Permissions = [(int)permissionId];
+    }
+}
