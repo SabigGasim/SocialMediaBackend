32213a421e5f4e1a29b4c81562121a9e91f321de
SabigGasim <sabgasim@gmail.com>
2025-07-10 10:49:36 +0300

    feat(feed.infra): add RoleEntityTypeConfiguration

---
+++ src/Modules/Feed/Infrastructure/Domain/Roles/RoleEntityTypeConfiguration.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SocialMediaBackend.Modules.Feed.Domain.Authorization;
+
+namespace SocialMediaBackend.Modules.Feed.Infrastructure.Domain.Roles;
+
+internal class RoleEntityTypeConfiguration : IEntityTypeConfiguration<Role>
+{
+    public void Configure(EntityTypeBuilder<Role> builder)
+    {
+        builder.HasKey(x => x.Name);
+
+        builder.HasKey(p => p.Id);
+
+        builder.HasIndex(p => p.Name)
+            .IsUnique();
+
+        builder.Property(p => p.Name)
+            .IsRequired();
+
+        builder.HasMany(x => x.Permissions)
+            .WithMany()
+            .UsingEntity<RolePermission>();
+
+        builder.HasMany(x => x.Authors)
+            .WithMany()
+            .UsingEntity<AuthorRole>();
+
+        var rolesToSeed = RolePermissionData
+            .Mappings
+            .Select(mapping => mapping.Key);
+
+        builder.HasData(rolesToSeed);
+    }
+}
