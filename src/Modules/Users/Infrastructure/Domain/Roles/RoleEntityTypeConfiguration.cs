42a17c2f1b9778b2fa2a8ae8bf347ab1dbfa4243
SabigGasim <sabgasim@gmail.com>
2025-07-10 10:38:15 +0300

    feat(users.infra): add RoleEntityTypeConfiguration

---
+++ src/Modules/Users/Infrastructure/Domain/Roles/RoleEntityTypeConfiguration.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SocialMediaBackend.Modules.Users.Domain.Authorization;
+
+namespace SocialMediaBackend.Modules.Users.Infrastructure.Domain.Roles;
+
+internal class RoleEntityTypeConfiguration : IEntityTypeConfiguration<Role>
+{
+    public void Configure(EntityTypeBuilder<Role> builder)
+    {
+        builder.HasKey(x => x.Name);
+
+        builder.HasKey(p => p.Id);
+
+        builder.HasIndex(p => p.Name)
+            .IsUnique();
+
+        builder.Property(p => p.Name)
+            .IsRequired();
+
+        builder.HasMany(x => x.Permissions)
+            .WithMany()
+            .UsingEntity<RolePermission>();
+
+        builder.HasMany(x => x.Users)
+            .WithMany()
+            .UsingEntity<UserRole>(            // EF Core can't easily infer this UserRole join table
+                l => l.HasOne(ur => ur.User)
+                      .WithMany()
+                      .HasForeignKey(ur => ur.UserId),
+                r => r.HasOne(ur => ur.Role)
+                      .WithMany()
+                      .HasForeignKey(ur => ur.RoleId),
+                j =>
+                {
+                    j.HasKey(ur => new { ur.RoleId, ur.UserId });
+                    j.ToTable("UserRoles");
+                });
+
+        var rolesToSeed = RolePermissionData
+            .Mappings
+            .Select(mapping => mapping.Key);
+
+        builder.HasData(rolesToSeed);
+    }
+}
