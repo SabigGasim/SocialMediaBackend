e2cc57aabf58f5e3bbf60fb6edfc920353b8bf32
SabigGasim <sabgasim@gmail.com>
2025-07-11 04:03:01 +0300

    feat(feed): add CommentPermissionsSeedMigration

---
+++ src/Modules/Feed/Infrastructure/Data/Migrations/20250711003000_CommentPermissionsSeedMigration.cs
@@ -0,0 +1,225 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace SocialMediaBackend.Modules.Feed.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class CommentPermissionsSeedMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                schema: "feed",
+                table: "Permissions",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { 7, "Permissions.Comments.Create" },
+                    { 8, "Permissions.Comments.Update" },
+                    { 9, "Permissions.Comments.Delete" },
+                    { 10, "Permissions.Comments.Get" },
+                    { 11, "Permissions.Comments.GetAll" },
+                    { 12, "Permissions.Comments.Reply" },
+                    { 13, "Permissions.Comments.GetAllReplies" },
+                    { 14, "Permissions.Comments.Like" },
+                    { 15, "Permissions.Comments.Unlike" }
+                });
+
+            migrationBuilder.InsertData(
+                schema: "feed",
+                table: "RolePermissions",
+                columns: new[] { "PermissionId", "RoleId" },
+                values: new object[,]
+                {
+                    { 7, 0 },
+                    { 8, 0 },
+                    { 9, 0 },
+                    { 10, 0 },
+                    { 11, 0 },
+                    { 12, 0 },
+                    { 13, 0 },
+                    { 14, 0 },
+                    { 15, 0 },
+                    { 7, 1 },
+                    { 8, 1 },
+                    { 9, 1 },
+                    { 10, 1 },
+                    { 11, 1 },
+                    { 12, 1 },
+                    { 13, 1 },
+                    { 14, 1 },
+                    { 15, 1 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "RolePermissions",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 7, 0 });
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "RolePermissions",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 8, 0 });
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "RolePermissions",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 9, 0 });
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "RolePermissions",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 10, 0 });
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "RolePermissions",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 11, 0 });
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "RolePermissions",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 12, 0 });
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "RolePermissions",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 13, 0 });
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "RolePermissions",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 14, 0 });
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "RolePermissions",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 15, 0 });
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "RolePermissions",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 7, 1 });
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "RolePermissions",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 8, 1 });
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "RolePermissions",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 9, 1 });
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "RolePermissions",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 10, 1 });
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "RolePermissions",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 11, 1 });
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "RolePermissions",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 12, 1 });
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "RolePermissions",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 13, 1 });
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "RolePermissions",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 14, 1 });
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "RolePermissions",
+                keyColumns: new[] { "PermissionId", "RoleId" },
+                keyValues: new object[] { 15, 1 });
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 7);
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 8);
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 9);
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 10);
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 11);
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 12);
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 13);
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 14);
+
+            migrationBuilder.DeleteData(
+                schema: "feed",
+                table: "Permissions",
+                keyColumn: "Id",
+                keyValue: 15);
+        }
+    }
+}
