1f830c5653c3c6153251e1e2e2b59353a15cdd9b
SabigGasim <sabgasim@gmail.com>
2025-07-15 09:14:44 +0300

    feat(app_subscriptions.app): add AppSubscriptionActivatedNotification/Handler

---
+++ src/Modules/AppSubscriptions/Application/Subscriptions/ActivateSubscription/SubscriptionActivatedNotification.cs
@@ -0,0 +1,8 @@
+﻿using SocialMediaBackend.BuildingBlocks.Domain;
+using SocialMediaBackend.Modules.AppSubscriptions.Domain.Subscriptions.Events;
+
+namespace SocialMediaBackend.Modules.AppSubscriptions.Application.Subscriptions.ActivateSubscription;
+
+public sealed class SubscriptionActivatedNotification(
+    AppSubscriptionActivatedDomainEvent domainEvent, Guid id)
+    : DomainNotificationBase<AppSubscriptionActivatedDomainEvent>(domainEvent, id);
---
+++ src/Modules/AppSubscriptions/Application/Subscriptions/ActivateSubscription/SubscriptionActivatedNotificationHandler.cs
@@ -0,0 +1,21 @@
+﻿using Mediator;
+using SocialMediaBackend.BuildingBlocks.Infrastructure.Messaging;
+using SocialMediaBackend.Modules.AppSubscriptions.Contracts;
+
+namespace SocialMediaBackend.Modules.AppSubscriptions.Application.Subscriptions.ActivateSubscription;
+
+internal sealed class SubscriptionActivatedNotificationHandler(IEventBus eventBus)
+    : INotificationHandler<SubscriptionActivatedNotification>
+{
+    private readonly IEventBus _eventBus = eventBus;
+
+    public async ValueTask Handle(SubscriptionActivatedNotification notification, CancellationToken cancellationToken)
+    {
+        await _eventBus.Publish(new AppSubscriptionActivatedIntegrationEvent(
+            notification.Event.SubscriptionId.Value,
+            notification.Event.SubscriberId.Value,
+            notification.Event.SubscriptionTier,
+            notification.Event.ActivatedAt,
+            notification.Event.ExpiresAt));
+    }
+}
