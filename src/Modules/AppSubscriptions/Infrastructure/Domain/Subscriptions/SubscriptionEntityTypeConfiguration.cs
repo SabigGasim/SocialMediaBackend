61bcd63c755883f6748cd2529960c0996a1554dc
SabigGasim <sabgasim@gmail.com>
2025-07-15 09:08:38 +0300

    feat(app_subscriptions.infra): add SubscriptionEntityTypeConfiguration

---
+++ src/Modules/AppSubscriptions/Infrastructure/Domain/Subscriptions/SubscriptionEntityTypeConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SocialMediaBackend.Modules.AppSubscriptions.Domain.Subscriptions;
+
+namespace SocialMediaBackend.Modules.AppSubscriptions.Infrastructure.Domain.Subscriptions;
+
+internal class SubscriptionEntityTypeConfiguration : IEntityTypeConfiguration<Subscription>
+{
+    public void Configure(EntityTypeBuilder<Subscription> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasConversion(
+                id => id.Value,
+                value => new(value));
+
+        builder.HasOne(x => x.Subscriber)
+            .WithOne(x => x.Subscription)
+            .HasForeignKey<Subscription>(x => x.SubscriberId)
+            .IsRequired(false)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
