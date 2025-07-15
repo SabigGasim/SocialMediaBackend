fa3d84d48f5bf45f47610b5d74ae4c9431fdfdcb
SabigGasim <sabgasim@gmail.com>
2025-07-15 09:13:32 +0300

    feat(app_subscriptions.app): add CompleteSubscriptionPaymentCommandHandler

---
+++ src/Modules/AppSubscriptions/Application/Subscriptions/ActivateSubscription/CompleteSubscriptionPaymentCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SocialMediaBackend.BuildingBlocks.Application;
+using SocialMediaBackend.BuildingBlocks.Application.Requests;
+using SocialMediaBackend.BuildingBlocks.Application.Requests.Commands;
+using SocialMediaBackend.Modules.AppSubscriptions.Domain.SubscriptionPayments;
+using SocialMediaBackend.Modules.AppSubscriptions.Infrastructure.Data;
+
+namespace SocialMediaBackend.Modules.AppSubscriptions.Application.Subscriptions.ActivateSubscription;
+
+internal sealed class CompleteSubscriptionPaymentCommandHandler(SubscriptionsDbContext context)
+    : ICommandHandler<CompleteSubscriptionPaymentCommand>
+{
+    private readonly SubscriptionsDbContext _context = context;
+
+    public async Task<HandlerResponse> ExecuteAsync(CompleteSubscriptionPaymentCommand command, CancellationToken ct)
+    {
+        var subscriptionPayment = await _context
+            .SubscriptionPayments
+            .Where(x => x.PayerId == command.SubscriberId
+                && x.PaymentStatus == SubscriptionPaymentStatus.PaymentIntentRequested)
+            .SingleAsync(ct);
+
+        subscriptionPayment.MarkAsPaid(
+            command.SubscriptionId,
+            command.PaidAt,
+            command.SubscriptionActivatedAt,
+            command.SubscriptionExpiresAt);
+
+        return HandlerResponseStatus.NoContent;
+    }
+}
