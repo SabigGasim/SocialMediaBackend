aaaa4d50bf4fab096823c1f4d4e9e52a03819110
SabigGasim <sabgasim@gmail.com>
2025-07-17 00:13:47 +0300

    feat(app_subscriptions.infra): add SubscriptionRenewalPaymentEntityTypeConfiguration

---
+++ src/Modules/AppSubscriptions/Infrastructure/Domain/Subscriptions/SubscriptionRenewalPaymentEntityTypeConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SocialMediaBackend.Modules.AppSubscriptions.Domain.SubscriptionRenewalPayments;
+
+namespace SocialMediaBackend.Modules.AppSubscriptions.Infrastructure.Domain.Subscriptions;
+
+internal class SubscriptionRenewalPaymentEntityTypeConfiguration : IEntityTypeConfiguration<SubscriptionRenewalPayment>
+{
+    public void Configure(EntityTypeBuilder<SubscriptionRenewalPayment> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasConversion(
+                id => id.Value,
+                value => new(value));
+
+        builder.HasOne(x => x.Payer)
+            .WithMany(x => x.SubscriptionRenewalPayments)
+            .HasForeignKey(x => x.PayerId)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder.HasOne(x => x.Subscription)
+            .WithMany()
+            .HasForeignKey(x => x.SubscriptionId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
