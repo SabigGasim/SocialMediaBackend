f4c97415980112fe5eb712922555dc4677588c25
SabigGasim <sabgasim@gmail.com>
2025-07-17 00:19:16 +0300

    feat(api): add CancelAppSubscriptionAtPeriodEndEndpoint feature

---
+++ src/Presentation/Api/Modules/AppSubscriptions/Endpoints/CancelAppSubscriptionAtPeriodEndEndpoint.cs
@@ -0,0 +1,18 @@
+﻿using SocialMediaBackend.Api.Abstractions;
+using SocialMediaBackend.Modules.AppSubscriptions.Application.Contracts;
+using SocialMediaBackend.Modules.AppSubscriptions.Application.Subscriptions.CancelSubscriptionAtPeriodEnd;
+
+namespace SocialMediaBackend.Api.Modules.AppSubscriptions.Endpoints;
+
+public class CancelAppSubscriptionAtPeriodEndEndpoint(IAppSubscriptionsModule module) : RequestEndpoint(module)
+{
+    public override void Configure()
+    {
+        Delete(ApiEndpoints.Payments.CancelSubscriptionAtPeriodEnd);
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        await HandleCommandAsync(new CancelSubscriptionAtPeriodEndCommand(), ct);
+    }
+}
